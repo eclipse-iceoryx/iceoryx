f159af5da07962e6384b4412c85c221cb3d84bc3
Hintz Martin (CC-AD/ESW1) <martin.hintz@de.bosch.com>
2020-01-29 16:05:06 +0100

    iox-#32 Disable ACL for now
    Signed-off-by: Hintz Martin (CC-AD/ESW1) <martin.hintz@de.bosch.com>

--- iceoryx_posh/include/iceoryx_posh/internal/mepoo/mepoo_segment.hpp
+++ iceoryx_posh/include/iceoryx_posh/internal/mepoo/mepoo_segment.hpp
@@ -16,7 +16,7 @@
 
 #include "iceoryx_posh/internal/mepoo/memory_manager.hpp"
 #include "iceoryx_posh/mepoo/mepoo_config.hpp"
-#include "iceoryx_utils/internal/posix_wrapper/access_control.hpp"
+// #include "iceoryx_utils/internal/posix_wrapper/access_control.hpp"
 #include "iceoryx_utils/internal/posix_wrapper/posix_access_rights.hpp"
 #include "iceoryx_utils/internal/posix_wrapper/shared_memory_object.hpp"
 #include "iceoryx_utils/internal/posix_wrapper/shared_memory_object/allocator.hpp"
