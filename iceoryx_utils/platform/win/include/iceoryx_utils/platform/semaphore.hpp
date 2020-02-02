a65373979d10f9236899bf64bff676aeff6280e3
Christian Eltzschig <christian.eltzschig2@de.bosch.com>
2020-02-02 13:24:28 +0100

    iox-#32 added basic windows semaphore functions - but it is not complete

--- iceoryx_utils/platform/win/include/iceoryx_utils/platform/semaphore.hpp
+++ iceoryx_utils/platform/win/include/iceoryx_utils/platform/semaphore.hpp
@@ -16,48 +16,68 @@
 
 #include "iceoryx_utils/platform/types.hpp"
 
+#include <stdio.h>
+#include <windows.h>
+
 #define SEM_FAILED 0
 
-using sem_t = int;
+using sem_t = HANDLE;
+static constexpr LONG MAX_SEMAPHORE_VALUE = LONG_MAX;
 
 inline int sem_getvalue(sem_t* sem, int* sval)
 {
+    static_assert(false, "Not available for Windows");
     return 0;
 }
 
 inline int sem_post(sem_t* sem)
 {
-    return 0;
+    int retVal = (ReleaseSemaphore(sem, 1, nullptr) == 0) ? -1 : 0;
+    return retVal;
 }
 
 inline int sem_wait(sem_t* sem)
 {
-    return 0;
+    int retVal = (WaitForSingleObject(sem, INFINITE) == WAIT_FAILED) ? -1 : 0;
+    return retVal;
 }
 
 inline int sem_trywait(sem_t* sem)
 {
+    if (WaitForSingleObject(sem, 0) == WAIT_FAILED)
+    {
+        return -1;
+    }
     return 0;
 }
 
 inline int sem_timedwait(sem_t* sem, const struct timespec* abs_timeout)
 {
-    return 0;
+    DWORD timeoutInMilliseconds = static_cast<DWORD>(1);
+    int retVal = (WaitForSingleObject(sem, timeoutInMilliseconds) == WAIT_FAILED) ? -1 : 0;
+
+    return retVal;
 }
 
 inline int sem_close(sem_t* sem)
 {
-    return 0;
+    int retVal = CloseHandle(sem) ? 0 : -1;
+    return retVal;
 }
 
 inline int sem_destroy(sem_t* sem)
 {
+    // semaphores are closed in windows when the last process which is
+    // holding a semaphore calls CloseHandle
     return 0;
 }
 
 inline int sem_init(sem_t* sem, int pshared, unsigned int value)
 {
-    return 0;
+    *sem = CreateSemaphore(nullptr, static_cast<LONG>(value), MAX_SEMAPHORE_VALUE, nullptr);
+    if (sem != nullptr)
+        return 0;
+    return -1;
 }
 
 // sem_t *
@@ -67,10 +87,12 @@ inline int sem_init(sem_t* sem, int pshared, unsigned int value)
 
 inline sem_t* sem_open(const char* name, int oflag, mode_t mode, unsigned int value)
 {
-    return 0;
+    return OpenSemaphoreW(0, false, name);
 }
 
 inline int sem_unlink(const char* name)
 {
+    // semaphores are closed in windows when the last process which is
+    // holding a semaphore calls CloseHandle
     return 0;
 }
