7ba013230afe0f71110773ef290f6f1efc984b52
Christian Eltzschig <christian.eltzschig2@de.bosch.com>
2020-03-19 17:14:28 +0100

    iox-#32 working in timer
    Signed-off-by: Christian Eltzschig <christian.eltzschig2@de.bosch.com>

--- iceoryx_utils/platform/mac/include/iceoryx_utils/platform/time.hpp
+++ iceoryx_utils/platform/mac/include/iceoryx_utils/platform/time.hpp
@@ -14,9 +14,9 @@
 
 #pragma once
 
+#include <dispatch/dispatch.h>
 #include <sys/time.h>
 
-using timer_t = void*;
 
 struct itimerspec
 {
@@ -24,87 +24,15 @@ struct itimerspec
     timespec it_value;
 };
 
-#if 0
-#include <dispatch/dispatch.h>
-
-#include <chrono>
-#include <iostream>
-#include <thread>
-
-int test(dispatch_source_t sourceTimer) { printf("test called\n"); }
-
-int main() {
-    dispatch_queue_attr_t queueAttributes = 0;
-    printf("creating queue\n");
-    dispatch_queue_t timerQueue =
-        dispatch_queue_create("timerQueueName", queueAttributes);
-
-    uintptr_t timerHandle;
-    unsigned long timerMask;
-    printf("creating timer\n");
-    dispatch_source_t timer = dispatch_source_create(
-        DISPATCH_SOURCE_TYPE_TIMER, timerHandle, timerMask, timerQueue);
-
-    printf("set handler\n");
-    dispatch_source_set_event_handler(timer, ^{
-      test(timer);
-    });
-
-    printf("set cancel handler\n");
-    dispatch_source_set_cancel_handler(timer, ^{
-      dispatch_release(timer);
-      dispatch_release(timerQueue);
-      printf("cleaned up timer and timerQueue");
-    });
-
-    printf("set timer interval\n");
-    dispatch_time_t start = dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_SEC / 10);
-    dispatch_source_set_timer(timer, start, 1 * NSEC_PER_SEC / 10, 0);
-
-    printf("activate timer\n");
-    dispatch_resume(timer);
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-
-    printf("cleaning up timer\n");
-    dispatch_source_cancel(timer);
-}
-
-#endif
-
-inline int timer_create(clockid_t clockid, struct sigevent* sevp, timer_t* timerid)
+struct appleTimer_t
 {
-    // dispatch_queue_t timerQueue =
-    //    dispatch_queue_create("timerQueueName", queueAttributes);
-    // dispatch_source_t timer = dispatch_source_create(
-    //    DISPATCH_SOURCE_TYPE_TIMER, timerHandle, timerMask, timerQueue);
-    // dispatch_source_set_cancel_handler(timer, ^{
-    //  dispatch_release(timer);
-    //  dispatch_release(timerQueue);
-    //  printf("cleaned up timer and timerQueue");
-    //});
-    // dispatch_resume(timer);
-    return 0;
-}
+    dispatch_source_t timer;
+};
 
-inline int timer_delete(timer_t timerid)
-{
-    // dispatch_source_cancel(timer);
-    return 0;
-}
+using timer_t = appleTimer_t*;
 
-inline int timer_settime(timer_t timerid, int flags, const struct itimerspec* new_value, struct itimerspec* old_value)
-{
-    // dispatch_source_set_timer(timer, start, 1 * NSEC_PER_SEC / 10, 0);
-    return 0;
-}
-
-inline int timer_gettime(timer_t timerid, struct itimerspec* curr_value)
-{
-    return 0;
-}
-
-inline int timer_getoverrun(timer_t timerid)
-{
-    return 0;
-}
+int timer_create(clockid_t clockid, struct sigevent* sevp, timer_t* timerid);
+int timer_delete(timer_t timerid);
+int timer_settime(timer_t timerid, int flags, const struct itimerspec* new_value, struct itimerspec* old_value);
+int timer_gettime(timer_t timerid, struct itimerspec* curr_value);
+int timer_getoverrun(timer_t timerid);
