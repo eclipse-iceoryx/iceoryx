c38ebd1d504cdaafeb47d62cb75c9e730ab91b7d
Hoinkis Simon (CC-AD/ESW1) <simon.hoinkis2@de.bosch.com>
2020-04-20 15:24:28 +0200

    iox-#25 Implement three threads for building block integration test
    Signed-off-by: Hoinkis Simon (CC-AD/ESW1) <simon.hoinkis2@de.bosch.com>

--- iceoryx_posh/test/integrationtests/test_popo_chunk_building_blocks.cpp
+++ iceoryx_posh/test/integrationtests/test_popo_chunk_building_blocks.cpp
@@ -12,11 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "iceoryx_posh/internal/popo/building_blocks/chunk_distributor.hpp"
+#include "iceoryx_posh/internal/popo/building_blocks/chunk_receiver.hpp"
 #include "iceoryx_posh/internal/popo/building_blocks/chunk_sender.hpp"
 #include "iceoryx_posh/mepoo/mepoo_config.hpp"
 
 #include "test.hpp"
 
+#include <chrono>
 #include <stdlib.h>
 #include <thread>
 
@@ -25,7 +28,7 @@ using namespace iox::popo;
 using namespace iox::cxx;
 using namespace iox::mepoo;
 using ::testing::Return;
-using namespace std::chrono_literals;
+//using namespace std::chrono_literals;
 
 struct DummySample
 {
@@ -48,11 +51,8 @@ class ChunkBuildingBlocks_IntegrationTest : public Test
 
     void SetUp()
     {
-        /// @todo connect all four sut's
-        // ChunkSender
-        // ChunkSenderData
-        // ChunkReceiver
-        // ChunkReceiverData
+        m_chunkSender.addQueue(&m_chunkData);
+        m_chunkDistributor.addQueue(&m_chunkReceiverData);
     }
 
 
@@ -61,10 +61,22 @@ class ChunkBuildingBlocks_IntegrationTest : public Test
     }
 
 
-    std::thread chunkSenderThread{([&] {
-        /// @todo in a loop
+    std::thread publishingThread{([&] {
         // std::this_thread::sleep_for((rand() % 100)ms);
-        // auto chunk = m_chunkSender.allocate(sizeof(DummySample));
+        for (size_t i = 0; i < NUM_CHUNKS_IN_POOL; i++)
+        {
+            auto maybeChunkHeader = m_chunkSender.allocate(sizeof(DummySample));
+            EXPECT_FALSE(maybeChunkHeader.has_error());
+
+            if (!maybeChunkHeader.has_error())
+            {
+                auto sample = (*maybeChunkHeader)->payload();
+                new (sample) DummySample();
+                static_cast<DummySample*>(sample)->dummy = i;
+                m_chunkSender.send(*maybeChunkHeader);
+            }
+        }
+
         // EXPECT_FALSE(chunk.has_error());
         // EXPECT_THAT(m_memoryManager.getMemPoolInfo(0).m_usedChunks, Eq(1u));
         // m_chunkSender.send(chunk);
@@ -72,16 +84,38 @@ class ChunkBuildingBlocks_IntegrationTest : public Test
         // std::this_thread::sleep_for((rand() % 100)ms);
     })};
 
-    std::thread chunkDistributorThread{([&] {
-        /// @todo in a loop
+    std::thread forwardingThread{([&] {
         // std::this_thread::sleep_for((rand() % 100)ms);
+        while (1)
+        {
+            auto maybeSharedChunk = m_popper.pop();
+            if (maybeSharedChunk.has_value())
+            {
+                m_chunkDistributor.deliverToAllStoredQueues(maybeSharedChunk.value());
+            }
+        }
         // deliverToAllStoredQueues(); // multi threaded policy
         // random sleep between 0-1s?
         // std::this_thread::sleep_for((rand() % 100)ms);
     })};
 
-    std::thread ChunkReceiverThread{([&] {
-        /// @todo in a loop
+    std::thread subscribingThread{([&] {
+        uint64_t counter{0};
+
+        while (1)
+        {
+            auto maybeChunkHeader = m_chunkReceiver.get();
+            if (!maybeChunkHeader.has_error() && maybeChunkHeader.get_value().has_value())
+            {
+                auto chunkHeader = maybeChunkHeader.get_value().value();
+
+                auto dummySample = *reinterpret_cast<DummySample*>(chunkHeader->payload());
+                EXPECT_THAT(dummySample.dummy, Eq(counter));
+                counter++;
+                m_chunkReceiver.release(chunkHeader);
+            }
+        }
+
         // std::this_thread::sleep_for((rand() % 100)ms);
         // get();
         // cast to SampleType
@@ -102,16 +136,26 @@ class ChunkBuildingBlocks_IntegrationTest : public Test
     iox::mepoo::MemoryManager m_memoryManager;
 
     using ChunkDistributorData_t = iox::popo::ChunkDistributorData<MAX_NUMBER_QUEUES, iox::popo::ThreadSafePolicy>;
-    iox::popo::ChunkSenderData<ChunkDistributorData_t> m_chunkSenderData{&m_memoryManager, 0}; // must be 0 for test
-    iox::popo::ChunkSenderData<ChunkDistributorData_t> m_chunkSenderDataWithHistory{&m_memoryManager, HISTORY_CAPACITY};
+    using ChunkDistributor_t = iox::popo::ChunkDistributor<ChunkDistributorData_t>;
 
-    using ChunkDistributor_t = iox::popo::ChunkDistributor<MAX_NUMBER_QUEUES, iox::popo::ThreadSafePolicy>;
+    // Publishing part
+    iox::popo::ChunkSenderData<ChunkDistributorData_t> m_chunkSenderData{&m_memoryManager};
     iox::popo::ChunkSender<ChunkDistributor_t> m_chunkSender{&m_chunkSenderData};
-    iox::popo::ChunkSender<ChunkDistributor_t> m_chunkSenderWithHistory{&m_chunkSenderDataWithHistory};
+
+    // Forwarding part
+    ChunkDistributorData_t m_chunkDistributorData;
+    ChunkDistributor_t m_chunkDistributor{&m_chunkDistributorData};
+    ChunkQueueData m_chunkData{iox::cxx::VariantQueueTypes::SoFi_SingleProducerSingleConsumer};
+    ChunkQueuePopper m_popper{&m_chunkData};
+
+    // Subscribing part
+    iox::popo::ChunkReceiverData m_chunkReceiverData{iox::cxx::VariantQueueTypes::SoFi_SingleProducerSingleConsumer};
+    iox::popo::ChunkReceiver m_chunkReceiver{&m_chunkReceiverData};
 };
 
 TEST_F(ChunkBuildingBlocks_IntegrationTest, SendWithoutConnection)
 {
+    // sleep?
 }
 
 TEST_F(ChunkBuildingBlocks_IntegrationTest, SendAndReceive)
