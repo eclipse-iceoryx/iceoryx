00d5ae217ee4d9663047a8fa18ec8f8b3806a3e0
Hintz Martin (CC-AD/ESW1) <martin.hintz@de.bosch.com>
2020-06-05 11:28:38 +0200

    iox-#30 Fix Windows platform correction
    Signed-off-by: Hintz Martin (CC-AD/ESW1) <martin.hintz@de.bosch.com>

--- iceoryx_utils/platform/win/include/iceoryx_utils/platform/platform_correction.hpp
+++ iceoryx_utils/platform/win/include/iceoryx_utils/platform/platform_correction.hpp
@@ -11,10 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#ifndef IOX_UTILS_PLATFORM_PLATFORM_CORRECTION_HPP
-#define IOX_UTILS_PLATFORM_PLATFORM_CORRECTION_HPP
 
-// !NO PRAGMA ONE HERE! we want that these macros are always executed and
+
+// !NO PRAGMA ONCE OR INCLUDE GUARD HERE!
+// We want that these macros are always executed and
 // not just once, otherwise the windows macros are kicking in and nothing
 // compiles
 //#pragma once
@@ -23,6 +23,9 @@
 // Usage Instructions: This header has to be ALWAYS the last header which
 // is included otherwise some windows header pops up and defines some macros
 
+#ifndef __PRETTY_FUNCTION__
+#define __PRETTY_FUNCTION__ __FUNCSIG__
+#endif
 
 #undef CreateMutex
 #undef max
@@ -31,5 +34,3 @@
 #undef interface
 #undef CreateSemaphore
 #undef NO_ERROR
-
-#endif // IOX_UTILS_PLATFORM_PLATFORM_CORRECTION_HPP
