328ef5d0fc93be7a1fc61c035d2abfff4ecc4284
Poehnl Michael (CC-AD/ESW1) <michael.poehnl@de.bosch.com>
2020-06-14 00:23:05 +0200

    iox-#25: subscriber port data
    Signed-off-by: Poehnl Michael (CC-AD/ESW1) <michael.poehnl@de.bosch.com>

---
+++ iceoryx_posh/include/iceoryx_posh/internal/popo/ports/subscriber_port_data.hpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2020 by Robert Bosch GmbH. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef IOX_POPO_SUBSCRIBER_PORT_DATA_HPP_
+#define IOX_POPO_SUBSCRIBER_PORT_DATA_HPP_
+
+#include "iceoryx_posh/capro/service_description.hpp"
+#include "iceoryx_posh/iceoryx_posh_types.hpp"
+#include "iceoryx_posh/internal/popo/building_blocks/chunk_receiver_data.hpp"
+#include "iceoryx_posh/internal/popo/ports/base_port_data.hpp"
+#include "iceoryx_utils/cxx/variant_queue.hpp"
+
+#include <atomic>
+
+namespace iox
+{
+namespace popo
+{
+struct SubscriberPortData : public BasePortData
+{
+    SubscriberPortData(const capro::ServiceDescription& serviceDescription,
+                       const ProcessName_t& processName,
+                       cxx::VariantQueueTypes queueType,
+                       const mepoo::MemoryInfo& memoryInfo = mepoo::MemoryInfo()) noexcept;
+
+    ChunkReceiverData m_chunkReceiverData;
+    std::atomic_bool m_subscribeRequested{false};
+    std::atomic<SubscribeState> m_subscriptionState{SubscribeState::NOT_SUBSCRIBED};
+};
+
+} // namespace popo
+} // namespace iox
+
+#endif
