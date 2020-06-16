0a2892fb572ee30b2a660a751e2ec7d54a63ff30
Hoinkis Simon (CC-AD/ESW1) <simon.hoinkis2@de.bosch.com>
2020-06-16 15:25:03 +0200

    iox-#25 Add empty unit tests for ConditionVariableSignaler and ConditionVariableWaiter
    Signed-off-by: Hoinkis Simon (CC-AD/ESW1) <simon.hoinkis2@de.bosch.com>

---
+++ iceoryx_posh/test/moduletests/test_popo_condition_variable_signaler.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2020 by Robert Bosch GmbH. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "iceoryx_posh/internal/mepoo/shared_chunk.hpp"
+#include "iceoryx_posh/internal/popo/waitset/condition_variable_data.hpp"
+#include "iceoryx_posh/mepoo/chunk_header.hpp"
+#include "test.hpp"
+
+#include <memory>
+
+using namespace ::testing;
+using ::testing::Return;
+using namespace iox::popo;
+using namespace iox::cxx;
+using namespace iox::mepoo;
+
+class ConditionVariableSignaler_test : public Test
+{
+  public:
+    static constexpr size_t MEGABYTE = 1 << 20;
+    static constexpr size_t MEMORY_SIZE = 1 * MEGABYTE;
+    const uint64_t HISTORY_SIZE = 16;
+    static constexpr uint32_t MAX_NUMBER_QUEUES = 128;
+    char memory[MEMORY_SIZE];
+    iox::posix::Allocator allocator{memory, MEMORY_SIZE};
+    MemPool mempool{128, 20, &allocator, &allocator};
+    MemPool chunkMgmtPool{128, 20, &allocator, &allocator};
+
+    void SetUp(){};
+    void TearDown(){};
+};
+
+TEST_F(ConditionVariableSignaler_test, NoSignalResultsInWait)
+{
+}
