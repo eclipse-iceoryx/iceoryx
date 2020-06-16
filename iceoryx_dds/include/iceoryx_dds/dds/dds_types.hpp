ce0fdd6c1edb9f6198b816365ddaf4c27e468452
Ithier Jeff (CC-AD/EYF1) <jeff.ithier@de.bosch.com>
2020-06-16 15:00:38 +0200

    iox-#65 Code cleanliness fixes.
    Signed-off-by: Ithier Jeff (CC-AD/EYF1) <jeff.ithier@de.bosch.com>

--- iceoryx_dds/include/iceoryx_dds/dds/dds_types.hpp
+++ iceoryx_dds/include/iceoryx_dds/dds/dds_types.hpp
@@ -27,7 +27,7 @@ namespace iox
 namespace dds
 {
 // Fixed strings
-using IdString = iox::cxx::string<100>;
+using IdString = iox::cxx::string<100u>;
 
 // DDS implementation defined with compiler flags
 #ifdef USE_CYCLONE_DDS
