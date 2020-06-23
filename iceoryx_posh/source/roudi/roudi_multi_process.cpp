99f0db032cdb1ffaa9b5f6f2662c34c057d94ec6
Killat Matthias (CC-AD/ESW1) <kia2be@ABTZ0I6P-OSDVM.bosch-si.com>
2020-06-23 16:02:17 +0200

    iox-#171 added option to defer mq thread startup
    Signed-off-by: Kroenke Dietrich (CC-AD/ESW1) <Dietrich.Kroenke2@de.bosch.com>

--- iceoryx_posh/source/roudi/roudi_multi_process.cpp
+++ iceoryx_posh/source/roudi/roudi_multi_process.cpp
@@ -29,7 +29,8 @@ namespace roudi
 RouDiMultiProcess::RouDiMultiProcess(RouDiMemoryInterface& roudiMemoryInterface,
                                      PortManager& portManager,
                                      const MonitoringMode monitoringMode,
-                                     const bool killProcessesInDestructor)
+                                     const bool killProcessesInDestructor,
+                                     const MQThreadMode mqThreadMode)
     : m_killProcessesInDestructor(killProcessesInDestructor)
     , m_runThreads(true)
     , m_roudiMemoryInterface(&roudiMemoryInterface)
@@ -54,13 +55,21 @@ RouDiMultiProcess::RouDiMultiProcess(RouDiMemoryInterface& roudiMemoryInterface,
     m_processManagementThread = std::thread(&RouDiMultiProcess::processThread, this);
     pthread_setname_np(m_processManagementThread.native_handle(), "ProcessMgmt");
 
-    m_processMQThread = std::thread(&RouDiMultiProcess::mqThread, this);
-    pthread_setname_np(m_processMQThread.native_handle(), "MQ-processing");
+    if(mqThreadMode == MQThreadMode::START) {
+        startMQThread();
+    }
+
 }
 
 RouDiMultiProcess::~RouDiMultiProcess()
 {
     shutdown();
+}
+
+void RouDiMultiProcess::startMQThread() 
+{
+    m_processMQThread = std::thread(&RouDiMultiProcess::mqThread, this);
+    pthread_setname_np(m_processMQThread.native_handle(), "MQ-processing");
 }
 
 void RouDiMultiProcess::shutdown()
