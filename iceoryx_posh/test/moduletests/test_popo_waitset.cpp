9ee2edc4eab38e216e60f9def388f7ab38851b4e
Hintz Martin (CC-AD/ESW1) <martin.hintz@de.bosch.com>
2020-06-30 13:33:34 +0200

    iox-#25: Remove unnecessary mem config from test
    Signed-off-by: Hintz Martin (CC-AD/ESW1) <martin.hintz@de.bosch.com>

--- iceoryx_posh/test/moduletests/test_popo_waitset.cpp
+++ iceoryx_posh/test/moduletests/test_popo_waitset.cpp
@@ -12,13 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "iceoryx_posh/internal/mepoo/shared_chunk.hpp"
 #include "iceoryx_posh/internal/popo/waitset/condition.hpp"
 #include "iceoryx_posh/internal/popo/waitset/condition_variable_data.hpp"
 #include "iceoryx_posh/internal/popo/waitset/condition_variable_signaler.hpp"
 #include "iceoryx_posh/internal/popo/waitset/guard_condition.hpp"
 #include "iceoryx_posh/internal/popo/waitset/wait_set.hpp"
-#include "iceoryx_posh/mepoo/chunk_header.hpp"
 #include "iceoryx_utils/cxx/vector.hpp"
 #include "test.hpp"
 
@@ -34,14 +32,6 @@ class WaitSet_test : public Test
 {
   public:
     static constexpr uint16_t MAX_NUMBER_OF_CONDITIONS_WITHOUT_GUARD = iox::popo::MAX_NUMBER_OF_CONDITIONS - 1;
-    static constexpr size_t MEGABYTE = 1 << 20;
-    static constexpr size_t MEMORY_SIZE = 1 * MEGABYTE;
-    const uint64_t HISTORY_SIZE = 16;
-    static constexpr uint32_t MAX_NUMBER_QUEUES = 128;
-    char memory[MEMORY_SIZE];
-    iox::posix::Allocator allocator{memory, MEMORY_SIZE};
-    MemPool mempool{128, 20, &allocator, &allocator};
-    MemPool chunkMgmtPool{128, 20, &allocator, &allocator};
 
     Condition m_condition;
     ConditionVariableData m_condVarData;
