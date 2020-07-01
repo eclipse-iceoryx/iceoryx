477563abdbb690eae014586d1b26fa12c380e098
Kraus Mathias (CC-AD/ESW1) <mathias.kraus2@de.bosch.com>
2020-07-01 17:28:08 +0200

    iox-#153 add '-Wconversion' only for GCC builds
    Signed-off-by: Kraus Mathias (CC-AD/ESW1) <mathias.kraus2@de.bosch.com>

--- iceoryx_utils/cmake/IceoryxPlatformDetection.cmake
+++ iceoryx_utils/cmake/IceoryxPlatformDetection.cmake
@@ -16,13 +16,13 @@ elseif(APPLE)
     set(ICEORYX_CXX_STANDARD 17)
 endif(LINUX)
 
-set (ICEORYX_WARNINGS -W -Wall -Wextra -Wconversion -Wuninitialized -Wpedantic -Wstrict-aliasing -Wcast-align -Wno-noexcept-type)
+set (ICEORYX_WARNINGS -W -Wall -Wextra -Wuninitialized -Wpedantic -Wstrict-aliasing -Wcast-align -Wno-noexcept-type)
 
-if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     # temporary disable conversion warnings for clang
     # this needs to be fixed with iox-#163
-    set(ICEORYX_WARNINGS ${ICEORYX_WARNINGS} -Wno-conversion)
-endif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    set(ICEORYX_WARNINGS ${ICEORYX_WARNINGS} -Wconversion)
+endif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
 
 if(BUILD_STRICT)
     set(ICEORYX_WARNINGS ${ICEORYX_WARNINGS} -Werror)
