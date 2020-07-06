06b61c09af2837b8b259ed830174fd94e571fc52
Ithier Jeff (CC-AD/EYF1) <jeff.ithier@de.bosch.com>
2020-07-06 09:45:13 +0200

    iox-#166 Added copyright notice to all scripts and dockerfiles.
    Signed-off-by: Ithier Jeff (CC-AD/EYF1) <jeff.ithier@de.bosch.com>

--- tools/docker/run_iceoryx_docker.sh
+++ tools/docker/run_iceoryx_docker.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+
+# Copyright (c) 2020 by Robert Bosch GmbH. All rights reserved.
 #
+#/ Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Runs the iceoryx docker.
 # The docker will immediately start RouDi. It can then be interacted with by
 # either binding a shell to it or connecting via screen.
@@ -8,7 +22,7 @@
 #
 
 print_usage () {
-        echo "Usage: $0 <shm-size>"
+        echo "Usage: $0 <shm-size> [<container_name>]"
 }
 
 SHM_SIZE=$1
@@ -17,4 +31,10 @@ then
 	SHM_SIZE=700M
 fi
 
-docker run -it --rm --shm-size $SHM_SIZE --name=RouDi iceoryx:latest
+CONTAINER_NAME=$2
+if [ -z "$1" ];
+then
+CONTAINER_NAME=RouDi
+fi 
+
+docker run -it --rm --shm-size $SHM_SIZE --name $CONTAINER_NAME iceoryx:latest
