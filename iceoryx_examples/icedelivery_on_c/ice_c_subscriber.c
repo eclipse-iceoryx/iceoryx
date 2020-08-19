1c48ad06b99f21758b94171aae2c7e7d54cc43e1
Christian Eltzschig <christian.eltzschig2@de.bosch.com>
2020-08-19 16:13:39 +0200

    iox-#252 added posh runtime into c example
    Signed-off-by: Christian Eltzschig <christian.eltzschig2@de.bosch.com>

--- iceoryx_examples/icedelivery_on_c/ice_c_subscriber.c
+++ iceoryx_examples/icedelivery_on_c/ice_c_subscriber.c
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "iceoryx_binding_c/posh_runtime.h"
 #include "iceoryx_binding_c/subscriber.h"
 
 void receiving()
@@ -20,6 +21,8 @@ void receiving()
 
 int main()
 {
+    PoshRuntime_getInstance("/iox-c-publisher");
+
     struct SubscriberPortData* subscriber = Subscriber_new();
     Subscriber_subscribe(subscriber, 10);
     Subscriber_delete(subscriber);
