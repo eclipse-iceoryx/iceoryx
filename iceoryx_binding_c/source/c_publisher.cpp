0c7366b8cb756f558010a20d016d265bd72f802b
Christian Eltzschig <christian.eltzschig2@de.bosch.com>
2020-09-08 09:49:58 +0200

    iox-#252 moved files accidentally deleted - readding them
    Signed-off-by: Christian Eltzschig <christian.eltzschig2@de.bosch.com>

---
+++ iceoryx_binding_c/source/c_publisher.cpp
@@ -0,0 +1,103 @@
+// Copyright (c) 2020 by Robert Bosch GmbH. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "iceoryx_posh/internal/popo/ports/publisher_port_user.hpp"
+
+using namespace iox;
+using namespace iox::cxx;
+using namespace iox::popo;
+using namespace iox::capro;
+using namespace iox::mepoo;
+
+extern "C" {
+#include "iceoryx_binding_c/publisher.h"
+}
+
+PublisherPortData*
+Publisher_new(const char* service, const char* instance, const char* event, const uint64_t historyCapacity)
+{
+    return new PublisherPortData(
+        ServiceDescription{
+            IdString(TruncateToCapacity, service),
+            IdString(TruncateToCapacity, instance),
+            IdString(TruncateToCapacity, event),
+        },
+        "JoinTheChurchOfHypnotoad!",
+        nullptr,
+        historyCapacity);
+}
+
+void Publisher_delete(PublisherPortData* const self)
+{
+    delete self;
+}
+
+iox_popo_AllocationResult
+Publisher_allocateChunk(PublisherPortData* const self, void** const chunk, const uint32_t payloadSize)
+{
+    auto result =
+        PublisherPortUser(self).allocateChunk(payloadSize).and_then([&](ChunkHeader* h) { *chunk = h->payload(); });
+    if (result.has_error())
+    {
+        switch (result.get_error())
+        {
+        case AllocationError::RUNNING_OUT_OF_CHUNKS:
+            return AllocationResult_RUNNING_OUT_OF_CHUNKS;
+        case AllocationError::TOO_MANY_CHUNKS_ALLOCATED_IN_PARALLEL:
+            return AllocationResult_TOO_MANY_CHUNKS_ALLOCATED_IN_PARALLEL;
+        default:
+            return AllocationResult_UNDEFINED_ERROR;
+        }
+    }
+
+    return AllocationResult_SUCCESS;
+}
+
+void Publisher_freeChunk(PublisherPortData* const self, void* const chunk)
+{
+    PublisherPortUser(self).freeChunk(convertPayloadPointerToChunkHeader(chunk));
+}
+
+void Publisher_sendChunk(PublisherPortData* const self, void* const chunk)
+{
+    PublisherPortUser(self).sendChunk(convertPayloadPointerToChunkHeader(chunk));
+}
+
+const void* Publisher_tryGetPreviousChunk(PublisherPortData* const self)
+{
+    const void* returnValue = nullptr;
+    PublisherPortUser(self).getLastChunk().and_then([&](const ChunkHeader* h) { returnValue = h->payload(); });
+    return returnValue;
+}
+
+void Publisher_offer(PublisherPortData* const self)
+{
+    PublisherPortUser(self).offer();
+}
+
+void Publisher_stopOffer(PublisherPortData* const self)
+{
+    PublisherPortUser(self).stopOffer();
+}
+
+bool Publisher_isOffered(PublisherPortData* const self)
+{
+    return PublisherPortUser(self).isOffered();
+}
+
+bool Publisher_hasSubscribers(PublisherPortData* const self)
+{
+    return PublisherPortUser(self).hasSubscribers();
+}
+
---
+++ iceoryx_binding_c/source/c_subscriber.cpp
@@ -0,0 +1,130 @@
+// Copyright (c) 2020 by Robert Bosch GmbH. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include "iceoryx_posh/internal/popo/building_blocks/condition_variable_data.hpp"
+#include "iceoryx_posh/internal/popo/ports/subscriber_port_user.hpp"
+#include "iceoryx_posh/mepoo/chunk_header.hpp"
+
+using namespace iox;
+using namespace iox::cxx;
+using namespace iox::popo;
+using namespace iox::capro;
+using namespace iox::mepoo;
+
+extern "C" {
+#include "iceoryx_binding_c/subscriber.h"
+}
+
+SubscriberPortData* Subscriber_new(const char* const service,
+                                   const char* const instance,
+                                   const char* const event,
+                                   const uint64_t historyRequest)
+{
+    return new SubscriberPortData(ServiceDescription{IdString(TruncateToCapacity, service),
+                                                     IdString(TruncateToCapacity, instance),
+                                                     IdString(TruncateToCapacity, event)},
+                                  "AllHailHypnotoad!",
+                                  VariantQueueTypes::SoFi_SingleProducerSingleConsumer,
+                                  historyRequest);
+}
+
+void Subscriber_delete(SubscriberPortData* const self)
+{
+    delete self;
+}
+
+void Subscriber_subscribe(SubscriberPortData* const self, const uint64_t queueCapacity)
+{
+    SubscriberPortUser(self).subscribe(queueCapacity);
+}
+
+void Subscriber_unsubscribe(SubscriberPortData* const self)
+{
+    SubscriberPortUser(self).unsubscribe();
+}
+
+iox_SubscribeState Subscriber_getSubscriptionState(SubscriberPortData* const self)
+{
+    switch (SubscriberPortUser(self).getSubscriptionState())
+    {
+    case SubscribeState::NOT_SUBSCRIBED:
+        return iox_SubscribeState::SubscribeState_NOT_SUBSCRIBED;
+    case SubscribeState::SUBSCRIBE_REQUESTED:
+        return iox_SubscribeState::SubscribeState_SUBSCRIBE_REQUESTED;
+    case SubscribeState::SUBSCRIBED:
+        return iox_SubscribeState::SubscribeState_SUBSCRIBED;
+    case SubscribeState::UNSUBSCRIBE_REQUESTED:
+        return iox_SubscribeState::SubscribeState_UNSUBSCRIBE_REQUESTED;
+    case SubscribeState::WAIT_FOR_OFFER:
+        return iox_SubscribeState::SubscribeState_WAIT_FOR_OFFER;
+    default:
+        return iox_SubscribeState::SubscribeState_UNDEFINED;
+    }
+}
+
+iox_popo_ChunkReceiveResult Subscriber_getChunk(SubscriberPortData* const self, const void** const payload)
+{
+    auto result = SubscriberPortUser(self).getChunk();
+    if (result.has_error())
+    {
+        return (result.get_error() == ChunkReceiveError::TOO_MANY_CHUNKS_HELD_IN_PARALLEL)
+                   ? ChunkReceiveResult_TOO_MANY_CHUNKS_HELD_IN_PARALLEL
+                   : ChunkReceiveResult_INTERNAL_ERROR;
+    }
+
+    if (!result->has_value())
+    {
+        return ChunkReceiveResult_NO_CHUNK_RECEIVED;
+    }
+
+    *payload = (**result)->payload();
+    return ChunkReceiveResult_SUCCESS;
+}
+
+void Subscriber_releaseChunk(SubscriberPortData* const self, const void* const chunk)
+{
+    SubscriberPortUser(self).releaseChunk(convertPayloadPointerToChunkHeader(chunk));
+}
+
+void Subscriber_releaseQueuedChunks(SubscriberPortData* const self)
+{
+    SubscriberPortUser(self).releaseQueuedChunks();
+}
+
+bool Subscriber_hasNewChunks(SubscriberPortData* const self)
+{
+    return SubscriberPortUser(self).hasNewChunks();
+}
+
+bool Subscriber_hasLostChunks(SubscriberPortData* const self)
+{
+    return SubscriberPortUser(self).hasLostChunks();
+}
+
+bool Subscriber_attachConditionVariable(SubscriberPortData* const self, ConditionVariableData* const cvHandle)
+{
+    return SubscriberPortUser(self).attachConditionVariable(cvHandle);
+}
+
+bool Subscriber_detachConditionVariable(SubscriberPortData* const self)
+{
+    return SubscriberPortUser(self).detachConditionVariable();
+}
+
+bool Subscriber_isConditionVariableAttached(SubscriberPortData* const self)
+{
+    return SubscriberPortUser(self).isConditionVariableAttached();
+}
+
