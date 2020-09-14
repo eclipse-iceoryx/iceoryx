3c95bbfcffc65cf48b77ab3ab12b601a9cd1ee25
Christian Eltzschig <christian.eltzschig2@de.bosch.com>
2020-09-14 15:53:58 +0200

    iox-#252 added typedefs for all storage struct types
    Signed-off-by: Christian Eltzschig <christian.eltzschig2@de.bosch.com>

--- iceoryx_binding_c/include/iceoryx_binding_c/types.h
+++ iceoryx_binding_c/include/iceoryx_binding_c/types.h
@@ -17,24 +17,26 @@
 
 #include "internal/c2cpp_binding.h"
 
-struct iox_wait_set_storage_t
+struct iox_wait_set_storage_t_
 {
     // the value of the array size is the result of the following formula:
     // sizeof(WaitSet) / 8
     uint64_t do_not_touch_me[133];
 };
+typedef struct iox_wait_set_storage_t_ iox_wait_set_storage_t;
 
-struct iox_guard_cond_storage_t
+struct iox_guard_cond_storage_t_
 {
     // the value of the array size is the result of the following formula:
     // sizeof(GuardCondition) / 8
     uint64_t do_not_touch_me[9];
 };
+typedef struct iox_guard_cond_storage_t_ iox_guard_cond_storage_t;
 
 struct iox_sub_storage_t_
 {
     // the value of the array size is the result of the following formula:
-    // sizeof(GuardCondition) / 8
+    // sizeof(cpp2c_Subscriber) / 8
     uint64_t do_not_touch_me[4];
 };
 typedef struct iox_sub_storage_t_ iox_sub_storage_t;
