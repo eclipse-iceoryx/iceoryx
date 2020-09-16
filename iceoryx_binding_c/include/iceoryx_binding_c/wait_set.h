1da2ff1f6e6b91f01e040d32ca326a69ec03b7c8
Christian Eltzschig <christian.eltzschig2@de.bosch.com>
2020-09-16 10:02:18 +0200

    iox-#252 added waitset doxygen docu
    Signed-off-by: Christian Eltzschig <christian.eltzschig2@de.bosch.com>

--- iceoryx_binding_c/include/iceoryx_binding_c/wait_set.h
+++ iceoryx_binding_c/include/iceoryx_binding_c/wait_set.h
@@ -24,21 +24,57 @@
 /// @brief wait set handle
 typedef struct WaitSet* iox_wait_set_t;
 
+/// @brief initialize wait set handle
+/// @param[in] self pointer to preallocated memory of size = sizeof(iox_wait_set_storage_t)
+/// @return handle to wait set
 iox_wait_set_t iox_wait_set_init(iox_wait_set_storage_t* self);
+
+/// @brief deinitialize wait set handle
+/// @param[in] self the handle which should be deinitialized
 void iox_wait_set_deinit(iox_wait_set_t const self);
 
+/// @brief Attach a condition to your wait set. If you would like to destroy the condition
+///         you have to detach it first from the wait set.
+/// @param[in] self handle to the wait set
+/// @param[in] condition the condition you would like to attach
+/// @return if the attachment was successful it returns WaitSetResult_SUCCESS, otherwise
+///          an enum which describes the error
 ENUM iox_WaitSetResult iox_wait_set_attach_condition(iox_wait_set_t const self, iox_cond_t const condition);
 
+/// @brief detaches a condition.
+/// @param[in] self handle to the wait set
+/// @param[in] condition the condition you would like to detach
+/// @return true if the condition could be detached, otherwise false for instance when the
+///         condition was not attached before
 bool iox_wait_set_detach_condition(iox_wait_set_t const self, iox_cond_t const condition);
 
+/// @brief detaches all conditions
+/// @param[in] self handle to the wait set
 void iox_wait_set_detach_all_conditions(iox_wait_set_t const self);
 
+/// @brief waits until a condition was triggered or the timeout was reached
+/// @param[in] self handle to the wait set
+/// @param[in] timeout duration how long this method should wait
+/// @param[in] conditionArray preallocated memory to an array of iox_cond_t in which
+///             the conditions which were triggered can be written to
+/// @param[in] conditionArrayCapacity the capacity of the preallocated conditionArray
+/// @param[in] missedElements if the conditionArray has insufficient size the number of missed elements
+///             which could not be written into the array are stored here
+/// @return number of elements which were written into the conditionArray
 uint64_t iox_wait_set_timed_wait(iox_wait_set_t const self,
                                  struct timespec timeout,
                                  iox_cond_t* const conditionArray,
                                  const uint64_t conditionArrayCapacity,
                                  uint64_t* missedElements);
 
+/// @brief waits until a condition was triggered
+/// @param[in] self handle to the wait set
+/// @param[in] conditionArray preallocated memory to an array of iox_cond_t in which
+///             the conditions which were triggered can be written to
+/// @param[in] conditionArrayCapacity the capacity of the preallocated conditionArray
+/// @param[in] missedElements if the conditionArray has insufficient size the number of missed elements
+///             which could not be written into the array are stored here
+/// @return number of elements which were written into the conditionArray
 uint64_t iox_wait_set_wait(iox_wait_set_t const self,
                            iox_cond_t* const conditionArray,
                            const uint64_t conditionArrayCapacity,
