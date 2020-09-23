3f89da20900bf1c765911b2a21592ab5bcc99a39
Hintz Martin (CC-AD/ESW1) <martin.hintz@de.bosch.com>
2020-09-23 14:28:12 +0200

    iox-#298: Add more tests
    Signed-off-by: Hintz Martin (CC-AD/ESW1) <martin.hintz@de.bosch.com>

--- iceoryx_utils/test/moduletests/test_cxx_unique_ptr.cpp
+++ iceoryx_utils/test/moduletests/test_cxx_unique_ptr.cpp
@@ -19,11 +19,15 @@
 
 using namespace ::testing;
 
+static constexpr double_t X_POS = 0.0;
+static constexpr double_t Y_POS = 1.1;
+static constexpr double_t Z_POS = 2.2;
+
 struct Position
 {
-    double_t x = 0.0;
-    double_t y = 0.0;
-    double_t z = 0.0;
+    double_t x{X_POS};
+    double_t y{Y_POS};
+    double_t z{Z_POS};
 };
 
 class UniquePtrTest : public Test
@@ -53,46 +57,97 @@ class UniquePtrTest : public Test
 
 TEST_F(UniquePtrTest, CtorWithNullptrSetsPtrToNull)
 {
-    auto ptr = iox::cxx::unique_ptr<Position>(nullptr);
-    EXPECT_EQ(ptr.get(), nullptr);
+    auto sut = iox::cxx::unique_ptr<Position>(nullptr);
+    EXPECT_FALSE(sut);
+    EXPECT_EQ(sut.get(), nullptr);
 }
 
 TEST_F(UniquePtrTest, CtorWithOnlyDeleterSetsPtrToNullAndDoesntCallDeleter)
 {
     {
-        auto ptr = iox::cxx::unique_ptr<Position>(deleter);
-        EXPECT_EQ(ptr.get(), nullptr);
+        auto sut = iox::cxx::unique_ptr<Position>(deleter);
+        EXPECT_FALSE(sut);
+        EXPECT_EQ(sut.get(), nullptr);
     }
 
     EXPECT_FALSE(m_deleterCalled);
 }
 
-
 TEST_F(UniquePtrTest, CtorWithObjectPtrAndDeleterSetsPtrToObjectAndCallsDeleter)
 {
     {
         auto object = new Position();
-        auto ptr = iox::cxx::unique_ptr<Position>(object, deleter);
-        EXPECT_EQ(ptr.get(), object);
+        auto sut = iox::cxx::unique_ptr<Position>(object, deleter);
+        EXPECT_TRUE(sut);
+        EXPECT_EQ(sut.get(), object);
     }
 
     EXPECT_TRUE(m_deleterCalled);
 }
 
-TEST_F(UniquePtrTest, DeleterIsProperlySet)
+TEST_F(UniquePtrTest, CtorWithObjectPtrToNullAndDeleterSetsPtrToObjectAndDoesntCallsDeleter)
 {
+    {
+        Position* object = nullptr;
+        auto sut = iox::cxx::unique_ptr<Position>(object, deleter);
+        EXPECT_FALSE(sut);
+        EXPECT_EQ(sut.get(), object);
+    }
+
+    EXPECT_FALSE(m_deleterCalled);
 }
 
-TEST_F(UniquePtrTest, DeleterNotCalledOnReleasedPointers)
+// TEST_F(UniquePtrTest, CtorWithObjectPtrToNullAndDeleterSetsPtrToObjectAndDoesntCallsDeleter2)
+// {
+//     {
+//         Position* object = nullptr;
+//         auto sut = iox::cxx::unique_ptr<Position>(object, nullptr);
+//         EXPECT_EQ(sut.get(), object);
+//     }
+
+//     EXPECT_FALSE(m_deleterCalled);
+// }
+
+TEST_F(UniquePtrTest, AccessUnderlyingObject)
 {
+    auto object = new Position();
+    auto sut = iox::cxx::unique_ptr<Position>(object, deleter);
+
+    EXPECT_EQ(sut->x, X_POS);
 }
 
-TEST_F(UniquePtrTest, DeleterNotCalledOnNullptrs)
+TEST_F(UniquePtrTest, ReleaseAnObject)
 {
+    auto object = new Position();
+    auto sut = iox::cxx::unique_ptr<Position>(object, deleter);
+
+    EXPECT_EQ(sut.release(), object);
+    EXPECT_FALSE(sut);
 }
 
-TEST_F(UniquePtrTest, CanResetToNullptr)
+TEST_F(UniquePtrTest, ReleaseNullObject)
 {
+    Position* object = nullptr;
+    auto sut = iox::cxx::unique_ptr<Position>(object, deleter);
+
+    EXPECT_EQ(sut.release(), object);
+    EXPECT_FALSE(sut);
+}
+
+TEST_F(UniquePtrTest, ReleaseNullObject1)
+{
+    auto sut = iox::cxx::unique_ptr<Position>(nullptr);
+
+    EXPECT_EQ(sut.release(), nullptr);
+    EXPECT_FALSE(sut);
+}
+
+TEST_F(UniquePtrTest, ReleaseNullObject2)
+{
+    auto sut = iox::cxx::unique_ptr<Position>(deleter);
+
+    EXPECT_EQ(sut.release(), nullptr);
+    EXPECT_FALSE(sut);
 }
 
 TEST_F(UniquePtrTest, CanResetToAnExistingRawPtr)
