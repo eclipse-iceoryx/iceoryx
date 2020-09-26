01cc53d8a5819bb51f19ea4ed2f9191e9c35c2aa
Ithier Jeff (CC-AD/EYF1) <jeff.ithier@de.bosch.com>
2020-09-26 23:02:07 +0200

    iox-#252 Move subscriber handling logic to own thread and set up guard condition to shutdown gracefully.
    Signed-off-by: Ithier Jeff (CC-AD/EYF1) <jeff.ithier@de.bosch.com>

--- iceoryx_examples/icedelivery/iox_subscriber_typed_modern.cpp
+++ iceoryx_examples/icedelivery/iox_subscriber_typed_modern.cpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "iceoryx_posh/popo/modern_api/typed_subscriber.hpp"
+#include "iceoryx_posh/popo/guard_condition.hpp"
 #include "iceoryx_posh/popo/wait_set.hpp"
 #include "iceoryx_posh/runtime/posh_runtime.hpp"
 #include "topic_data.hpp"
@@ -22,11 +23,43 @@
 #include <iostream>
 
 bool killswitch = false;
+iox::popo::GuardCondition shutdownGuard;
 
 static void sigHandler(int f_sig [[gnu::unused]])
 {
     // caught SIGINT, now exit gracefully
     killswitch = true;
+    shutdownGuard.trigger(); // unblock waitsets
+}
+
+void handler(iox::popo::WaitSet& waitSet)
+{
+    // run until interrupted
+    while(!killswitch)
+    {
+        auto triggeredConditions = waitSet.wait();
+        for(auto& condition : triggeredConditions)
+        {
+            auto subscriber = dynamic_cast<iox::popo::TypedSubscriber<Position>*>(condition);
+            if(subscriber)
+            {
+                // new data received on an attached subscriber
+
+                subscriber->receive().and_then([](iox::cxx::optional<iox::popo::Sample<const Position>>& maybePosition){
+                    if(maybePosition.has_value())
+                    {
+                        auto& position = maybePosition.value();
+                        std::cout << "Got value: (" << position->x << ", " << position->y << ", " << position->z << ")" << std::endl;
+                    }
+                });
+            }
+            else
+            {
+                // shutdown guard has triggered
+                std::cout << "Shutdown Guard triggered. Shutting down." << std::endl;
+            }
+        }
+    }
 }
 
 int main()
@@ -44,26 +77,13 @@ int main()
     // set up waitset
     iox::popo::WaitSet waitSet{};
     waitSet.attachCondition(typedSubscriber);
+    waitSet.attachCondition(shutdownGuard);
 
-    // run until interrupted
-    while(!killswitch)
-    {
-        auto triggeredConditions = waitSet.wait();
-        for(auto& condition : triggeredConditions)
-        {
-            /// @todo How to ensure the condition is a subscriber ? What about guard conditions?
-            auto subscriber = dynamic_cast<iox::popo::TypedSubscriber<Position>*>(condition);
-            subscriber->receive().and_then([](iox::cxx::optional<iox::popo::Sample<const Position>>& maybePosition){
-                if(maybePosition.has_value())
-                {
-                    auto& position = maybePosition.value();
-                    std::cout << "Got value: (" << position->x << ", " << position->y << ", " << position->z << ")" << std::endl;
-                }
-            });
+    // delegate handling of received data to another thread
+    std::thread handlerThread(handler, std::ref(waitSet));
+    handlerThread.join();
 
-        }
-    }
-
+    // clean up
     waitSet.detachAllConditions();
 
     return (EXIT_SUCCESS);
