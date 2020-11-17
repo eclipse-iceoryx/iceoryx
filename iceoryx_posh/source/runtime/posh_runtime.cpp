1df59e97885787aff3430acc361570f97fa9e182
Mathias Kraus <mathias.kraus@apex.ai>
2020-11-17 19:56:57 +0100

    iox-#260 remove std::string from RouDiEnvironment and from the public PoshRuntime API and fixed a bug in the runtime C binding
    Signed-off-by: Mathias Kraus <mathias.kraus@apex.ai>

--- iceoryx_posh/source/runtime/posh_runtime.cpp
+++ iceoryx_posh/source/runtime/posh_runtime.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 by Robert Bosch GmbH. All rights reserved.
+// Copyright (c) 2019, 2020 by Robert Bosch GmbH, Apex.AI Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -29,22 +29,29 @@ namespace iox
 {
 namespace runtime
 {
-std::function<PoshRuntime&(const std::string& name)> PoshRuntime::s_runtimeFactory = PoshRuntime::defaultRuntimeFactory;
+std::function<PoshRuntime&(const ProcessName_t& name)> PoshRuntime::s_runtimeFactory =
+    PoshRuntime::defaultRuntimeFactory;
 
 
-PoshRuntime& PoshRuntime::defaultRuntimeFactory(const std::string& name) noexcept
+PoshRuntime& PoshRuntime::defaultRuntimeFactory(const ProcessName_t& name) noexcept
 {
     static PoshRuntime instance(name);
     return instance;
 }
 
 // singleton access
-PoshRuntime& PoshRuntime::getInstance(const std::string& name) noexcept
+PoshRuntime& PoshRuntime::getInstance(const ProcessName_t& name) noexcept
 {
     return PoshRuntime::s_runtimeFactory(name);
 }
 
-PoshRuntime::PoshRuntime(const std::string& name, const bool doMapSharedMemoryIntoThread) noexcept
+ProcessName_t& PoshRuntime::defaultRuntimeInstanceName() noexcept
+{
+    static ProcessName_t defaultInstanceName = "dummy";
+    return defaultInstanceName;
+}
+
+PoshRuntime::PoshRuntime(const ProcessName_t& name, const bool doMapSharedMemoryIntoThread) noexcept
     : m_appName(verifyInstanceName(name))
     , m_MqInterface(MQ_ROUDI_NAME, name, PROCESS_WAITING_FOR_ROUDI_TIMEOUT)
     , m_ShmInterface(doMapSharedMemoryIntoThread,
@@ -66,34 +73,29 @@ PoshRuntime::~PoshRuntime() noexcept
 }
 
 
-const std::string& PoshRuntime::verifyInstanceName(const std::string& name) noexcept
+const ProcessName_t& PoshRuntime::verifyInstanceName(const ProcessName_t& name) noexcept
 {
     if (name.empty())
     {
         LogError() << "Cannot initialize runtime. Application name must not be empty!";
         std::terminate();
     }
-    else if (name.compare(DEFAULT_RUNTIME_INSTANCE_NAME) == 0)
+    else if (name.compare(defaultRuntimeInstanceName()) == 0)
     {
         LogError() << "Cannot initialize runtime. Application name has not been specified!";
         std::terminate();
     }
-    else if (name.front() != '/')
+    else if (name.c_str()[0] != '/')
     {
         LogError() << "Cannot initialize runtime. Application name " << name
                    << " does not have the required leading slash '/'";
-        std::terminate();
-    }
-    else if (name.length() > MAX_PROCESS_NAME_LENGTH)
-    {
-        LogError() << "Application name has more than 100 characters, including null termination!";
         std::terminate();
     }
 
     return name;
 }
 
-std::string PoshRuntime::getInstanceName() const noexcept
+ProcessName_t PoshRuntime::getInstanceName() const noexcept
 {
     return m_appName;
 }
