2f2ed6984ad734ebbb91df4ab7449ace76f00da3
Christian Eltzschig <me@elchris.org>
2020-11-19 12:28:24 +0100

    iox-#341 sync example implemented
    Signed-off-by: Christian Eltzschig <me@elchris.org>

--- iceoryx_examples/waitset/ice_waitset_sync.cpp
+++ iceoryx_examples/waitset/ice_waitset_sync.cpp
@@ -12,6 +12,75 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "iceoryx_posh/popo/guard_condition.hpp"
+#include "iceoryx_posh/popo/modern_api/typed_subscriber.hpp"
+#include "iceoryx_posh/popo/wait_set.hpp"
+#include "iceoryx_posh/runtime/posh_runtime.hpp"
+#include "topic_data.hpp"
+
+#include <chrono>
+#include <csignal>
+#include <iostream>
+
+iox::popo::GuardCondition shutdownGuard;
+
+static void sigHandler(int f_sig [[gnu::unused]])
+{
+    shutdownGuard.trigger();
+}
+
+void waitForNextActivation(iox::popo::GuardCondition*)
+{
+    std::cout << "activation callback\n";
+}
+
+void receiving()
+{
+    iox::runtime::PoshRuntime::getInstance("/iox-ex-subscriber-waitset");
+
+    iox::popo::WaitSet waitset;
+
+    iox::popo::GuardCondition waitForNextActivationTrigger;
+    waitForNextActivationTrigger.attachToWaitset(waitset, 0, waitForNextActivation);
+
+    shutdownGuard.attachToWaitset(waitset);
+
+    std::thread t1([&] {
+        while (true)
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            waitForNextActivationTrigger.trigger();
+        }
+    });
+
+    while (true)
+    {
+        auto triggeredConditions = waitset.wait();
+
+        for (auto& condition : triggeredConditions)
+        {
+            if (condition.doesOriginateFrom(&shutdownGuard))
+            {
+                return;
+            }
+            else
+            {
+                condition();
+            }
+        }
+
+        std::cout << std::endl;
+    }
+
+    t1.join();
+}
+
 int main()
 {
+    signal(SIGINT, sigHandler);
+
+    std::thread rx(receiving);
+    rx.join();
+
+    return (EXIT_SUCCESS);
 }
