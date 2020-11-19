fd26f516f3ba1f0a1a189ad58aa2dd982d1d2c53
Christian Eltzschig <me@elchris.org>
2020-11-19 12:09:37 +0100

    iox-#341 implemented gateway example
    Signed-off-by: Christian Eltzschig <me@elchris.org>

---
+++ iceoryx_examples/waitset/ice_waitset_gateway.cpp
@@ -0,0 +1,91 @@
+// Copyright (c) 2020 by Apex.AI Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "iceoryx_posh/popo/guard_condition.hpp"
+#include "iceoryx_posh/popo/modern_api/untyped_subscriber.hpp"
+#include "iceoryx_posh/popo/wait_set.hpp"
+#include "iceoryx_posh/runtime/posh_runtime.hpp"
+#include "topic_data.hpp"
+
+#include <chrono>
+#include <csignal>
+#include <iostream>
+
+iox::popo::GuardCondition shutdownGuard;
+using Subscriber = iox::popo::UntypedSubscriber;
+
+static void sigHandler(int f_sig [[gnu::unused]])
+{
+    shutdownGuard.trigger();
+}
+
+void subscriberCallback(iox::popo::UntypedSubscriber* const subscriber)
+{
+    subscriber->take().and_then([&](iox::popo::Sample<const void>& sample) {
+        const CounterTopic* data = reinterpret_cast<const CounterTopic*>(sample.get());
+        std::cout << "subscriber: " << std::hex << subscriber << " received: " << std::dec << data->counter
+                  << std::endl;
+    });
+}
+
+void receiving()
+{
+    iox::runtime::PoshRuntime::getInstance("/iox-ex-subscriber-waitset");
+    iox::popo::WaitSet waitset;
+
+    iox::cxx::vector<iox::popo::UntypedSubscriber, 4> subscriberVector;
+
+    for (auto i = 0; i < 2; ++i)
+    {
+        subscriberVector.emplace_back(iox::capro::ServiceDescription{"Radar", "FrontLeft", "Counter"});
+        auto& subscriber = subscriberVector.back();
+
+        subscriber.subscribe();
+        subscriber.attachToWaitset(waitset, iox::popo::SubscriberEvent::HAS_NEW_SAMPLES, 1, subscriberCallback);
+    }
+
+
+    shutdownGuard.attachToWaitset(waitset);
+
+    while (true)
+    {
+        auto triggeredConditions = waitset.wait();
+
+        for (auto& condition : triggeredConditions)
+        {
+            if (condition.doesOriginateFrom(&shutdownGuard))
+            {
+                for (auto& s : subscriberVector)
+                {
+                    s.unsubscribe();
+                }
+                return;
+            }
+            else
+            {
+                condition();
+            }
+        }
+    }
+}
+
+int main()
+{
+    signal(SIGINT, sigHandler);
+
+    std::thread rx(receiving);
+    rx.join();
+
+    return (EXIT_SUCCESS);
+}
