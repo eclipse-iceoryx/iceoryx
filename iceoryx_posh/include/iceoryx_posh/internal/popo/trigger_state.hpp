c3e87d935346b35b4e54dc0ebd074c8c67475d3f
Christian Eltzschig <me@elchris.org>
2020-11-20 15:33:56 +0100

    iox-#341 documented trigger state
    Signed-off-by: Christian Eltzschig <me@elchris.org>

--- iceoryx_posh/include/iceoryx_posh/internal/popo/trigger_state.hpp
+++ iceoryx_posh/include/iceoryx_posh/internal/popo/trigger_state.hpp
@@ -25,6 +25,8 @@ namespace iox
 {
 namespace popo
 {
+/// @brief TriggerState holds the state of a trigger and is the base class
+///        for trigger.
 class TriggerState
 {
   public:
@@ -32,22 +34,42 @@ class TriggerState
     template <typename T>
     using Callback = void (*)(T* const);
 
+    /// @brief constructs an empty TriggerState
     TriggerState() = default;
 
+    /// @brief constructs a TriggerState object
+    /// @param[in] origin pointer to the origin of the TriggerState
+    /// @param[in] triggerId id of the trigger
+    /// @param[in] callback the callback of the trigger
     template <typename T>
     TriggerState(T* const origin, const uint64_t triggerId, const Callback<T> callback) noexcept;
 
+    /// @brief returns the trigger id
+    /// @return the empty TriggerState always returns INVALID_TRIGGER_ID, otherwise the actual triggerId is returned
+    /// which can also be INVALID_TRIGGER_ID
     uint64_t getTriggerId() const noexcept;
 
+    /// @brief confirms the origin
+    /// @param[in] origin the possible origin
+    /// @return true if the address is equal to the origin, otherwise false. The empty TriggerState returns always
+    /// false.
     template <typename T>
     bool doesOriginateFrom(T* const origin) const noexcept;
 
+    /// @brief returns the pointer to the origin.
+    /// @return If T equals the origin type and the TriggerState is not empty it returns the pointer to the origin.
+    /// Otherwise it returns nullptr. If the type is wrong it prints an additional FATAL error message.
     template <typename T>
     T* getOrigin() noexcept;
 
+    /// @brief returns the pointer to the origin.
+    /// @return If T equals the origin type and the TriggerState is not empty it returns the pointer to the origin.
+    /// Otherwise it returns nullptr. If the type is wrong it prints an additional FATAL error message.
     template <typename T>
     const T* getOrigin() const noexcept;
 
+    /// @brief If a callback is set it executes the callback.
+    /// @return true if the callback was called, otherwise false
     bool operator()() const noexcept;
 
   protected:
@@ -55,7 +77,7 @@ class TriggerState
     uint64_t m_originTypeHash = 0U;
     uint64_t m_triggerId = INVALID_TRIGGER_ID;
 
-    Callback<void> m_callbackPtr;
+    Callback<void> m_callbackPtr = nullptr;
     cxx::function_ref<void(void* const, Callback<void>)> m_callback;
 };
 
