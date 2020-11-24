fc9618b85e9fb092cefc0b06aae8389ff24aab93
Christian Eltzschig <me@elchris.org>
2020-11-24 14:22:56 +0100

    iox-#341 added documentation to method callback
    Signed-off-by: Christian Eltzschig <me@elchris.org>

--- iceoryx_utils/include/iceoryx_utils/cxx/method_callback.hpp
+++ iceoryx_utils/include/iceoryx_utils/cxx/method_callback.hpp
@@ -39,25 +39,52 @@ class ConstMethodCallback
 {
   public:
     ConstMethodCallback() = default;
+    ConstMethodCallback(const ConstMethodCallback& rhs) = default;
+    ConstMethodCallback& operator=(const ConstMethodCallback& rhs) = default;
+    ~ConstMethodCallback() = default;
+
+    /// @brief Constructs a ConstMethodCallback from a pointer to a specific class
+    ///        and a pointer to a method of that class.
+    /// @param[in] classPtr class pointer
+    /// @param[in] methodPtr pointer to a const method
     template <typename ClassType>
     ConstMethodCallback(ClassType* const classPtr, ReturnValue (ClassType::*methodPtr)(Args...) const) noexcept;
 
-    ConstMethodCallback(const ConstMethodCallback& rhs) = default;
-    ConstMethodCallback& operator=(const ConstMethodCallback& rhs) = default;
-
+    /// @brief Move constructor
+    /// @param[in] rhs move origin
     ConstMethodCallback(ConstMethodCallback&& rhs) noexcept;
+
+    /// @brief Move assignment operator
+    /// @param[in] rhs move origin
+    /// @return reference to this
     ConstMethodCallback& operator=(ConstMethodCallback&& rhs) noexcept;
 
-    ~ConstMethodCallback() = default;
-
+    /// @brief Calls the method if the ConstMethodCallback is valid, otherwise it
+    ///         will return MethodCallbackError::UNABLE_TO_CALL_METHOD_ON_NULLPTR_CLASS_PTR
+    /// @param[in] args... arguments which will be perfectly forwarded to the method
+    /// @return If ConstMethodCallback is valid the return value of the method, otherwise
+    ///         an error.
     expected<ReturnValue, MethodCallbackError> operator()(Args&&... args) const noexcept;
 
+    /// @brief Comparision operator. Two ConstMethodCallbacks are equal if they have the
+    ///        same class pointer and method pointer
     bool operator==(const ConstMethodCallback& rhs) const noexcept;
+
+    /// @brief Inequality operator. Two ConstMethodCallback are not equal if they have
+    ///        different class or method pointer
     bool operator!=(const ConstMethodCallback& rhs) const noexcept;
 
+    /// @brief Verifies if the ConstMethodCallback is valid.
+    /// @return true if classPtr != nullptr otherwise false
     explicit operator bool() const noexcept;
+
+    /// @brief Verifies if the ConstMethodCallback is valid.
+    /// @return true if classPtr != nullptr otherwise false
     bool isValid() const noexcept;
 
+    /// @brief Sets a new classPtr. The callback will then call the same method
+    ///        on a different class.
+    /// @param[in] classPtr class pointer
     template <typename ClassType>
     void setClassPtr(ClassType* const classPtr) noexcept;
 
@@ -72,27 +99,54 @@ class MethodCallback
 {
   public:
     MethodCallback() = default;
+    MethodCallback(const MethodCallback& rhs) = default;
+    MethodCallback& operator=(const MethodCallback& rhs) = default;
+    ~MethodCallback() = default;
+
+    /// @brief Constructs a MethodCallback from a pointer to a specific class
+    ///        and a pointer to a method of that class.
+    /// @param[in] classPtr class pointer
+    /// @param[in] methodPtr pointer to a method
     template <typename ClassType>
     MethodCallback(ClassType* const classPtr, ReturnValue (ClassType::*methodPtr)(Args...)) noexcept;
 
-    MethodCallback(const MethodCallback& rhs) = default;
-    MethodCallback& operator=(const MethodCallback& rhs) = default;
-
+    /// @brief Move constructor
+    /// @param[in] rhs move origin
     MethodCallback(MethodCallback&& rhs) noexcept;
+
+    /// @brief Move assignment operator
+    /// @param[in] rhs move origin
+    /// @return reference to this
     MethodCallback& operator=(MethodCallback&& rhs) noexcept;
 
-    ~MethodCallback() = default;
-
+    /// @brief Calls the method if the MethodCallback is valid, otherwise it
+    ///         will return MethodCallbackError::UNABLE_TO_CALL_METHOD_ON_NULLPTR_CLASS_PTR
+    /// @param[in] args... arguments which will be perfectly forwarded to the method
+    /// @return If MethodCallback is valid the return value of the method, otherwise
+    ///         an error.
     expected<ReturnValue, MethodCallbackError> operator()(Args&&... args) noexcept;
+
+    /// @brief Comparision operator. Two MethodCallbacks are equal if they have the
+    ///        same class pointer and method pointer
     bool operator==(const MethodCallback& rhs) const noexcept;
+
+    /// @brief Inequality operator. Two MethodCallbacks are not equal if they have
+    ///        different class or method pointer
     bool operator!=(const MethodCallback& rhs) const noexcept;
 
+    /// @brief Verifies if the MethodCallback is valid.
+    /// @return true if classPtr != nullptr otherwise false
     explicit operator bool() const noexcept;
+
+    /// @brief Verifies if the MethodCallback is valid.
+    /// @return true if classPtr != nullptr otherwise false
     bool isValid() const noexcept;
 
+    /// @brief Sets a new classPtr. The callback will then call the same method
+    ///        on a different class.
+    /// @param[in] classPtr class pointer
     template <typename ClassType>
     void setClassPtr(ClassType* const classPtr) noexcept;
-
 
   private:
     void* m_classPtr{nullptr};
