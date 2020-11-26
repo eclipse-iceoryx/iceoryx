d47cbba4809413b92c6c6675a5532dd46145e3f4
Christian Eltzschig <me@elchris.org>
2020-11-26 19:13:39 +0100

    iox-#341 c gateway example identical to the c++ one
    Signed-off-by: Christian Eltzschig <me@elchris.org>

--- iceoryx_examples/waitset_on_c/ice_c_waitset_gateway.c
+++ iceoryx_examples/waitset_on_c/ice_c_waitset_gateway.c
@@ -26,13 +26,10 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-#define NUMBER_OF_CONDITIONS 2
+#define NUMBER_OF_TRIGGER 2
 
 iox_user_trigger_storage_t shutdowGuardStorage;
 iox_user_trigger_t shutdownGuard;
-
-iox_ws_storage_t waitSetStorage;
-iox_ws_t waitSet;
 
 iox_sub_t subscriber;
 
@@ -43,63 +40,85 @@ static void sigHandler(int signalValue)
     iox_user_trigger_trigger(shutdownGuard);
 }
 
+// The callback of the trigger. Every callback must have an argument which is
+// a pointer to the origin of the Trigger. In our case the trigger origin is
+// an iox_sub_t.
 void subscriberCallback(iox_sub_t const subscriber)
 {
     const void* chunk;
-    iox_sub_get_chunk(subscriber, &chunk);
-    printf("subscriber: %p received %u\n", subscriber, ((struct CounterTopic*)chunk)->counter);
+    if (iox_sub_get_chunk(subscriber, &chunk))
+    {
+        printf("subscriber: %p received %u\n", subscriber, ((struct CounterTopic*)chunk)->counter);
 
-    iox_sub_release_chunk(subscriber, chunk);
+        iox_sub_release_chunk(subscriber, chunk);
+    }
 }
 
 int main()
 {
     iox_runtime_register("/iox-c-ex-waitset-gateway");
 
-    waitSet = iox_ws_init(&waitSetStorage);
+    iox_ws_storage_t waitSetStorage;
+    iox_ws_t waitSet = iox_ws_init(&waitSetStorage);
     shutdownGuard = iox_user_trigger_init(&shutdowGuardStorage);
 
+    // attach shutdownGuard with no callback to handle CTRL+C
     iox_user_trigger_attach_to_ws(shutdownGuard, waitSet, 0, NULL);
 
     //// register signal after guard condition since we are using it in the handler
     signal(SIGINT, sigHandler);
 
+    // array where the subscriber are stored
+    iox_sub_storage_t subscriberStorage[2];
+
+    // create subscriber and subscribe them to our service
     uint64_t historyRequest = 1U;
-    iox_sub_storage_t subscriberStorage;
+    for (uint64_t i = 0U; i < 2U; ++i)
+    {
+        subscriber = iox_sub_init(&(subscriberStorage[i]), "Radar", "FrontLeft", "Counter", historyRequest);
 
-    subscriber = iox_sub_init(&subscriberStorage, "Radar", "FrontLeft", "Counter", historyRequest);
-    iox_sub_subscribe(subscriber, 256);
-    iox_sub_attach_to_ws(subscriber, waitSet, SubscriberEvent_HAS_NEW_SAMPLES, 123, subscriberCallback);
+        iox_sub_subscribe(subscriber, 256);
+        iox_sub_attach_to_ws(subscriber, waitSet, SubscriberEvent_HAS_NEW_SAMPLES, 1, subscriberCallback);
+    }
 
-    iox_trigger_state_storage_t triggerArray[NUMBER_OF_CONDITIONS];
+
     uint64_t missedElements = 0U;
     uint64_t numberOfTriggeredConditions = 0U;
 
+    // array where all trigger from iox_ws_wait will be stored
+    iox_trigger_state_storage_t triggerArray[NUMBER_OF_TRIGGER];
+
+    // event loop
     while (true)
     {
-        // wait until an event has occurred
         numberOfTriggeredConditions =
-            iox_ws_wait(waitSet, (iox_trigger_state_t)triggerArray, NUMBER_OF_CONDITIONS, &missedElements);
+            iox_ws_wait(waitSet, (iox_trigger_state_t)triggerArray, NUMBER_OF_TRIGGER, &missedElements);
 
         for (uint64_t i = 0U; i < numberOfTriggeredConditions; ++i)
         {
             if (iox_trigger_state_does_originate_from_user_trigger((iox_trigger_state_t) & (triggerArray[i]),
                                                                    shutdownGuard))
             {
+                // CTRL+c was pressed -> exit
                 return 0;
             }
             else
             {
-                iox_trigger_state_call((iox_trigger_state_t) & (triggerArray[0]));
+                // call the callback which was assigned to the trigger
+                iox_trigger_state_call((iox_trigger_state_t) & (triggerArray[i]));
             }
         }
     }
 
-    iox_sub_unsubscribe(subscriber);
+    // cleanup all resources
+    for (uint64_t i = 0U; i < 2U; ++i)
+    {
+        iox_sub_unsubscribe((iox_sub_t) & (subscriberStorage[i]));
+        iox_sub_deinit((iox_sub_t) & (subscriberStorage[i]));
+    }
 
     iox_ws_deinit(waitSet);
     iox_user_trigger_deinit(shutdownGuard);
-    iox_sub_deinit(subscriber);
 
 
     return 0;
