3992699cb4b3f9a725146d4bd9bd33fd7463e424
Simon Hoinkis <simon.hoinkis@apex.ai>
2020-12-08 19:46:28 +0100

    iox-#91 Refactor to remove GenericRAII usage in RouDiComponents
    Signed-off-by: Simon Hoinkis <simon.hoinkis@apex.ai>

--- iceoryx_posh/include/iceoryx_posh/roudi/iceoryx_roudi_components.hpp
+++ iceoryx_posh/include/iceoryx_posh/roudi/iceoryx_roudi_components.hpp
@@ -39,25 +39,11 @@ struct IceOryxRouDiComponents
     /// @brief Handles MemoryProvider and MemoryBlocks
     IceOryxRouDiMemoryManager m_rouDiMemoryManager;
 
-    /// @todo Make this an object
-    /// @brief Cleanup resources and initalize memory
-    cxx::GenericRAII m_initalizer{
-        [this]() {
-            // this temporary object will create a roudi mqueue and close it immediatelly
-            // if there was an outdated roudi message queue, it will be cleaned up
-            // if there is an outdated mqueue, the start of the apps will be terminated
-            runtime::MqBase::cleanupOutdatedMessageQueue(MQ_ROUDI_NAME);
-
-            m_rouDiMemoryManager.createAndAnnounceMemory().or_else([](RouDiMemoryManagerError error) {
-                LogFatal() << "Could not create SharedMemory! Error: " << static_cast<uint64_t>(error);
-                errorHandler(Error::kROUDI_COMPONENTS__SHARED_MEMORY_UNAVAILABLE, nullptr, iox::ErrorLevel::FATAL);
-            });
-            return true;
-        },
-        []() {}};
+    /// @brief Prepare the memory and clean up old ressources
+    IceOryxRouDiMemoryManager* initRouDiMemoryManager() noexcept;
 
     /// @brief Handles the ports in shared memory
-    PortManager m_portManager{&m_rouDiMemoryManager};
+    PortManager m_portManager{initRouDiMemoryManager()};
 };
 } // namespace roudi
 } // namespace iox
