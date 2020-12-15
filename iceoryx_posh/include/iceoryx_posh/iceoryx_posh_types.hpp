a8d623de28e4b37999b2a3d1b36e46c1729bf49e
Mathias Kraus <mathias.kraus@apex.ai>
2020-12-15 19:17:18 +0100

    iox-#260 rename ShmNameString alias to ShmName_t
    Signed-off-by: Mathias Kraus <mathias.kraus@apex.ai>

--- iceoryx_posh/include/iceoryx_posh/iceoryx_posh_types.hpp
+++ iceoryx_posh/include/iceoryx_posh/iceoryx_posh_types.hpp
@@ -194,6 +194,7 @@ struct DefaultChunkQueueConfig
 using ConfigFilePathString_t = cxx::string<1024>;
 using ProcessName_t = cxx::string<MAX_PROCESS_NAME_LENGTH>;
 using NodeName_t = cxx::string<100>;
+using ShmName_t = cxx::string<100>;
 
 namespace capro
 {
