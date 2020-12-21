1f55d24bea91920910a2e80d04f2ff5164561fef
Simon Hoinkis <simon.hoinkis@apex.ai>
2020-12-21 17:20:11 +0100

    iox-#283 Clean-up includes in icedelivery
    Signed-off-by: Simon Hoinkis <simon.hoinkis@apex.ai>

--- iceoryx_examples/icedelivery/iox_publisher_typed.cpp
+++ iceoryx_examples/icedelivery/iox_publisher_typed.cpp
@@ -17,9 +17,7 @@
 #include "iceoryx_posh/popo/typed_publisher.hpp"
 #include "iceoryx_posh/runtime/posh_runtime.hpp"
 
-#include <chrono>
 #include <iostream>
-#include <thread>
 
 bool killswitch = false;
 
--- iceoryx_examples/icedelivery/iox_subscriber_untyped.cpp
+++ iceoryx_examples/icedelivery/iox_subscriber_untyped.cpp
@@ -12,9 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "topic_data.hpp"
+
 #include "iceoryx_posh/popo/subscriber.hpp"
 #include "iceoryx_posh/runtime/posh_runtime.hpp"
-#include "topic_data.hpp"
 
 #include <csignal>
 #include <iostream>
