4c66eed199c30a9f0d609a720ed566b887eec1f1
Dietrich Krönke <dietrich.kroenke@apex.ai>
2021-01-05 13:03:40 +0100

    iox-#468 adapt copyright header
    Signed-off-by: Dietrich Krönke <dietrich.kroenke@apex.ai>

--- tools/iceoryx_build_test.sh
+++ tools/iceoryx_build_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2019-2020 by Robert Bosch GmbH. All rights reserved.
+# Copyright (c) 2019-2020, 2020 by Robert Bosch GmbH, Apex.AI Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
