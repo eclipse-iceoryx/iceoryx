5c11b3cc8d218428c2b29deea0cec24e43ebb61b
Dietrich Krönke <dietrich.kroenke@apex.ai>
2021-01-07 13:28:44 +0100

    iox-#163 refactor fixed size container
    Signed-off-by: Dietrich Krönke <dietrich.kroenke@apex.ai>

--- iceoryx_posh/include/iceoryx_posh/internal/roudi/introspection/fixed_size_container.hpp
+++ iceoryx_posh/include/iceoryx_posh/internal/roudi/introspection/fixed_size_container.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 by Robert Bosch GmbH. All rights reserved.
+// Copyright (c) 2019, 2021 by Robert Bosch GmbH, Apex.AI Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,23 +18,26 @@
 
 #include <cstdint>
 
-// Use: allows allocating a predefined capacity of T objects on the stack
-// and using there pointers outside
-//(pointers stay valid until object is removed from theFixedSizeContainer)
-// therefore we avoid heap allocation but we can still use pointers to objects
-// for efficient update/passing until we exhaust the fixed size container
 
-// note: no bounds checking during access for efficiency (as in STL containers)
-// access to indices < 0 or >= capacity is undefined behaviour (most likely a
-// segmentation fault)
+namespace iox
+{
+namespace roudi
+{
+/// @details allows allocating a predefined capacity of T objects on the stack
+/// and using there pointers outside
+/// (pointers stay valid until object is removed from theFixedSizeContainer)
+/// therefore we avoid heap allocation but we can still use pointers to objects
+/// for efficient update/passing until we exhaust the fixed size container
 
-using Capacity_t = uint64_t;
-
+/// @attention no bounds checking during access for efficiency (as in STL containers)
+/// access to indices < 0 or >= capacity is undefined behaviour (most likely a
+/// segmentation fault)
 template <typename T, uint32_t capacity = 1U>
 class FixedSizeContainer
 {
   public:
     using Index_t = int32_t;
+    using Capacity_t = decltype(capacity);
     static constexpr int32_t NOT_AN_ELEMENT = -1;
 
     FixedSizeContainer()
@@ -42,9 +45,9 @@ class FixedSizeContainer
     {
     }
 
-    // returns index or -1 if element could not be added
-    // a successful add returns an arbitrary index which can be non consecutive
-    // for consecutive adds
+    /// @note returns index or -1 if element could not be added
+    /// a successful add returns an arbitrary index which can be non consecutive
+    /// for consecutive adds
     Index_t add(const T& element)
     {
         auto nextElement = nextFree();
@@ -69,9 +72,8 @@ class FixedSizeContainer
         }
     }
 
-    // note: access can change the underlying object, without modifying valid
-    // flag
-    // if the index is invalid than the behavior is undefined
+    /// @note access can change the underlying object, without modifying valid flag
+    /// if the index is invalid than the behavior is undefined
     T& operator[](Index_t index)
     {
         return m_values[static_cast<Capacity_t>(index)].value;
@@ -79,7 +81,8 @@ class FixedSizeContainer
 
     T* get(Index_t index)
     {
-        return (m_values[static_cast<Capacity_t>(index)].isValid) ? &m_values[static_cast<uint32_t>(index)].value : nullptr;
+        return (m_values[static_cast<Capacity_t>(index)].isValid) ? &m_values[static_cast<uint32_t>(index)].value
+                                                                  : nullptr;
     }
 
     size_t size()
@@ -93,7 +96,8 @@ class FixedSizeContainer
         if (m_size >= capacity)
             return NOT_AN_ELEMENT; // container is full
 
-        for (; m_values[static_cast<Capacity_t>(m_freeIndex)].isValid; m_freeIndex = (m_freeIndex + 1) % static_cast<Index_t>(capacity))
+        for (; m_values[static_cast<Capacity_t>(m_freeIndex)].isValid;
+             m_freeIndex = (m_freeIndex + 1) % static_cast<Index_t>(capacity))
             ;
 
         return m_freeIndex;
@@ -120,5 +124,8 @@ class FixedSizeContainer
 
     iox::cxx::vector<entry_t, capacity> m_values;
 };
+
+} // namespace roudi
+} // namespace iox
 
 #endif // IOX_POSH_ROUDI_INTROSPECTION_FIXED_SIZE_CONTAINER_HPP
