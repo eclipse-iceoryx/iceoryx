7b3bdedcdf68d8c8385cf647c1de6498a87f11a5
Dietrich Krönke <dietrich.kroenke@apex.ai>
2021-01-08 11:09:17 +0100

    iox-#378 rework cmake
    Signed-off-by: Dietrich Krönke <dietrich.kroenke@apex.ai>

--- iceoryx_integration_test/simple_roudi_test.py
+++ iceoryx_integration_test/simple_roudi_test.py
@@ -38,8 +38,6 @@ def generate_test_description():
     print("index: ", ament_index_python.get_package_prefix('iceoryx_integration_test'))
     TEST_PROC_PATH = os.path.join(
         ament_index_python.get_package_prefix('iceoryx_integration_test'),
-        # 'bin/iceoryx_integration_test',
-        # 'iox-roudi'
         'bin/iceoryx_integration_test/',
         'iox-roudi'
     )
@@ -50,33 +48,35 @@ def generate_test_description():
 
     dut_process = launch.actions.ExecuteProcess(
         cmd=[sys.executable, subprocess.run([TEST_PROC_PATH, '-l', 'debug'])],
-        env=proc_env, output='screen'
+        env=proc_env, output='log'
     )
 
     return launch.LaunchDescription([
-        wait_for_roudi(),
         dut_process,
         # Start tests right away - no need to wait for anything
         launch_testing.actions.ReadyToTest(),
     ]), {'dut_process': dut_process}
 
 
-def wait_for_roudi():
-    print('wait_for_roudi')
+
 
 # These tests will run concurrently with the dut process.  After all these tests are done,
 # the launch system will shut down the processes that it started up
 class TestGoodProcess(unittest.TestCase):
 
+    def wait_for_roudi(self, proc_output):
+        print('wait_for_roudi', proc_output)
+
     def test_count_to_four(self, proc_output):
+        print('some printout')
         # This will match stdout from any process.  In this example there is only one process
         # running
         time.sleep(1)
         print('test_count_to_four')
         proc_output.assertWaitFor('RouDi is ready for clients', timeout=10, stream='stdout')
-        # proc_output.assertWaitFor('Loop 2', timeout=10, stream='stdout')
-        # proc_output.assertWaitFor('Loop 3', timeout=10, stream='stdout')
-        # proc_output.assertWaitFor('Loop 4', timeout=10, stream='stdout')
+        proc_output.assertWaitFor('Loop 2', timeout=10, stream='stdout')
+        proc_output.assertWaitFor('Loop 3', timeout=10, stream='stdout')
+        proc_output.assertWaitFor('Loop 4', timeout=10, stream='stdout')
 
 
 @launch_testing.post_shutdown_test()
@@ -87,28 +87,28 @@ class TestProcessOutput(unittest.TestCase):
         print('test_exit_code')
         # Check that all processes in the launch (in this case, there's just one) exit
         # with code 0
-        # launch_testing.asserts.assertExitCodes(proc_info)
+        launch_testing.asserts.assertExitCodes(proc_info)
 
     def test_full_output(self, proc_output, dut_process):
         time.sleep(1)
         print('test_full_output')
         # Using the SequentialStdout context manager asserts that the following stdout
         # happened in the same order that it's checked
-        # with assertSequentialStdout(proc_output, dut_process) as cm:
-        #     cm.assertInStdout('Starting Up')
-        #     for n in range(4):
-        #         cm.assertInStdout('Loop {}'.format(n))
-        #     if os.name != 'nt':
-        #         # On Windows, process termination is always forced
-        #         # and thus the last print in good_proc never makes it.
-        #         cm.assertInStdout('Shutting Down')
+        with assertSequentialStdout(proc_output, dut_process) as cm:
+            cm.assertInStdout('Starting Up')
+            for n in range(4):
+                cm.assertInStdout('Loop {}'.format(n))
+            if os.name != 'nt':
+                # On Windows, process termination is always forced
+                # and thus the last print in good_proc never makes it.
+                cm.assertInStdout('Shutting Down')
 
     def test_out_of_order(self, proc_output, dut_process):
         time.sleep(1)
         print('test_out_of_order')
         # This demonstrates that we notice out-of-order IO
-        # with self.assertRaisesRegex(AssertionError, "'Loop 2' not found"):
-        #     with assertSequentialStdout(proc_output, dut_process) as cm:
-        #         cm.assertInStdout('Loop 1')
-        #         cm.assertInStdout('Loop 3')
-        #         cm.assertInStdout('Loop 2')  # This should raise
+        with self.assertRaisesRegex(AssertionError, "'Loop 2' not found"):
+            with assertSequentialStdout(proc_output, dut_process) as cm:
+                cm.assertInStdout('Loop 1')
+                cm.assertInStdout('Loop 3')
+                cm.assertInStdout('Loop 2')  # This should raise
