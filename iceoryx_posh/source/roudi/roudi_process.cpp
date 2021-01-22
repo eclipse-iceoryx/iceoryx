2ab3c633175adddb3f6aa50605382c9d5349f769
Dietrich Krönke <dietrich.kroenke@apex.ai>
2021-01-22 09:54:35 +0100

    iox-#498 clear up return value in roudi_process
    Signed-off-by: Dietrich Krönke <dietrich.kroenke@apex.ai>

--- iceoryx_posh/source/roudi/roudi_process.cpp
+++ iceoryx_posh/source/roudi/roudi_process.cpp
@@ -271,15 +271,13 @@ bool ProcessManager::isProcessAlive(const RouDiProcess& process) noexcept
     {
         return false;
     }
-    else
+
+    if (checkCommand.hasErrors())
     {
-        if (checkCommand.hasErrors())
-        {
-            evaluateKillError(
-                process, checkCommand.getErrNum(), checkCommand.getErrorString(), ShutdownPolicy::SIG_TERM);
-        }
-        return true;
+        evaluateKillError(process, checkCommand.getErrNum(), checkCommand.getErrorString(), ShutdownPolicy::SIG_TERM);
     }
+
+    return true;
 }
 
 void ProcessManager::evaluateKillError(const RouDiProcess& process,
