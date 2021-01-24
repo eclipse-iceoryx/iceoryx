9fdb6712267b51e1474c663942ec29213cb8a868
Manel Melaouhi <external.Manel.Melaouhi@de.bosch.com>
2021-01-24 18:30:55 +0100

    iox-#496 delete unusful lines of code
    Signed-off-by: Manel Melaouhi <external.Manel.Melaouhi@de.bosch.com>

--- iceoryx_posh/test/moduletests/test_shared_memory_user.cpp
+++ iceoryx_posh/test/moduletests/test_shared_memory_user.cpp
@@ -50,9 +50,7 @@ class shared_memory_user_test : public Test
     virtual void TearDown()
     {
     };
-    const ProcessName_t m_runtimeName{"App"};
     RouDiEnvironment m_roudiEnv{iox::RouDiConfig_t().setDefaults()};
-    PoshRuntime* m_runtime{&iox::runtime::PoshRuntime::initRuntime(m_runtimeName)}; 
 };
 
 TEST_F(shared_memory_user_test, ConstructorShmObjectIsSuccess)
