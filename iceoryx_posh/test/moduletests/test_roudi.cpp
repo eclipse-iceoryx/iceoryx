3c914ec1d03110401bd3879620a37af90cb82c92
Jimmy Belloche <external.jimmy.belloche@de.bosch.com>
2021-02-01 14:12:35 +0100

    iox-#454 improve unit test for class roudi
    Signed-off-by: Jimmy Belloche <external.jimmy.belloche@de.bosch.com>

--- iceoryx_posh/test/moduletests/test_roudi.cpp
+++ iceoryx_posh/test/moduletests/test_roudi.cpp
@@ -33,11 +33,7 @@ namespace iox
 {
 namespace test
 {
-/// @req IOX_SWRS_112, IOX_SWRS_200
-/// @brief Test goal: "This test suite verifies that the BaseClass function is verified"
-/// @pre describe what needs to be done in setup()
-/// @post describe what needs to be done in teardown()
-/// @note name of the Testfixture should match to the Class you want to test
+/// @brief Test goal: Verify base class roudi
 
 class RoudiClassTest : public RouDi
 {
@@ -165,7 +161,7 @@ TEST_F(RouDiBaseClass_test, SendCorrectRegMessagetoRoudiReturnSuccessfullySent)
 TEST_F(RouDiBaseClass_test, SendWrongRegMessagetoRoudiReturnError)
 {
     std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
-        std::unique_ptr<IceOryxRouDiComponents>(new IceOryxRouDiComponents(iox::RouDiConfig_t().setDefaults()));
+        std::unique_ptr<IceOryxRouDiComponents>(new IceOryxRouDiComponents(iox::RouDiConfig_t().optimize()));
     std::unique_ptr<RoudiClassTest> m_roudiApp = std::unique_ptr<RoudiClassTest>(
         new RoudiClassTest(m_roudiComponents->m_rouDiMemoryManager,
                            m_roudiComponents->m_portManager,
@@ -182,48 +178,43 @@ TEST_F(RouDiBaseClass_test, SendWrongRegMessagetoRoudiReturnError)
 
 TEST_F(RouDiBaseClass_test, ProcessMessageChangeCounter)
 {
-    std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
-        std::unique_ptr<IceOryxRouDiComponents>(new IceOryxRouDiComponents(iox::RouDiConfig_t().setDefaults()));
-    std::unique_ptr<RoudiClassTest> m_roudiApp = std::unique_ptr<RoudiClassTest>(
-        new RoudiClassTest(m_roudiComponents->m_rouDiMemoryManager,
-                           m_roudiComponents->m_portManager,
-                           RouDi::RoudiStartupParameters{roudi::MonitoringMode::ON, true}));
+    const iox::ProcessName_t m_runtimeName{"App"};
+    RouDiEnvironment m_roudiEnv{iox::RouDiConfig_t().setDefaults()};
+    PoshRuntime* m_runtime{&iox::runtime::PoshRuntime::initRuntime(m_runtimeName)};
 
     MqMessage message;
-    message << mqMessageTypeToString(MqMessageType::SERVICE_REGISTRY_CHANGE_COUNTER) << "AppName"
-            << "123123";
-    auto cmd = runtime::stringToMqMessageType(message.getElementAtIndex(0).c_str());
-    std::string processName = message.getElementAtIndex(1);
+    MqMessage m_receiveBuffer;
+    message << mqMessageTypeToString(MqMessageType::SERVICE_REGISTRY_CHANGE_COUNTER) << m_runtimeName;
 
-    m_roudiApp->processMessageTest(message, cmd, ProcessName_t(cxx::TruncateToCapacity, processName));
+    const auto successfullySent = m_runtime->sendRequestToRouDi(message, m_receiveBuffer);
 
-    m_roudiApp->shutDownTest();
+    EXPECT_TRUE(m_receiveBuffer.isValid());
+    EXPECT_TRUE(successfullySent);
 }
 
 TEST_F(RouDiBaseClass_test, ProcessMessageCreatePublisher)
 {
-    std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
-        std::unique_ptr<IceOryxRouDiComponents>(new IceOryxRouDiComponents(iox::RouDiConfig_t().setDefaults()));
-    std::unique_ptr<RoudiClassTest> m_roudiApp = std::unique_ptr<RoudiClassTest>(
-        new RoudiClassTest(m_roudiComponents->m_rouDiMemoryManager,
-                           m_roudiComponents->m_portManager,
-                           RouDi::RoudiStartupParameters{roudi::MonitoringMode::ON, true}));
+    const iox::ProcessName_t m_runtimeName{"App"};
+    RouDiEnvironment m_roudiEnv{iox::RouDiConfig_t().setDefaults()};
+    PoshRuntime* m_runtime{&iox::runtime::PoshRuntime::initRuntime(m_runtimeName)};
 
     MqMessage message;
+    MqMessage m_receiveBuffer;
+    iox::popo::PublisherOptions publisherOptions;
+    publisherOptions.historyCapacity = 13U;
     const iox::NodeName_t m_nodeName{"testNode"};
-    message << mqMessageTypeToString(MqMessageType::CREATE_PUBLISHER) << "m_appName"
+
+    message << mqMessageTypeToString(MqMessageType::CREATE_PUBLISHER) << m_runtimeName
             << static_cast<cxx::Serialization>(iox::capro::ServiceDescription(99U, 1U, 20U)).toString()
-            << std::to_string(13U) << m_nodeName
+            << std::to_string(publisherOptions.historyCapacity) << m_nodeName
             << static_cast<cxx::Serialization>(iox::runtime::PortConfigInfo(11U, 22U, 33U)).toString();
-    auto cmd = runtime::stringToMqMessageType(message.getElementAtIndex(0).c_str());
-    std::string processName = message.getElementAtIndex(1);
 
-    m_roudiApp->processMessageTest(message, cmd, ProcessName_t(cxx::TruncateToCapacity, processName));
+    const auto successfullySent = m_runtime->sendRequestToRouDi(message, m_receiveBuffer);
 
-    m_roudiApp->shutDownTest();
+    EXPECT_TRUE(m_receiveBuffer.isValid());
+    EXPECT_TRUE(successfullySent);
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageCreatePublisherError)
 {
     std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
@@ -244,32 +235,30 @@ TEST_F(RouDiBaseClass_test, ProcessMessageCreatePublisherError)
     m_roudiApp->shutDownTest();
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageCreateSubscriber)
 {
-    std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
-        std::unique_ptr<IceOryxRouDiComponents>(new IceOryxRouDiComponents(iox::RouDiConfig_t().setDefaults()));
-    std::unique_ptr<RoudiClassTest> m_roudiApp = std::unique_ptr<RoudiClassTest>(
-        new RoudiClassTest(m_roudiComponents->m_rouDiMemoryManager,
-                           m_roudiComponents->m_portManager,
-                           RouDi::RoudiStartupParameters{roudi::MonitoringMode::ON, true}));
+    const iox::ProcessName_t m_runtimeName{"App"};
+    RouDiEnvironment m_roudiEnv{iox::RouDiConfig_t().setDefaults()};
+    PoshRuntime* m_runtime{&iox::runtime::PoshRuntime::initRuntime(m_runtimeName)};
 
     MqMessage message;
-    message << mqMessageTypeToString(MqMessageType::CREATE_SUBSCRIBER) << "AppName"
-            << "123123"
-            << "456"
-            << "789"
-            << "123"
-            << "abc";
-    auto cmd = runtime::stringToMqMessageType(message.getElementAtIndex(0).c_str());
-    std::string processName = message.getElementAtIndex(1);
+    MqMessage m_receiveBuffer;
+    iox::popo::SubscriberOptions subscriberOptions;
+    subscriberOptions.historyRequest = 13U;
+    subscriberOptions.queueCapacity = 42U;
+    const iox::NodeName_t m_nodeName{"testNode"};
 
-    m_roudiApp->processMessageTest(message, cmd, ProcessName_t(cxx::TruncateToCapacity, processName));
+    message << mqMessageTypeToString(MqMessageType::CREATE_SUBSCRIBER) << m_runtimeName
+            << static_cast<cxx::Serialization>(iox::capro::ServiceDescription(99U, 1U, 20U)).toString()
+            << std::to_string(subscriberOptions.historyRequest) << std::to_string(subscriberOptions.queueCapacity)
+            << m_nodeName << static_cast<cxx::Serialization>(iox::runtime::PortConfigInfo(11U, 22U, 33U)).toString();
 
-    m_roudiApp->shutDownTest();
+    const auto successfullySent = m_runtime->sendRequestToRouDi(message, m_receiveBuffer);
+
+    EXPECT_TRUE(m_receiveBuffer.isValid());
+    EXPECT_TRUE(successfullySent);
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageCreateSubscriberError)
 {
     std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
@@ -290,27 +279,22 @@ TEST_F(RouDiBaseClass_test, ProcessMessageCreateSubscriberError)
     m_roudiApp->shutDownTest();
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageCreateConditionVariable)
 {
-    std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
-        std::unique_ptr<IceOryxRouDiComponents>(new IceOryxRouDiComponents(iox::RouDiConfig_t().setDefaults()));
-    std::unique_ptr<RoudiClassTest> m_roudiApp = std::unique_ptr<RoudiClassTest>(
-        new RoudiClassTest(m_roudiComponents->m_rouDiMemoryManager,
-                           m_roudiComponents->m_portManager,
-                           RouDi::RoudiStartupParameters{roudi::MonitoringMode::ON, true}));
+    const iox::ProcessName_t m_runtimeName{"App"};
+    RouDiEnvironment m_roudiEnv{iox::RouDiConfig_t().setDefaults()};
+    PoshRuntime* m_runtime{&iox::runtime::PoshRuntime::initRuntime(m_runtimeName)};
 
     MqMessage message;
-    message << mqMessageTypeToString(MqMessageType::CREATE_CONDITION_VARIABLE) << "AppName";
-    auto cmd = runtime::stringToMqMessageType(message.getElementAtIndex(0).c_str());
-    std::string processName = message.getElementAtIndex(1);
+    MqMessage m_receiveBuffer;
+    message << mqMessageTypeToString(MqMessageType::CREATE_CONDITION_VARIABLE) << m_runtimeName;
 
-    m_roudiApp->processMessageTest(message, cmd, ProcessName_t(cxx::TruncateToCapacity, processName));
+    const auto successfullySent = m_runtime->sendRequestToRouDi(message, m_receiveBuffer);
 
-    m_roudiApp->shutDownTest();
+    EXPECT_TRUE(m_receiveBuffer.isValid());
+    EXPECT_TRUE(successfullySent);
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageCreateConditionVariableError)
 {
     std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
@@ -331,29 +315,24 @@ TEST_F(RouDiBaseClass_test, ProcessMessageCreateConditionVariableError)
     m_roudiApp->shutDownTest();
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageCreateInterface)
 {
-    std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
-        std::unique_ptr<IceOryxRouDiComponents>(new IceOryxRouDiComponents(iox::RouDiConfig_t().setDefaults()));
-    std::unique_ptr<RoudiClassTest> m_roudiApp = std::unique_ptr<RoudiClassTest>(
-        new RoudiClassTest(m_roudiComponents->m_rouDiMemoryManager,
-                           m_roudiComponents->m_portManager,
-                           RouDi::RoudiStartupParameters{roudi::MonitoringMode::ON, true}));
+    const iox::ProcessName_t m_runtimeName{"App"};
+    RouDiEnvironment m_roudiEnv{iox::RouDiConfig_t().setDefaults()};
+    PoshRuntime* m_runtime{&iox::runtime::PoshRuntime::initRuntime(m_runtimeName)};
+    const iox::NodeName_t m_nodeName{"testNode"};
 
     MqMessage message;
-    message << mqMessageTypeToString(MqMessageType::CREATE_INTERFACE) << "AppName"
-            << "123123"
-            << "456";
-    auto cmd = runtime::stringToMqMessageType(message.getElementAtIndex(0).c_str());
-    std::string processName = message.getElementAtIndex(1);
+    MqMessage m_receiveBuffer;
+    message << mqMessageTypeToString(MqMessageType::CREATE_INTERFACE) << m_runtimeName
+            << static_cast<uint32_t>(iox::capro::Interfaces::INTERNAL) << m_nodeName;
 
-    m_roudiApp->processMessageTest(message, cmd, ProcessName_t(cxx::TruncateToCapacity, processName));
+    const auto successfullySent = m_runtime->sendRequestToRouDi(message, m_receiveBuffer);
 
-    m_roudiApp->shutDownTest();
+    EXPECT_TRUE(m_receiveBuffer.isValid());
+    EXPECT_TRUE(successfullySent);
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageCreateInterfaceError)
 {
     std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
@@ -374,27 +353,22 @@ TEST_F(RouDiBaseClass_test, ProcessMessageCreateInterfaceError)
     m_roudiApp->shutDownTest();
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageCreateApplication)
 {
-    std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
-        std::unique_ptr<IceOryxRouDiComponents>(new IceOryxRouDiComponents(iox::RouDiConfig_t().setDefaults()));
-    std::unique_ptr<RoudiClassTest> m_roudiApp = std::unique_ptr<RoudiClassTest>(
-        new RoudiClassTest(m_roudiComponents->m_rouDiMemoryManager,
-                           m_roudiComponents->m_portManager,
-                           RouDi::RoudiStartupParameters{roudi::MonitoringMode::ON, true}));
+    const iox::ProcessName_t m_runtimeName{"App"};
+    RouDiEnvironment m_roudiEnv{iox::RouDiConfig_t().setDefaults()};
+    PoshRuntime* m_runtime{&iox::runtime::PoshRuntime::initRuntime(m_runtimeName)};
 
     MqMessage message;
-    message << mqMessageTypeToString(MqMessageType::CREATE_APPLICATION) << "AppName";
-    auto cmd = runtime::stringToMqMessageType(message.getElementAtIndex(0).c_str());
-    std::string processName = message.getElementAtIndex(1);
+    MqMessage m_receiveBuffer;
+    message << mqMessageTypeToString(MqMessageType::CREATE_APPLICATION) << m_runtimeName;
 
-    m_roudiApp->processMessageTest(message, cmd, ProcessName_t(cxx::TruncateToCapacity, processName));
+    const auto successfullySent = m_runtime->sendRequestToRouDi(message, m_receiveBuffer);
 
-    m_roudiApp->shutDownTest();
+    EXPECT_TRUE(m_receiveBuffer.isValid());
+    EXPECT_TRUE(successfullySent);
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageCreateApplicationError)
 {
     std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
@@ -415,29 +389,26 @@ TEST_F(RouDiBaseClass_test, ProcessMessageCreateApplicationError)
     m_roudiApp->shutDownTest();
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageCreateNode)
 {
-    std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
-        std::unique_ptr<IceOryxRouDiComponents>(new IceOryxRouDiComponents(iox::RouDiConfig_t().setDefaults()));
-    std::unique_ptr<RoudiClassTest> m_roudiApp = std::unique_ptr<RoudiClassTest>(
-        new RoudiClassTest(m_roudiComponents->m_rouDiMemoryManager,
-                           m_roudiComponents->m_portManager,
-                           RouDi::RoudiStartupParameters{roudi::MonitoringMode::ON, true}));
+    const iox::ProcessName_t m_runtimeName{"App"};
+    RouDiEnvironment m_roudiEnv{iox::RouDiConfig_t().setDefaults()};
+    PoshRuntime* m_runtime{&iox::runtime::PoshRuntime::initRuntime(m_runtimeName)};
+    const iox::NodeName_t m_nodeName{"testNode"};
+    const uint32_t nodeDeviceIdentifier = 1U;
+    iox::runtime::NodeProperty nodeProperty(m_nodeName, nodeDeviceIdentifier);
 
     MqMessage message;
-    message << mqMessageTypeToString(MqMessageType::CREATE_NODE) << "AppName"
-            << "123123"
-            << "456";
-    auto cmd = runtime::stringToMqMessageType(message.getElementAtIndex(0).c_str());
-    std::string processName = message.getElementAtIndex(1);
+    MqMessage m_receiveBuffer;
+    message << mqMessageTypeToString(MqMessageType::CREATE_NODE) << m_runtimeName
+            << static_cast<cxx::Serialization>(nodeProperty).toString();
 
-    m_roudiApp->processMessageTest(message, cmd, ProcessName_t(cxx::TruncateToCapacity, processName));
+    const auto successfullySent = m_runtime->sendRequestToRouDi(message, m_receiveBuffer);
 
-    m_roudiApp->shutDownTest();
+    EXPECT_TRUE(m_receiveBuffer.isValid());
+    EXPECT_TRUE(successfullySent);
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageCreateNodeError)
 {
     std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
@@ -458,28 +429,23 @@ TEST_F(RouDiBaseClass_test, ProcessMessageCreateNodeError)
     m_roudiApp->shutDownTest();
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageFindService)
 {
-    std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
-        std::unique_ptr<IceOryxRouDiComponents>(new IceOryxRouDiComponents(iox::RouDiConfig_t().setDefaults()));
-    std::unique_ptr<RoudiClassTest> m_roudiApp = std::unique_ptr<RoudiClassTest>(
-        new RoudiClassTest(m_roudiComponents->m_rouDiMemoryManager,
-                           m_roudiComponents->m_portManager,
-                           RouDi::RoudiStartupParameters{roudi::MonitoringMode::ON, true}));
+    const iox::ProcessName_t m_runtimeName{"App"};
+    RouDiEnvironment m_roudiEnv{iox::RouDiConfig_t().setDefaults()};
+    PoshRuntime* m_runtime{&iox::runtime::PoshRuntime::initRuntime(m_runtimeName)};
 
     MqMessage message;
-    message << mqMessageTypeToString(MqMessageType::FIND_SERVICE) << "AppName"
-            << "123123";
-    auto cmd = runtime::stringToMqMessageType(message.getElementAtIndex(0).c_str());
-    std::string processName = message.getElementAtIndex(1);
+    MqMessage m_receiveBuffer;
+    message << mqMessageTypeToString(MqMessageType::FIND_SERVICE) << m_runtimeName
+            << static_cast<cxx::Serialization>(iox::capro::ServiceDescription()).toString();
 
-    m_roudiApp->processMessageTest(message, cmd, ProcessName_t(cxx::TruncateToCapacity, processName));
+    const auto successfullySent = m_runtime->sendRequestToRouDi(message, m_receiveBuffer);
 
-    m_roudiApp->shutDownTest();
+    EXPECT_TRUE(m_receiveBuffer.isValid());
+    EXPECT_TRUE(successfullySent);
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageFindServiceError)
 {
     std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
@@ -499,7 +465,6 @@ TEST_F(RouDiBaseClass_test, ProcessMessageFindServiceError)
     m_roudiApp->shutDownTest();
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageKeepAlive)
 {
     std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
@@ -520,7 +485,6 @@ TEST_F(RouDiBaseClass_test, ProcessMessageKeepAlive)
     m_roudiApp->shutDownTest();
 }
 
-/// @note name of the Testcase shall describe the test case in detail to avoid additional comments
 TEST_F(RouDiBaseClass_test, ProcessMessageError)
 {
     std::unique_ptr<IceOryxRouDiComponents> m_roudiComponents =
