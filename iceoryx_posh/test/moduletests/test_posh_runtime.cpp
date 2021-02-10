d9a1d3e20863a93a7e7ec6ee578be230a8e6e70e
Jimmy Belloche <external.jimmy.belloche@de.bosch.com>
2021-02-10 17:01:50 +0100

    iox-#496 change copyright header
    Signed-off-by: Jimmy Belloche <external.jimmy.belloche@de.bosch.com>

--- iceoryx_posh/test/moduletests/test_posh_runtime.cpp
+++ iceoryx_posh/test/moduletests/test_posh_runtime.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, 2021 by Robert Bosch GmbH, Apex.AI Inc. All rights reserved.
+// Copyright (c) 2020, 2021 by Robert Bosch GmbH. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 #include "iceoryx_posh/iceoryx_posh_types.hpp"
 #include "iceoryx_posh/internal/roudi_environment/roudi_environment.hpp"
 #include "iceoryx_posh/runtime/posh_runtime.hpp"
--- iceoryx_posh/test/moduletests/test_posh_runtime_single_process.cpp
+++ iceoryx_posh/test/moduletests/test_posh_runtime_single_process.cpp
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
 
 #include "iceoryx_posh/internal/roudi_environment/roudi_environment.hpp"
 #include "iceoryx_posh/runtime/posh_runtime_single_process.hpp"
