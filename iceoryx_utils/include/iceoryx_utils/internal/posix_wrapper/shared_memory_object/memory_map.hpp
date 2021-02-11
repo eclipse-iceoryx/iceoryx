48a847b63e5106137cbb2a03e321406b2ec25437
Christian Eltzschig <me@elchris.org>
2021-02-11 12:19:52 +0100

    iox-#542 updated licenses
    Signed-off-by: Christian Eltzschig <me@elchris.org>

--- iceoryx_utils/include/iceoryx_utils/internal/posix_wrapper/shared_memory_object/memory_map.hpp
+++ iceoryx_utils/include/iceoryx_utils/internal/posix_wrapper/shared_memory_object/memory_map.hpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2019 by Robert Bosch GmbH. All rights reserved.
+// Copyright (c) 2021 by Apex.AI Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +12,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 #ifndef IOX_UTILS_POSIX_WRAPPER_SHARED_MEMORY_OBJECT_MEMORY_MAP_HPP
 #define IOX_UTILS_POSIX_WRAPPER_SHARED_MEMORY_OBJECT_MEMORY_MAP_HPP
 
