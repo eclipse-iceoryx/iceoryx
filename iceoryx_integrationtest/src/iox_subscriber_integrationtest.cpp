4d1af82d7648303ba5cbf604a475b2ffd110850f
Dietrich Krönke <dietrich.kroenke@apex.ai>
2021-02-11 11:24:59 +0100

    iox-#378 add SPDX identifier to fileheader
    Signed-off-by: Dietrich Krönke <dietrich.kroenke@apex.ai>

--- iceoryx_integrationtest/src/iox_subscriber_integrationtest.cpp
+++ iceoryx_integrationtest/src/iox_subscriber_integrationtest.cpp
@@ -11,7 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+//
+// SPDX-License-Identifier: Apache-2.0
 #include "topic_data.hpp"
 
 #include "iceoryx_posh/popo/untyped_subscriber.hpp"
