f35591b135c6437b592213a8fa25a0174b955224
Michael Poehnl <michael.poehnl@apex.ai>
2021-03-01 16:32:31 +0100

    iox-#408 header fun
    Signed-off-by: Michael Poehnl <michael.poehnl@apex.ai>

--- iceoryx_posh/source/runtime/posh_runtime.cpp
+++ iceoryx_posh/source/runtime/posh_runtime.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019, 2021 by Robert Bosch GmbH, Apex.AI Inc. All rights reserved.
+// Copyright (c) 2019 - 2021 by Robert Bosch GmbH. All rights reserved.
 // Copyright (c) 2021 by Apex.AI Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
