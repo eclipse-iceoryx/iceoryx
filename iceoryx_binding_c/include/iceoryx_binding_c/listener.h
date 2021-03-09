2402def82956dcba08ad5e55faac64bd599f8392
Christian Eltzschig <me@elchris.org>
2021-03-09 14:24:51 +0100

    iox-#350 documented listener c api
    Signed-off-by: Christian Eltzschig <me@elchris.org>

--- iceoryx_binding_c/include/iceoryx_binding_c/listener.h
+++ iceoryx_binding_c/include/iceoryx_binding_c/listener.h
@@ -25,25 +25,58 @@
 
 typedef CLASS Listener* iox_listener_t;
 
+
+/// @brief initializes a listener struct from a storage struct pointer
+/// @param self pointer to raw memory which can hold a listener
+/// @return an initialized iox_listener_t
 iox_listener_t iox_listener_init(iox_listener_storage_t* self);
 
+/// @brief after using an iox_listener_t it must be cleaned up with this function
+/// @param self the listener which should be deinitialized
 void iox_listener_deinit(iox_listener_t const self);
 
+/// @brief Attaches a subscriber event to the listener
+/// @param self listener to which the event should be attached to
+/// @param subscriber subscriber which emits the event
+/// @param subscriberEvent the event which should trigger the listener
+/// @param callback the callback which is called when an event triggers the listener
+/// @return when successful iox_ListenerResult::ListenerResult_SUCCESS otherwise an enum which describes the error
 ENUM iox_ListenerResult iox_listener_attach_subscriber_event(iox_listener_t const self,
                                                              iox_sub_t const subscriber,
                                                              const ENUM iox_SubscriberEvent subscriberEvent,
                                                              void (*callback)(iox_sub_t));
+
+/// @brief Attaches a user trigger to the listener
+/// @param self listener to which the event should be attached to
+/// @param userTrigger user trigger which emits the event
+/// @param callback the callback which is called when the user trigger triggers the listener
+/// @return when successful iox_ListenerResult::ListenerResult_SUCCESS otherwise an enum which describes the error
 ENUM iox_ListenerResult iox_listener_attach_user_trigger_event(iox_listener_t const self,
                                                                iox_user_trigger_t const userTrigger,
                                                                void (*callback)(iox_user_trigger_t));
 
+/// @brief Detaches a subscriber event from the listener
+/// @param self listener from which the event should be detached
+/// @param subscriber the subscriber which emits the event
+/// @param subscriberEvent the subscriber event which is registered at the listener
 void iox_listener_detach_subscriber_event(iox_listener_t const self,
                                           iox_sub_t const subscriber,
                                           const ENUM iox_SubscriberEvent subscriberEvent);
+
+/// @brief Detaches a user trigger from the listener
+/// @param self listener from which the event should be detached
+/// @param userTrigger the user trigger which emits the event
 void iox_listener_detach_user_trigger_event(iox_listener_t const self, iox_user_trigger_t const userTrigger);
 
+
+/// @brief Returns the size, the number of attached events of a listener.
+/// @param self listener where the size should be acquired
+/// @return the size of the listener
 uint64_t iox_listener_size(iox_listener_t const self);
 
+/// @brief Returns the capacity of a listener (how many events can be attached).
+/// @param self listener where the capacity should be acquired
+/// @return the capacity of the listener
 uint64_t iox_listener_capacity(iox_listener_t const self);
 
 #endif
