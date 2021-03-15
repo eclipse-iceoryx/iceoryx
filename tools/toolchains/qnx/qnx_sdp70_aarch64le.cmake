2ddbae30e0e9163e5a1c3612734b7fa85ba71bb8
Hintz Martin (XC-AD/ESB5) <martin.hintz@de.bosch.com>
2021-03-15 15:13:34 +0100

    iox-#609 Add generic QNX 7.0 toolchain files
    Signed-off-by: Hintz Martin (XC-AD/ESB5) <martin.hintz@de.bosch.com>

---
+++ tools/toolchains/qnx/qnx_sdp70_aarch64le.cmake
@@ -0,0 +1,4 @@
+SET(CMAKE_SYSTEM_PROCESSOR aarch64)
+SET(arch gcc_ntoaarch64le)
+
+include(${CMAKE_CURRENT_LIST_DIR}/qnx_sdp70_common.cmake)
---
+++ tools/toolchains/qnx/qnx_sdp70_x86_64.cmake
@@ -0,0 +1,4 @@
+SET(CMAKE_SYSTEM_PROCESSOR x86_64)
+SET(arch gcc_ntox86_64)
+
+include(${CMAKE_CURRENT_LIST_DIR}/qnx_sdp70_common.cmake)
