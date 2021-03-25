3d10c08aff5b9360d09bb2befa851ca793661c8a
Christian Eltzschig <me@elchris.org>
2021-03-25 16:49:03 +0100

    iox-#615 added blocking test and adjusted overflow test
    Signed-off-by: Christian Eltzschig <me@elchris.org>

--- iceoryx_utils/test/moduletests/test_cxx_variant_queue.cpp
+++ iceoryx_utils/test/moduletests/test_cxx_variant_queue.cpp
@@ -16,6 +16,7 @@
 
 #include "iceoryx_utils/cxx/variant_queue.hpp"
 #include "test.hpp"
+#include "testutils/watch_dog.hpp"
 
 using namespace ::testing;
 using namespace iox;
@@ -27,6 +28,7 @@ class VariantQueue_test : public Test
   public:
     void SetUp()
     {
+        m_watchdog.watchAndActOnFailure([] { std::terminate(); });
     }
     void TearDown()
     {
@@ -39,7 +41,8 @@ class VariantQueue_test : public Test
     }
 
     // if a new fifo type is added this variable has to be adjusted
-    uint64_t numberOfQueueTypes = 6;
+    uint64_t numberOfQueueTypes = 6U;
+    Watchdog m_watchdog{units::Duration::fromSeconds(2U)};
 };
 
 TEST_F(VariantQueue_test, isEmptyWhenCreated)
@@ -108,9 +111,15 @@ TEST_F(VariantQueue_test, pushTwoElementsAfterSecondPopIsInvalid)
 
 TEST_F(VariantQueue_test, handlesOverflow)
 {
-    return;
     PerformTestForQueueTypes([](uint64_t typeID) {
-        VariantQueue<int, 2> sut(static_cast<VariantQueueTypes>(typeID));
+        VariantQueueTypes type = static_cast<VariantQueueTypes>(typeID);
+        if (type == VariantQueueTypes::BlockingFiFo_MultiProducerSingleConsumer
+            || type == VariantQueueTypes::BlockingFiFo_SingleProducerSingleConsumer)
+        {
+            return;
+        }
+
+        VariantQueue<int, 2> sut(type);
         // current SOFI can hold capacity +1 values, so push some more to ensure overflow
         sut.push(14123);
         sut.push(24123);
@@ -118,6 +127,41 @@ TEST_F(VariantQueue_test, handlesOverflow)
         sut.push(33333);
         auto maybeOverflowValue = sut.push(667);
         EXPECT_THAT(maybeOverflowValue.has_value(), Eq(true));
+    });
+}
+
+TEST_F(VariantQueue_test, blocksOnOverflow)
+{
+    PerformTestForQueueTypes([](uint64_t typeID) {
+        VariantQueueTypes type = static_cast<VariantQueueTypes>(typeID);
+        if (!(type == VariantQueueTypes::BlockingFiFo_MultiProducerSingleConsumer
+              || type == VariantQueueTypes::BlockingFiFo_SingleProducerSingleConsumer))
+        {
+            return;
+        }
+
+        VariantQueue<int, 2> sut(type);
+
+        for (uint64_t i = 0U; i < sut.capacity(); ++i)
+        {
+            sut.push(24123 + i);
+        }
+
+        std::atomic_uint64_t counter{0U};
+        std::thread t([&] {
+            sut.push(1231);
+            ++counter;
+        });
+
+        constexpr int64_t TIMEOUT_IN_MS = 100;
+        std::this_thread::sleep_for(std::chrono::milliseconds(TIMEOUT_IN_MS));
+        EXPECT_THAT(counter.load(), Eq(0U));
+        auto result = sut.pop();
+        ASSERT_THAT(result.has_value(), Eq(true));
+        EXPECT_THAT(*result, Eq(24123));
+        std::this_thread::sleep_for(std::chrono::milliseconds(TIMEOUT_IN_MS));
+        EXPECT_THAT(counter.load(), Eq(1U));
+        t.join();
     });
 }
 
