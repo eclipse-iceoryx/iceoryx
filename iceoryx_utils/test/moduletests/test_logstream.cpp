64354f049678591b7d99b477b9acbdb51bad11de
Mathias Kraus <mathias.kraus@apex.ai>
2021-04-01 21:34:11 +0200

    iox-#563 update copyright header
    Signed-off-by: Mathias Kraus <mathias.kraus@apex.ai>

--- iceoryx_utils/test/moduletests/test_logstream.cpp
+++ iceoryx_utils/test/moduletests/test_logstream.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2019 by Robert Bosch GmbH. All rights reserved.
+// Copyright (c) 2021 by Apex.AI Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
