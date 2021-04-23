9527196ed2178e7bb349d26e1d0eefc3fe1a7457
Mathias Kraus <mathias.kraus@apex.ai>
2021-04-23 19:16:54 +0200

    iox-#14 add user-header example for untyped C++ API
    Signed-off-by: Mathias Kraus <mathias.kraus@apex.ai>

---
+++ iceoryx_examples/user_header/publisher_untyped_cxx_api.cpp
@@ -0,0 +1,80 @@
+// Copyright (c) 2021 by Apex.AI Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "user_header_and_payload_types.hpp"
+
+#include "iceoryx_posh/popo/untyped_publisher.hpp"
+#include "iceoryx_posh/runtime/posh_runtime.hpp"
+#include "iceoryx_utils/posix_wrapper/signal_handler.hpp"
+
+#include <atomic>
+#include <iostream>
+
+std::atomic<bool> killswitch{false};
+constexpr char APP_NAME[] = "iox-cpp-user-header-untyped-publisher";
+
+static void sigHandler(int sig IOX_MAYBE_UNUSED)
+{
+    // caught SIGINT or SIGTERM, now exit gracefully
+    killswitch = true;
+}
+
+int main()
+{
+    // register sigHandler
+    auto signalIntGuard = iox::posix::registerSignalHandler(iox::posix::Signal::INT, sigHandler);
+    auto signalTermGuard = iox::posix::registerSignalHandler(iox::posix::Signal::TERM, sigHandler);
+
+    // initialize runtime
+    iox::runtime::PoshRuntime::initRuntime(APP_NAME);
+
+    // create the publisher
+    iox::popo::UntypedPublisher publisher({"Example", "User-Header", "Timestamp"});
+
+    auto startTime = std::chrono::steady_clock::now();
+    uint64_t fibonacciLast = 0;
+    uint64_t fibonacciCurrent = 1;
+    while (!killswitch)
+    {
+        auto fibonacciNext = fibonacciCurrent + fibonacciLast;
+        fibonacciLast = fibonacciCurrent;
+        fibonacciCurrent = fibonacciNext;
+        publisher.loan(sizeof(Data), alignof(Data), sizeof(Header), alignof(Header))
+            .and_then([&](auto& userPayload) {
+                auto elapsedTime = std::chrono::steady_clock::now() - startTime;
+                auto elapsedMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(elapsedTime).count();
+
+                auto header = static_cast<Header*>(iox::mepoo::ChunkHeader::fromUserPayload(userPayload)->userHeader());
+                header->publisherTimestamp = elapsedMilliseconds;
+
+                auto data = static_cast<Data*>(userPayload);
+                data->fibonacci = fibonacciCurrent;
+
+                publisher.publish(userPayload);
+
+                std::cout << APP_NAME << " sent data: " << fibonacciCurrent << " with timestamp " << elapsedMilliseconds
+                          << "ms" << std::endl;
+            })
+            .or_else([](auto& error) {
+                std::cout << APP_NAME << " could not loan sample! Error code: " << static_cast<uint64_t>(error)
+                          << std::endl;
+            });
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+
+    return 0;
+}
---
+++ iceoryx_examples/user_header/subscriber_untyped_cxx_api.cpp
@@ -0,0 +1,64 @@
+// Copyright (c) 2021 by Apex.AI Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "user_header_and_payload_types.hpp"
+
+#include "iceoryx_posh/popo/untyped_subscriber.hpp"
+#include "iceoryx_posh/runtime/posh_runtime.hpp"
+#include "iceoryx_utils/posix_wrapper/signal_handler.hpp"
+
+#include <atomic>
+#include <iostream>
+
+std::atomic<bool> killswitch{false};
+constexpr char APP_NAME[] = "iox-cpp-user-header-untyped-subscriber";
+
+static void sigHandler(int sig IOX_MAYBE_UNUSED)
+{
+    // caught SIGINT or SIGTERM, now exit gracefully
+    killswitch = true;
+}
+
+int main()
+{
+    // register sigHandler
+    auto signalIntGuard = iox::posix::registerSignalHandler(iox::posix::Signal::INT, sigHandler);
+    auto signalTermGuard = iox::posix::registerSignalHandler(iox::posix::Signal::TERM, sigHandler);
+
+    // initialize runtime
+    iox::runtime::PoshRuntime::initRuntime(APP_NAME);
+
+    // create subscriber
+    iox::popo::UntypedSubscriber subscriber({"Example", "User-Header", "Timestamp"});
+
+    // run until interrupted by Ctrl-C
+    while (!killswitch)
+    {
+        subscriber.take().and_then([](auto& userPayload) {
+            auto header =
+                static_cast<const Header*>(iox::mepoo::ChunkHeader::fromUserPayload(userPayload)->userHeader());
+
+            auto data = static_cast<const Data*>(userPayload);
+
+            std::cout << APP_NAME << " got value: " << data->fibonacci << " with timestamp "
+                      << header->publisherTimestamp << std::endl;
+        });
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+
+    return (EXIT_SUCCESS);
+}
