2590df65c36d5926bff2a6c4cd824255e438b65c
Chari Nehal Dattaram (RBEI/EBB1) <NehalDattaram.Chari@in.bosch.com>
2021-05-20 16:48:44 +0530

    iox-#496 used variable inplace of constants
    Signed-off-by: Chari Nehal Dattaram (RBEI/EBB1) <NehalDattaram.Chari@in.bosch.com>

--- iceoryx_posh/test/moduletests/test_mepoo_shared_chunk.cpp
+++ iceoryx_posh/test/moduletests/test_mepoo_shared_chunk.cpp
@@ -59,8 +59,8 @@ class SharedChunk_Test : public Test
 
     char memory[4096U];
     iox::posix::Allocator allocator{memory, 4096U};
-    MemPool mempool{sizeof(ChunkHeader) + USER_PAYLOAD_SIZE, 10U, allocator, allocator};
-    MemPool chunkMgmtPool{64U, 10U, allocator, allocator};
+    MemPool mempool{sizeof(ChunkHeader) + USER_PAYLOAD_SIZE, NUMBER_OF_CHUNKS, allocator, allocator};
+    MemPool chunkMgmtPool{CHUNK_SIZE, NUMBER_OF_CHUNKS, allocator, allocator};
     void* memoryChunk{mempool.getChunk()};
     ChunkManagement* chunkManagement = GetChunkManagement(memoryChunk);
     SharedChunk sut{chunkManagement};
