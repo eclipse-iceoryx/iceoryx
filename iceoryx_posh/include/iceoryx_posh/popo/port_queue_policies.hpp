4238d7207499f3609537c3b0975b6333a9ceffdc
Mathias Kraus <mathias.kraus@apex.ai>
2021-06-15 14:41:48 +0200

    iox-#27 Add new queue policies
    Signed-off-by: Mathias Kraus <mathias.kraus@apex.ai>

--- iceoryx_posh/include/iceoryx_posh/popo/port_queue_policies.hpp
+++ iceoryx_posh/include/iceoryx_posh/popo/port_queue_policies.hpp
@@ -24,17 +24,39 @@ namespace iox
 namespace popo
 {
 /// @brief Used by publisher
+/// @todo iox-#27 replace with ConsumerTooSlowPolicy
 enum class SubscriberTooSlowPolicy : uint8_t
 {
     WAIT_FOR_SUBSCRIBER,
     DISCARD_OLDEST_DATA
 };
 /// @brief Used by subscriber
+/// @todo iox-#27 replace with QueueFullPolicy2
 enum class QueueFullPolicy : uint8_t
 {
     BLOCK_PUBLISHER,
     DISCARD_OLDEST_DATA
 };
+
+/// @brief Used by producers how to adjust to slow consumer
+enum class ConsumerTooSlowPolicy
+{
+    /// Waits for the consumer it it's queue is full
+    WAIT_FOR_CONSUMER,
+    /// Discards the oldest data and pushes the newest one into the queue
+    DISCARD_OLDEST_DATA
+};
+
+/// @brief Used by consumers to request a specific behavior from the producer
+/// @todo iox-#27 Rename this to QueueFullPolicy
+enum class QueueFullPolicy2
+{
+    /// Requests the producer to block when the consumer queue is full
+    BLOCK_PRODUCER,
+    /// Request to discard the oldest data and push the newest one into the queue
+    DISCARD_OLDEST_DATA
+};
+
 } // namespace popo
 } // namespace iox
 #endif // IOX_POSH_POPO_PORT_QUEUE_POLICIES_HPP
