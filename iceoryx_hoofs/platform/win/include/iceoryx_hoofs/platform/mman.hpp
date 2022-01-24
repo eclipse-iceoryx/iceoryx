c55a17215d9c1a22e152ca8c41e8020c5542ea5e
Christian Eltzschig <me@elchris.org>
2022-01-24 18:04:49 +0100

    iox-#1036 Add MAP_PRIVATE and MAP_FIXED for windows
    Signed-off-by: Christian Eltzschig <me@elchris.org>

--- iceoryx_hoofs/platform/win/include/iceoryx_hoofs/platform/mman.hpp
+++ iceoryx_hoofs/platform/win/include/iceoryx_hoofs/platform/mman.hpp
@@ -30,7 +30,9 @@
 // windows will define some macros which makes the code uncompilable
 #include "iceoryx_hoofs/platform/platform_correction.hpp"
 
-#define MAP_SHARED 0
+#define MAP_SHARED 0x01
+#define MAP_PRIVATE 0x02
+#define MAP_FIXED 0x10
 #define MAP_FAILED 1
 #define PROT_NONE 0
 #define PROT_READ 3
