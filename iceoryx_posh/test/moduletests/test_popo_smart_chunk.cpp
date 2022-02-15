94b000b2c86ce04600c8bf3fedf85323406efde7
Mathias Kraus <mathias.kraus@apex.ai>
2022-02-15 16:57:01 +0100

    iox-#27 Add RcpInterface and use it in Request and Response

--- iceoryx_posh/test/moduletests/test_popo_smart_chunk.cpp
+++ iceoryx_posh/test/moduletests/test_popo_smart_chunk.cpp
@@ -19,6 +19,8 @@
 #include "iceoryx_hoofs/cxx/unique_ptr.hpp"
 #include "iceoryx_posh/internal/popo/smart_chunk.hpp"
 #include "iceoryx_posh/popo/publisher.hpp"
+#include "iceoryx_posh/popo/request.hpp"
+#include "iceoryx_posh/popo/response.hpp"
 #include "iceoryx_posh/popo/sample.hpp"
 #include "iceoryx_posh/testing/mocks/chunk_mock.hpp"
 
