e56a5f0d7550cfdc3f15baea70057b3a37bb317b
Mathias Kraus <mathias.kraus@apex.ai>
2022-02-18 23:52:32 +0100

    iox-#27 Update documentation

--- iceoryx_posh/include/iceoryx_posh/internal/popo/untyped_client_impl.hpp
+++ iceoryx_posh/include/iceoryx_posh/internal/popo/untyped_client_impl.hpp
@@ -34,11 +34,37 @@ class UntypedClientImpl : public BaseClientT
     explicit UntypedClientImpl(const capro::ServiceDescription& service,
                                const ClientOptions& clientOptions = {}) noexcept;
 
+    /// @brief Get a request chunk from loaned shared memory.
+    /// @param payloadSize The expected payload size of the chunk.
+    /// @param payloadAlignment The expected payload alignment of the chunk.
+    /// @return A pointer to the payload of a chunk of memory with the requested size or
+    ///         an AllocationError if no chunk could be loaned.
+    /// @note An AllocationError occurs if no chunk is available in the shared memory.
     cxx::expected<void*, AllocationError> loan(const uint32_t payloadSize, const uint32_t payloadAlignment) noexcept;
+
+    /// @brief Releases the ownership of the request chunk provided by the payload pointer.
+    /// @param requestPayload pointer to the payload of the chunk to be released
+    /// @details The requestPayload pointer must have been previously provided by `loan`
+    ///          and not have been already released. The chunk must not be accessed afterwards
+    ///          as its memory may have been reclaimed.
     void releaseRequest(void* const requestPayload) noexcept;
+
+    /// @brief Sends the provided memory chunk as request to the server.
+    /// @param requestPayload Pointer to the payload of the allocated shared memory chunk.
+    /// @return Error if provided pointer is not a payload of a valid memory chunk.
     void send(void* const requestPayload) noexcept;
 
+    /// @brief Take the response chunk from the top of the receive queue.
+    /// @return The payload pointer of the request chunk taken.
+    /// @details No automatic cleanup of the associated chunk is performed
+    ///          and must be manually done by calling `releaseResponse`
     cxx::expected<const void*, ChunkReceiveResult> take() noexcept;
+
+    /// @brief Releases the ownership of the response chunk provided by the payload pointer.
+    /// @param responsePayload pointer to the payload of the chunk to be released
+    /// @details The responsePayload pointer must have been previously provided by `take`
+    ///          and not have been already released. The chunk must not be accessed afterwards
+    ///          as its memory may have been reclaimed.
     void releaseResponse(const void* const responsePayload) noexcept;
 
   protected:
--- iceoryx_posh/include/iceoryx_posh/internal/popo/untyped_server_impl.hpp
+++ iceoryx_posh/include/iceoryx_posh/internal/popo/untyped_server_impl.hpp
@@ -32,13 +32,41 @@ class UntypedServerImpl : public BaseServerT
     explicit UntypedServerImpl(const capro::ServiceDescription& service,
                                const ServerOptions& serverOptions = {}) noexcept;
 
+    /// @brief Take the request chunk from the top of the receive queue.
+    /// @return The payload pointer of the request chunk taken.
+    /// @details No automatic cleanup of the associated chunk is performed
+    ///          and must be manually done by calling `releaseRequest`
     cxx::expected<const void*, ServerRequestResult> take() noexcept;
+
+    /// @brief Releases the ownership of the request chunk provided by the payload pointer.
+    /// @param requestPayload pointer to the payload of the chunk to be released
+    /// @details The requestPayload pointer must have been previously provided by `take`
+    ///          and not have been already released. The chunk must not be accessed afterwards
+    ///          as its memory may have been reclaimed.
     void releaseRequest(const void* const requestPayload) noexcept;
 
+    /// @brief Get a response chunk from loaned shared memory.
+    /// @param[in] requestHeader The requestHeader to which the response belongs to, to determine where to send the
+    /// response
+    /// @param payloadSize The expected payload size of the chunk.
+    /// @param payloadAlignment The expected payload alignment of the chunk.
+    /// @return A pointer to the payload of a chunk of memory with the requested size or
+    ///         an AllocationError if no chunk could be loaned.
+    /// @note An AllocationError occurs if no chunk is available in the shared memory.
     cxx::expected<void*, AllocationError> loan(const RequestHeader* const requestHeader,
                                                const uint32_t payloadSize,
                                                const uint32_t payloadAlignment) noexcept;
+
+    /// @brief Sends the provided memory chunk as response to the client.
+    /// @param responsePayload Pointer to the payload of the allocated shared memory chunk.
+    /// @return Error if provided pointer is not a payload of a valid memory chunk.
     void send(void* const responsePayload) noexcept;
+
+    /// @brief Releases the ownership of the response chunk provided by the payload pointer.
+    /// @param responsePayload pointer to the payload of the chunk to be released
+    /// @details The responsePayload pointer must have been previously provided by `loan`
+    ///          and not have been already released. The chunk must not be accessed afterwards
+    ///          as its memory may have been reclaimed.
     void releaseResponse(void* const responsePayload) noexcept;
 
   protected:
