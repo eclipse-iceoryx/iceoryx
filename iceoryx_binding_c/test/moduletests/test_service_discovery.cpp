7c7fe100140db990dfd35d543701900a0441caeb
Marika Lehmann <marika.lehmann@apex.ai>
2022-02-22 11:47:23 +0100

    iox-#1142 Add functional find service test
    Signed-off-by: Marika Lehmann <marika.lehmann@apex.ai>

--- iceoryx_binding_c/test/moduletests/test_service_discovery.cpp
+++ iceoryx_binding_c/test/moduletests/test_service_discovery.cpp
@@ -21,6 +21,7 @@ using namespace iox;
 using namespace iox::runtime;
 
 extern "C" {
+#include "iceoryx_binding_c/publisher.h"
 #include "iceoryx_binding_c/runtime.h"
 #include "iceoryx_binding_c/service_discovery.h"
 }
@@ -34,16 +35,16 @@ class iox_service_discovery_test : public RouDi_GTest
   public:
     void SetUp() override
     {
-        iox_runtime_init(runtimeName.c_str());
+        iox_runtime_init("runtime");
         sut = iox_service_discovery_init(&sutStorage);
     }
 
     void TearDown() override
     {
+        searchResult.clear();
         iox_service_discovery_deinit(sut);
     }
 
-    std::string runtimeName = "runtime";
     iox_service_discovery_storage_t sutStorage;
     iox_service_discovery_t sut;
 
@@ -56,14 +57,43 @@ class iox_service_discovery_test : public RouDi_GTest
 
 cxx::vector<iox_service_description_t, MAX_FINDSERVICE_RESULT_SIZE> iox_service_discovery_test::searchResult;
 
+TEST(iox_service_discovery_DeathTest, InitServiceDiscoveryWithNullptrForStorageTerminates)
+{
+    ::testing::Test::RecordProperty("TEST_ID", "be551a9e-7dcf-406a-a74c-7dcb1ee16c30");
+    EXPECT_DEATH({ iox_service_discovery_init(nullptr); }, ".*");
+}
 
+/// @note We test only if the arguments of iox_service_discovery_find_service are correctly passed to
+/// ServiceDiscovery::findService.
 TEST_F(iox_service_discovery_test, FindServiceWithNullptrsForServiceInstanceEventsReturnsAllServices)
 {
+    ::testing::Test::RecordProperty("TEST_ID", "09a2cd6c-fba9-4b9d-af96-c5a6cc168d98");
     iox_service_discovery_find_service(sut, nullptr, nullptr, nullptr, findHandler);
     for (const auto& service : searchResult)
     {
         EXPECT_THAT(service.instanceString, StrEq("RouDi_ID"));
     }
+}
+
+TEST_F(iox_service_discovery_test, FindServiceReturnsOfferedService)
+{
+    ::testing::Test::RecordProperty("TEST_ID", "bb12e514-e7af-4946-b098-98b3cd0f43a5");
+    iox_pub_options_t options;
+    iox_pub_options_init(&options);
+    iox_pub_storage_t storage;
+    auto* publisher = iox_pub_init(&storage, "service", "instance", "event", &options);
+    ASSERT_NE(publisher, nullptr);
+    const iox_service_description_t SERVICE_DESCRIPTION = iox_pub_get_service_description(publisher);
+
+    iox_service_discovery_find_service(sut,
+                                       SERVICE_DESCRIPTION.serviceString,
+                                       SERVICE_DESCRIPTION.instanceString,
+                                       SERVICE_DESCRIPTION.eventString,
+                                       findHandler);
+    ASSERT_THAT(searchResult.size(), Eq(1U));
+    EXPECT_THAT(*searchResult.begin()->serviceString, Eq(*SERVICE_DESCRIPTION.serviceString));
+    EXPECT_THAT(*searchResult.begin()->instanceString, Eq(*SERVICE_DESCRIPTION.instanceString));
+    EXPECT_THAT(*searchResult.begin()->eventString, Eq(*SERVICE_DESCRIPTION.eventString));
 }
 
 } // namespace
