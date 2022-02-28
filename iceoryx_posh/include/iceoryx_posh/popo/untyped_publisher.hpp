6d97f01b9c0a6cb21b9eda01cf40f4ab33babfbb
Mathias Kraus <mathias.kraus@apex.ai>
2022-02-28 18:38:49 +0100

    iox-#1173 Split untyped_publisher.hpp/.inl into untyped_publisher_impl.hpp/.inl

--- iceoryx_posh/include/iceoryx_posh/popo/untyped_publisher.hpp
+++ iceoryx_posh/include/iceoryx_posh/popo/untyped_publisher.hpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2020 by Robert Bosch GmbH. All rights reserved.
-// Copyright (c) 2020 - 2021 by Apex.AI Inc. All rights reserved.
+// Copyright (c) 2020 - 2022 by Apex.AI Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,64 +18,20 @@
 #ifndef IOX_POSH_POPO_UNTYPED_PUBLISHER_HPP
 #define IOX_POSH_POPO_UNTYPED_PUBLISHER_HPP
 
-#include "iceoryx_posh/popo/base_publisher.hpp"
-#include "iceoryx_posh/popo/sample.hpp"
+#include "iceoryx_posh/internal/popo/untyped_publisher_impl.hpp"
 
 namespace iox
 {
 namespace popo
 {
-template <typename BasePublisher_t = BasePublisher<>>
-class UntypedPublisherImpl : public BasePublisher_t
+/// @brief The UntypedPublisher class for the publish-subscribe messaging pattern in iceoryx.
+class UntypedPublisher : public UntypedPublisherImpl<>
 {
   public:
-    UntypedPublisherImpl(const capro::ServiceDescription& service,
-                         const PublisherOptions& publisherOptions = PublisherOptions());
-    UntypedPublisherImpl(const UntypedPublisherImpl& other) = delete;
-    UntypedPublisherImpl& operator=(const UntypedPublisherImpl&) = delete;
-    UntypedPublisherImpl(UntypedPublisherImpl&& rhs) = default;
-    UntypedPublisherImpl& operator=(UntypedPublisherImpl&& rhs) = default;
-    virtual ~UntypedPublisherImpl() = default;
-
-    ///
-    /// @brief Get a chunk from loaned shared memory.
-    /// @param usePayloadSize The expected user-payload size of the chunk.
-    /// @param userPayloadAlignment The expected user-payload alignment of the chunk.
-    /// @return A pointer to the user-payload of a chunk of memory with the requested size or
-    ///         an AllocationError if no chunk could be loaned.
-    /// @note An AllocationError occurs if no chunk is available in the shared memory.
-    ///
-    cxx::expected<void*, AllocationError>
-    loan(const uint32_t userPayloadSize,
-         const uint32_t userPayloadAlignment = iox::CHUNK_DEFAULT_USER_PAYLOAD_ALIGNMENT,
-         const uint32_t userHeaderSize = iox::CHUNK_NO_USER_HEADER_SIZE,
-         const uint32_t userHeaderAlignment = iox::CHUNK_NO_USER_HEADER_ALIGNMENT) noexcept;
-
-    ///
-    /// @brief Publish the provided memory chunk.
-    /// @param userPayload Pointer to the user-payload of the allocated shared memory chunk.
-    /// @return Error if provided pointer is not a user-payload of a valid memory chunk.
-    ///
-    void publish(void* const userPayload) noexcept;
-
-    ///
-    /// @brief Releases the ownership of the chunk provided by the user-payload pointer.
-    /// @param userPayload pointer to the user-payload of the chunk to be released
-    /// @details The userPayload pointer must have been previously provided by loan
-    ///          and not have been already released. The chunk must not be accessed afterwards
-    ///          as its memory may have been reclaimed.
-    ///
-    void release(void* const userPayload) noexcept;
-
-  protected:
-    using BasePublisher_t::port;
+    using UntypedPublisherImpl<>::UntypedPublisherImpl;
 };
-
-using UntypedPublisher = UntypedPublisherImpl<>;
 
 } // namespace popo
 } // namespace iox
-
-#include "iceoryx_posh/internal/popo/untyped_publisher.inl"
 
 #endif // IOX_POSH_POPO_UNTYPED_PUBLISHER_HPP
