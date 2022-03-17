2543ac90d8de49396f1614818abc9879fbc9c9ee
Christian Eltzschig <me@elchris.org>
2022-03-17 20:41:32 +0100

    iox-#1294 Add single file check feature
    Signed-off-by: Christian Eltzschig <me@elchris.org>

--- tools/ci/markdownLinkVerificator.sh
+++ tools/ci/markdownLinkVerificator.sh
@@ -4,6 +4,8 @@ ICEORYX_ROOT_PATH=$(git rev-parse --show-toplevel)
 EXIT_CODE=0
 ENABLE_URL_CHECK=0
 
+FILE_TO_SCAN=$1
+
 setupTerminalColors()
 {
     if [[ -t 1 ]]
@@ -94,6 +96,7 @@ isAbsolutePath()
 printLinkFailureSource()
 {
     echo -e "  name:    ${COLOR_LIGHT_YELLOW} $LINK_NAME ${COLOR_RESET}"
+    echo -e "  line:    ${COLOR_LIGHT_YELLOW} $LINE_NR ${COLOR_RESET}"
     echo -e "  link:    ${COLOR_LIGHT_RED} $LINK${COLOR_RESET}"
     echo
 
@@ -125,6 +128,88 @@ checkLinkToUrl()
     fi
 }
 
+checkLinksInFile()
+{
+    FILE=$1
+
+    FILE_DIRECTORY=$(dirname $FILE)
+    IS_IN_CODE_ENV="0"
+
+    readarray FILE_CONTENT < $FILE
+    LINE_NR=0
+    for LINE in "${FILE_CONTENT[@]}"
+    do
+        let LINE_NR=$LINE_NR+1
+
+        if [[ $(echo $LINE | grep -E "^[ ]*\`\`\`" | wc -l) == "1" ]]
+        then
+            if [[ $IS_IN_CODE_ENV == "1" ]]
+            then
+                IS_IN_CODE_ENV="0"
+            else
+                IS_IN_CODE_ENV="1"
+            fi
+        fi
+
+        if [[ $IS_IN_CODE_ENV == "1" ]]
+        then
+            continue
+        fi
+
+        ## sed -e 's/[^[]`[^`]*`//g' 
+        ## remove inline code env like `auto bla = [blubb](auto i) ..` which could be mistaken as
+        ## a markdown link like [linkName](linkValue)
+        ##
+        ## sed -n "s/.*\[\(.*\)](\([^)]*\)).*/\1[\2/p"
+        ## extract markdown links
+        link=$(echo $LINE | sed -e 's/[^[]`[^`]*`//g' | sed -n "s/.*\[\(.*\)](\([^)]*\)).*/\1[\2/p" | tr ' ' _)
+        if [[ $link == "" ]]
+        then
+            continue
+        fi
+
+        LINK_NAME=$(echo $link | cut -f 1 -d '[')
+        LINK_VALUE=$(echo $link | cut -f 2 -d '[')
+
+        if [[ $(isMailLink $LINK_VALUE) == "1" ]]
+        then
+            continue
+        elif [[ $(isWebLink $LINK_VALUE) == "1" ]]
+        then
+            checkLinkToUrl $LINK_VALUE
+        elif [[ $(isLinkToSection $LINK_VALUE) == "1" ]]
+        then
+            checkLinkToSection $LINK_VALUE $FILE
+        else
+            if [[ ${isAbsolutePath} == "1" ]]
+            then
+                LINK=$LINK
+            else
+                LINK=${FILE_DIRECTORY}/${LINK_VALUE}
+            fi
+
+            if [[ $(echo $LINK | grep '#' | wc -l) == "1" ]]
+            then
+                SECTION_IN_FILE=$(echo $LINK | cut -f 2 -d '#')
+                LINK=$(echo $LINK | cut -f 1 -d '#')
+            fi
+
+            if ! [ -f $LINK ] && ! [ -d $LINK ]
+            then
+                printLinkFailureSource
+                continue
+            fi
+
+            if ! [[ $SECTION_IN_FILE == "" ]]
+            then
+                checkLinkToSection "#$SECTION_IN_FILE" $LINK
+            fi
+
+            SECTION_IN_FILE=""
+        fi
+    done
+}
+
 performLinkCheck()
 {
     NUMBER_OF_FILES=$(find $ICEORYX_ROOT_PATH -type f -iname "*.md" | grep -v ${ICEORYX_ROOT_PATH}/build | grep -v ${ICEORYX_ROOT_PATH}/.github | wc -l)
@@ -133,88 +218,20 @@ performLinkCheck()
     for FILE in $(find $ICEORYX_ROOT_PATH -type f -iname "*.md" | grep -v ${ICEORYX_ROOT_PATH}/build | grep -v ${ICEORYX_ROOT_PATH}/.github)
     do
         let CURRENT_FILE=$CURRENT_FILE+1
-        FILE_DIRECTORY=$(dirname $FILE)
-        echo [$CURRENT_FILE/$NUMBER_OF_FILES] $FILE
+        echo -e "[$CURRENT_FILE/$NUMBER_OF_FILES] ${COLOR_LIGHT_GREEN}$FILE${COLOR_RESET}"
 
-        OLD_IFS=$IFS
-        IFS=$'\n'
-        IS_IN_CODE_ENV="0"
-
-        for LINE in $(cat $FILE)
-        do
-            if [[ $(echo $LINE | grep -E "^[ ]*\`\`\`" | wc -l) == "1" ]]
-            then
-                if [[ $IS_IN_CODE_ENV == "1" ]]
-                then
-                    IS_IN_CODE_ENV="0"
-                else
-                    IS_IN_CODE_ENV="1"
-                fi
-            fi
-
-            if [[ $IS_IN_CODE_ENV == "1" ]]
-            then
-                continue
-            fi
-
-            ## sed -e 's/[^[]`[^`]*`//g' 
-            ## remove inline code env like `auto bla = [blubb](auto i) ..` which could be mistaken as
-            ## a markdown link like [linkName](linkValue)
-            ##
-            ## sed -n "s/.*\[\(.*\)](\([^)]*\)).*/\1[\2/p"
-            ## extract markdown links
-            link=$(echo $LINE | sed -e 's/[^[]`[^`]*`//g' | sed -n "s/.*\[\(.*\)](\([^)]*\)).*/\1[\2/p" | tr ' ' _)
-            if [[ $link == "" ]]
-            then
-                continue
-            fi
-
-            LINK_NAME=$(echo $link | cut -f 1 -d '[')
-            LINK_VALUE=$(echo $link | cut -f 2 -d '[')
-
-            if [[ $(isMailLink $LINK_VALUE) == "1" ]]
-            then
-                continue
-            elif [[ $(isWebLink $LINK_VALUE) == "1" ]]
-            then
-                checkLinkToUrl $LINK_VALUE
-            elif [[ $(isLinkToSection $LINK_VALUE) == "1" ]]
-            then
-                checkLinkToSection $LINK_VALUE $FILE
-            else
-                if [[ ${isAbsolutePath} == "1" ]]
-                then
-                    LINK=$LINK
-                else
-                    LINK=${FILE_DIRECTORY}/${LINK_VALUE}
-                fi
-
-                if [[ $(echo $LINK | grep '#' | wc -l) == "1" ]]
-                then
-                    SECTION_IN_FILE=$(echo $LINK | cut -f 2 -d '#')
-                    LINK=$(echo $LINK | cut -f 1 -d '#')
-                fi
-
-                if ! [ -f $LINK ] && ! [ -d $LINK ]
-                then
-                    printLinkFailureSource
-                    continue
-                fi
-
-                if ! [[ $SECTION_IN_FILE == "" ]]
-                then
-                    checkLinkToSection "#$SECTION_IN_FILE" $LINK
-                fi
-
-                SECTION_IN_FILE=""
-            fi
-        done
+        checkLinksInFile $FILE
     done
 }
 
 setupTerminalColors
 setupTerminalFormat
 
-performLinkCheck
+if ! [ -z $1 ]
+then
+    checkLinksInFile $FILE_TO_SCAN
+else
+    performLinkCheck
+fi
 
 exit $EXIT_CODE
