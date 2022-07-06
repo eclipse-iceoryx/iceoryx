a0098ee2823f3e278222198f07e0c010bcd75b4e
Christian Eltzschig <christian.eltzschig@apex.ai>
2022-07-06 17:46:21 +0200

    iox-#1196 Fix algorithm warnings in test
    Signed-off-by: Christian Eltzschig <christian.eltzschig@apex.ai>

--- iceoryx_hoofs/test/moduletests/test_cxx_algorithm.cpp
+++ iceoryx_hoofs/test/moduletests/test_cxx_algorithm.cpp
@@ -124,15 +124,16 @@ TEST_F(algorithm_test, MergeTwoDisjunctNonEmptySortedContainers)
 {
     ::testing::Test::RecordProperty("TEST_ID", "4f39641f-de8a-434a-8a50-cd2b66b476da");
     constexpr uint64_t OFFSET = 1337U;
-    vector<uint64_t, 10U> first;
-    vector<uint64_t, 10U> second;
+    constexpr uint64_t VECTOR_SIZE = 10U;
+    vector<uint64_t, VECTOR_SIZE> first;
+    vector<uint64_t, VECTOR_SIZE> second;
 
-    for (uint64_t i = 0U; i < 5U; ++i)
+    for (uint64_t i = 0U; i < VECTOR_SIZE / 2U; ++i)
     {
         first.emplace_back(i + OFFSET);
     }
 
-    for (uint64_t i = 5U; i < 10U; ++i)
+    for (uint64_t i = VECTOR_SIZE / 2U; i < VECTOR_SIZE; ++i)
     {
         second.emplace_back(i + OFFSET);
     }
@@ -140,8 +141,8 @@ TEST_F(algorithm_test, MergeTwoDisjunctNonEmptySortedContainers)
     auto mergedContainer = uniqueMergeSortedContainers(first, second);
     auto mergedContainerSwitched = uniqueMergeSortedContainers(second, first);
 
-    ASSERT_THAT(mergedContainer.size(), Eq(10U));
-    for (uint64_t i = 0U; i < 10U; ++i)
+    ASSERT_THAT(mergedContainer.size(), Eq(VECTOR_SIZE));
+    for (uint64_t i = 0U; i < VECTOR_SIZE; ++i)
     {
         EXPECT_THAT(mergedContainer[i], Eq(i + OFFSET));
     }
@@ -152,16 +153,17 @@ TEST_F(algorithm_test, MergeTwoDisjunctNonEmptySortedContainersWithAGap)
 {
     ::testing::Test::RecordProperty("TEST_ID", "15d3c063-8bc5-47eb-84a4-35f055a1d82c");
     constexpr uint64_t OFFSET = 41U;
-    constexpr uint64_t GAP = 13;
-    vector<uint64_t, 10U> first;
-    vector<uint64_t, 10U> second;
+    constexpr uint64_t GAP = 13U;
+    constexpr uint64_t VECTOR_SIZE = 10U;
+    vector<uint64_t, VECTOR_SIZE> first;
+    vector<uint64_t, VECTOR_SIZE> second;
 
-    for (uint64_t i = 0U; i < 5U; ++i)
+    for (uint64_t i = 0U; i < VECTOR_SIZE / 2U; ++i)
     {
         first.emplace_back(i + OFFSET);
     }
 
-    for (uint64_t i = 5U; i < 10U; ++i)
+    for (uint64_t i = VECTOR_SIZE / 2U; i < VECTOR_SIZE; ++i)
     {
         second.emplace_back(i + OFFSET + GAP);
     }
@@ -169,12 +171,12 @@ TEST_F(algorithm_test, MergeTwoDisjunctNonEmptySortedContainersWithAGap)
     auto mergedContainer = uniqueMergeSortedContainers(first, second);
     auto mergedContainerSwitched = uniqueMergeSortedContainers(second, first);
 
-    ASSERT_THAT(mergedContainer.size(), Eq(10U));
-    for (uint64_t i = 0U; i < 5U; ++i)
+    ASSERT_THAT(mergedContainer.size(), Eq(VECTOR_SIZE));
+    for (uint64_t i = 0U; i < VECTOR_SIZE / 2U; ++i)
     {
         EXPECT_THAT(mergedContainer[i], Eq(i + OFFSET));
     }
-    for (uint64_t i = 5U; i < 10U; ++i)
+    for (uint64_t i = VECTOR_SIZE / 2U; i < VECTOR_SIZE; ++i)
     {
         EXPECT_THAT(mergedContainer[i], Eq(i + OFFSET + GAP));
     }
@@ -185,15 +187,16 @@ TEST_F(algorithm_test, MergeTwoAlternatingDisjunctNonEmptySortedContainers)
 {
     ::testing::Test::RecordProperty("TEST_ID", "02cc9514-6cfe-4e08-8806-f371561fef41");
     constexpr uint64_t OFFSET = 4301U;
-    vector<uint64_t, 10U> first;
-    vector<uint64_t, 10U> second;
+    constexpr uint64_t VECTOR_SIZE = 10U;
+    vector<uint64_t, VECTOR_SIZE> first;
+    vector<uint64_t, VECTOR_SIZE> second;
 
-    for (uint64_t i = 0U; i < 5U; ++i)
+    for (uint64_t i = 0U; i < VECTOR_SIZE / 2U; ++i)
     {
         first.emplace_back(i * 2 + OFFSET);
     }
 
-    for (uint64_t i = 0U; i < 5U; ++i)
+    for (uint64_t i = 0U; i < VECTOR_SIZE / 2U; ++i)
     {
         second.emplace_back(i * 2 + 1 + OFFSET);
     }
@@ -201,8 +204,8 @@ TEST_F(algorithm_test, MergeTwoAlternatingDisjunctNonEmptySortedContainers)
     auto mergedContainer = uniqueMergeSortedContainers(first, second);
     auto mergedContainerSwitched = uniqueMergeSortedContainers(second, first);
 
-    ASSERT_THAT(mergedContainer.size(), Eq(10U));
-    for (uint64_t i = 0; i < 10; ++i)
+    ASSERT_THAT(mergedContainer.size(), Eq(VECTOR_SIZE));
+    for (uint64_t i = 0; i < VECTOR_SIZE; ++i)
     {
         EXPECT_THAT(mergedContainer[i], Eq(i + OFFSET));
     }
@@ -213,17 +216,18 @@ TEST_F(algorithm_test, MergingIdenticalContainerResultsInUnchangedContainer)
 {
     ::testing::Test::RecordProperty("TEST_ID", "50f05cf2-62fa-49b8-8380-1dd0ac2470ec");
     constexpr uint64_t OFFSET = 313U;
-    vector<uint64_t, 10U> someContainer;
+    constexpr uint64_t VECTOR_SIZE = 10U;
+    vector<uint64_t, VECTOR_SIZE> someContainer;
 
-    for (uint64_t i = 0U; i < 5U; ++i)
+    for (uint64_t i = 0U; i < VECTOR_SIZE / 2U; ++i)
     {
         someContainer.emplace_back(i * 2 + OFFSET);
     }
 
     auto mergedContainer = uniqueMergeSortedContainers(someContainer, someContainer);
 
-    ASSERT_THAT(mergedContainer.size(), Eq(5U));
-    for (uint64_t i = 0U; i < 5U; ++i)
+    ASSERT_THAT(mergedContainer.size(), Eq(VECTOR_SIZE / 2U));
+    for (uint64_t i = 0U; i < VECTOR_SIZE; ++i)
     {
         EXPECT_THAT(mergedContainer[static_cast<uint64_t>(i)], Eq(i * 2 + OFFSET));
     }
@@ -233,17 +237,18 @@ TEST_F(algorithm_test, MergingWithOneEmptyContainerResultsInUnchangedContainer)
 {
     ::testing::Test::RecordProperty("TEST_ID", "b0a0eb3a-08a3-4898-a8c9-a4f7eff0115c");
     constexpr uint64_t OFFSET = 707U;
-    vector<uint64_t, 10U> someContainer;
+    constexpr uint64_t VECTOR_SIZE = 10U;
+    vector<uint64_t, VECTOR_SIZE> someContainer;
 
-    for (uint64_t i = 0U; i < 5U; ++i)
+    for (uint64_t i = 0U; i < VECTOR_SIZE / 2U; ++i)
     {
         someContainer.emplace_back(i * 3 + OFFSET);
     }
 
-    auto mergedContainer = uniqueMergeSortedContainers(someContainer, vector<uint64_t, 10U>());
+    auto mergedContainer = uniqueMergeSortedContainers(someContainer, vector<uint64_t, VECTOR_SIZE>());
 
     ASSERT_THAT(mergedContainer.size(), Eq(5U));
-    for (uint64_t i = 0U; i < 5U; ++i)
+    for (uint64_t i = 0U; i < VECTOR_SIZE / 2U; ++i)
     {
         EXPECT_THAT(mergedContainer[i], Eq(i * 3 + OFFSET));
     }
@@ -252,16 +257,18 @@ TEST_F(algorithm_test, MergingWithOneEmptyContainerResultsInUnchangedContainer)
 TEST_F(algorithm_test, MergePartiallyOverlappingSortedContainers)
 {
     ::testing::Test::RecordProperty("TEST_ID", "c57dda77-81a5-413f-b54b-e924e67b66a5");
+    constexpr uint64_t VECTOR_SIZE = 10U;
+    constexpr uint64_t MAX_OVERLAPPING_INDEX = 8U;
     constexpr uint64_t OFFSET = 8055U;
-    vector<uint64_t, 10U> first;
-    vector<uint64_t, 10U> second;
+    vector<uint64_t, VECTOR_SIZE> first;
+    vector<uint64_t, VECTOR_SIZE> second;
 
-    for (uint64_t i = 3U; i < 10U; ++i)
+    for (uint64_t i = 3U; i < VECTOR_SIZE; ++i)
     {
         first.emplace_back(i + OFFSET);
     }
 
-    for (uint64_t i = 0U; i < 8U; ++i)
+    for (uint64_t i = 0U; i < MAX_OVERLAPPING_INDEX; ++i)
     {
         second.emplace_back(i + OFFSET);
     }
@@ -269,8 +276,8 @@ TEST_F(algorithm_test, MergePartiallyOverlappingSortedContainers)
     auto mergedContainer = uniqueMergeSortedContainers(first, second);
     auto mergedContainerSwitched = uniqueMergeSortedContainers(second, first);
 
-    ASSERT_THAT(mergedContainer.size(), Eq(10U));
-    for (uint64_t i = 0U; i < 10U; ++i)
+    ASSERT_THAT(mergedContainer.size(), Eq(VECTOR_SIZE));
+    for (uint64_t i = 0U; i < VECTOR_SIZE; ++i)
     {
         EXPECT_THAT(mergedContainer[i], Eq(i + OFFSET));
     }
@@ -281,21 +288,22 @@ TEST_F(algorithm_test, MergeWithDisjunctOneElementContainer)
 {
     ::testing::Test::RecordProperty("TEST_ID", "7a56b0f9-82d2-4f9a-881f-338dd572a453");
     constexpr uint64_t OFFSET = 333331U;
-    vector<uint64_t, 10U> first;
-    vector<uint64_t, 10U> second;
+    constexpr uint64_t VECTOR_SIZE = 10U;
+    vector<uint64_t, VECTOR_SIZE> first;
+    vector<uint64_t, VECTOR_SIZE> second;
 
-    for (uint64_t i = 0U; i < 5U; ++i)
+    for (uint64_t i = 0U; i < VECTOR_SIZE / 2U; ++i)
     {
         first.emplace_back(i + OFFSET);
     }
 
-    second.emplace_back(5 + OFFSET);
+    second.emplace_back(VECTOR_SIZE / 2U + OFFSET);
 
     auto mergedContainer = uniqueMergeSortedContainers(first, second);
     auto mergedContainerSwitched = uniqueMergeSortedContainers(second, first);
 
-    ASSERT_THAT(mergedContainer.size(), Eq(6U));
-    for (uint64_t i = 0U; i < 6U; ++i)
+    ASSERT_THAT(mergedContainer.size(), Eq(VECTOR_SIZE / 2U + 1));
+    for (uint64_t i = 0U; i < VECTOR_SIZE / 2U + 1; ++i)
     {
         EXPECT_THAT(mergedContainer[i], Eq(i + OFFSET));
     }
@@ -306,10 +314,11 @@ TEST_F(algorithm_test, MergeWithOverlappingOneElementContainer)
 {
     ::testing::Test::RecordProperty("TEST_ID", "05fb7baf-51e9-4ff9-bb35-8ae4174b0216");
     constexpr uint64_t OFFSET = 29292929U;
-    vector<uint64_t, 10U> first;
-    vector<uint64_t, 10U> second;
+    constexpr uint64_t VECTOR_SIZE = 10U;
+    vector<uint64_t, VECTOR_SIZE> first;
+    vector<uint64_t, VECTOR_SIZE> second;
 
-    for (uint64_t i = 0U; i < 5U; ++i)
+    for (uint64_t i = 0U; i < VECTOR_SIZE / 2U; ++i)
     {
         first.emplace_back(i + OFFSET);
     }
@@ -319,8 +328,8 @@ TEST_F(algorithm_test, MergeWithOverlappingOneElementContainer)
     auto mergedContainer = uniqueMergeSortedContainers(first, second);
     auto mergedContainerSwitched = uniqueMergeSortedContainers(second, first);
 
-    ASSERT_THAT(mergedContainer.size(), Eq(5U));
-    for (uint64_t i = 0U; i < 5U; ++i)
+    ASSERT_THAT(mergedContainer.size(), Eq(VECTOR_SIZE / 2U));
+    for (uint64_t i = 0U; i < VECTOR_SIZE / 2U; ++i)
     {
         EXPECT_THAT(mergedContainer[i], Eq(i + OFFSET));
     }
