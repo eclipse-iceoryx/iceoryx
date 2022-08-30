bfaf16ef25bafcfed9718e7291c97546f6b1622b
Simon Hoinkis <simon.hoinkis@apex.ai>
2022-08-30 18:59:56 +0200

    iox-#1594 Extend documentation on ScopeGuard
    Signed-off-by: Simon Hoinkis <simon.hoinkis@apex.ai>

--- iceoryx_hoofs/include/iceoryx_hoofs/cxx/scope_guard.hpp
+++ iceoryx_hoofs/include/iceoryx_hoofs/cxx/scope_guard.hpp
@@ -28,16 +28,17 @@ namespace cxx
 ///             idiom quickly. You set 2 functions, one which is called in the
 ///             constructor and another function is called in the destructor
 ///             which can be useful when handling resources.
+/// @tparam [in] CleanupCapacity The static storage capacity available to store a cleanup callable in bytes
 /// @code
 /// // This example leads to a console output of:
 /// // hello world
 /// // I am doing stuff
 /// // goodbye
 /// void someFunc() {
-///     auto raii{[](){ std::cout << "hello world\n"; },
+///     ScopeGuard myScopeGuard{[](){ std::cout << "hello world\n"; },
 ///                 [](){ std::cout << "goodbye\n"; }};
 ///     std::cout << "I am doing stuff\n";
-///     // raii goes out of scope here and the cleanupFunction is called in the
+///     // myScopeGuard goes out of scope here and the cleanupFunction is called in the
 ///     // destructor
 /// }
 /// @endcode
