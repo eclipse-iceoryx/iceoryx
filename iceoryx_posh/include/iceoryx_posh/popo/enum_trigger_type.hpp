fb3e55fdba30531bed90c5635e2d2aada9615281
Dietrich Krönke <dietrich.kroenke@apex.ai>
2022-09-16 17:21:37 +0200

    iox-#1640 Clang format files
    Signed-off-by: Dietrich Krönke <dietrich.kroenke@apex.ai>

--- iceoryx_posh/include/iceoryx_posh/popo/enum_trigger_type.hpp
+++ iceoryx_posh/include/iceoryx_posh/popo/enum_trigger_type.hpp
@@ -33,12 +33,12 @@ using EventEnumIdentifier = int64_t;
 /// @brief contains true when T is an event based enum, otherwise false
 template <typename T>
 constexpr bool IS_EVENT_ENUM =
-    std::is_enum<T>::value&& std::is_same<std::underlying_type_t<T>, EventEnumIdentifier>::value;
+    std::is_enum<T>::value && std::is_same<std::underlying_type_t<T>, EventEnumIdentifier>::value;
 
 /// @brief contains true when T is a state based enum, otherwise false
 template <typename T>
 constexpr bool IS_STATE_ENUM =
-    std::is_enum<T>::value&& std::is_same<std::underlying_type_t<T>, StateEnumIdentifier>::value;
+    std::is_enum<T>::value && std::is_same<std::underlying_type_t<T>, StateEnumIdentifier>::value;
 } // namespace popo
 } // namespace iox
 
