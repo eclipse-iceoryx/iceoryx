9919aec87dc205c209a397011d5cb74a057c2f9a
Christian Eltzschig <christian.eltzschig@apex.ai>
2022-10-10 13:49:53 +0200

    iox-#1394 Add todo to use a string_view
    Signed-off-by: Christian Eltzschig <christian.eltzschig@apex.ai>

--- iceoryx_hoofs/include/iceoryx_hoofs/cxx/functional_interface.hpp
+++ iceoryx_hoofs/include/iceoryx_hoofs/cxx/functional_interface.hpp
@@ -52,6 +52,7 @@ struct HasGetErrorMethod<Derived, cxx::void_t<decltype(std::declval<Derived>().g
 {
 };
 
+// @todo iox-#1723 use string_view as argument
 // AXIVION Next Construct AutosarC++19_03-A3.9.1 : Requires char base type to print strings originating
 // from string literals, cxx::string::c_str(), std::string::c_str() or other sources. Only with char we
 // can achieve the task.
