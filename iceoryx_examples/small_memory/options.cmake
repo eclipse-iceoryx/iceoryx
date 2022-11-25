272c8533f3dde48603371c7ac4bac4332e9e5c48
Jakub Sosnovec <jakub.sosnovec@nxp.com>
2022-11-25 10:42:22 +0100

    iox-#1803 Add new example for small memory footprint

---
+++ iceoryx_examples/small_memory/options.cmake
@@ -0,0 +1,36 @@
+# Copyright (c) 2022 by NXP. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set(IOX_MAX_PUBLISHERS 8 CACHE STRING "")
+set(IOX_MAX_SUBSCRIBERS 8 CACHE STRING "")
+set(IOX_MAX_INTERFACE_NUMBER 2 CACHE STRING "")
+set(IOX_MAX_SUBSCRIBERS_PER_PUBLISHER 2 CACHE STRING "")
+set(IOX_MAX_CHUNKS_ALLOCATED_PER_PUBLISHER_SIMULTANEOUSLY 2 CACHE STRING "")
+set(IOX_MAX_PUBLISHER_HISTORY 2 CACHE STRING "")
+set(IOX_MAX_CHUNKS_HELD_PER_SUBSCRIBER_SIMULTANEOUSLY 2 CACHE STRING "")
+set(IOX_MAX_PROCESS_NUMBER 2 CACHE STRING "")
+set(IOX_MAX_NODE_NUMBER 8 CACHE STRING "")
+set(IOX_MAX_NODE_PER_PROCESS 8 CACHE STRING "")
+set(IOX_MAX_SHM_SEGMENTS 2 CACHE STRING "")
+set(IOX_MAX_NUMBER_OF_MEMPOOLS 3 CACHE STRING "")
+set(IOX_MAX_NUMBER_OF_CONDITION_VARIABLES 8 CACHE STRING "")
+set(IOX_MAX_NODE_NAME_LENGTH 30 CACHE STRING "")
+set(IOX_MAX_ID_STRING_LENGTH 30 CACHE STRING "")
+set(IOX_MAX_RUNTIME_NAME_LENGTH 30 CACHE STRING "")
+set(IOX_MAX_RESPONSES_PROCESSED_SIMULTANEOUSLY 2 CACHE STRING "")
+set(IOX_MAX_RESPONSE_QUEUE_CAPACITY 2 CACHE STRING "")
+set(IOX_MAX_REQUEST_QUEUE_CAPACITY 2 CACHE STRING "")
+set(IOX_MAX_CLIENTS_PER_SERVER 2 CACHE STRING "")
