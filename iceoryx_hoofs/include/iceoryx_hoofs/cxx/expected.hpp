7c10ac7a49a03576ed32652cecacc583edca6237
Jakub Sosnovec <jakub.sosnovec@nxp.com>
2022-12-01 16:57:17 +0100

    iox-#1812 Fix another `expected::operator bool` doxygen

--- iceoryx_hoofs/include/iceoryx_hoofs/cxx/expected.hpp
+++ iceoryx_hoofs/include/iceoryx_hoofs/cxx/expected.hpp
@@ -350,8 +350,8 @@ class IOX_NO_DISCARD expected<ValueType, ErrorType>
     template <typename... Targs>
     static expected create_error(Targs&&... args) noexcept;
 
-    /// @brief  returns true if the expected contains an error otherwise false
-    /// @return bool which contains true if the expected contains an error
+    /// @brief  returns true if the expected signals success otherwise false
+    /// @return bool which contains true if the expected signals success
     explicit operator bool() const noexcept;
 
     /// @brief  returns true if the expected contains an error otherwise false
