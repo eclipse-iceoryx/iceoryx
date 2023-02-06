d0faa4eb0421f470626c8a4eaade77ee74ecedfe
Mathias Kraus <mathias.kraus@apex.ai>
2023-02-06 19:38:30 +0100

    iox-#1877 Speed up check for missing test IDs

--- tools/scripts/check_test_ids.sh
+++ tools/scripts/check_test_ids.sh
@@ -24,21 +24,52 @@ set -e
 ICEORYX_PATH=$(git rev-parse --show-toplevel)
 cd $ICEORYX_PATH
 
+function numberOfTestCases() {
+    grep -rn --include="*.cpp" -e "^\(TEST\|TYPED_TEST\|TIMING_TEST\)" $1 | wc -l
+}
+function numberOfFalsePositives() {
+    grep -rn --include="*.cpp" TYPED_TEST_SUITE $1 | wc -l
+}
+function numberOfTestIDs() {
+    grep -rn --include="*.cpp" "::testing::Test::RecordProperty" $1 | wc -l
+}
+
 ## sanity check for number of tests and test IDs
-for file in $(find . -iname "*.cpp")
-do
-    echo "Check file: $file"
-    numberOfTestCases=$(grep -rn --include="*.cpp" -e "^\(TEST\|TYPED_TEST\|TIMING_TEST\)" $file | wc -l)
-    numberOfFalsePositives=$(grep -rn --include="*.cpp" TYPED_TEST_SUITE $file | wc -l)
-    set +e
-    numberOfTestCasesWithoutFalsePositives=$(expr $numberOfTestCases - $numberOfFalsePositives)
-    set -e
-    numberOfTestIDs=$(grep -rn --include="*.cpp" "::testing::Test::RecordProperty" $file | wc -l)
-    if [[ "$numberOfTestCasesWithoutFalsePositives" -gt "$numberOfTestIDs" ]]; then
-        echo -e "\e[1;31mThe file \"$file\" is missing test IDs for some test cases!\e[m"
-        exit 1
+## at first a quick check and then a slower check for the respective files
+numberOfTestCasesTotal=$(numberOfTestCases "iceoryx_*")
+numberOfFalsePositivesTotal=$(numberOfFalsePositives "iceoryx_*")
+numberOfTestCasesWithoutFalsePositivesTotal=$numberOfTestCasesTotal-$numberOfFalsePositivesTotal
+numberOfTestIDsTotal=$(numberOfTestIDs "iceoryx_*")
+if [[ "$numberOfTestCasesWithoutFalsePositivesTotal" -gt "$numberOfTestIDsTotal" ]]; then
+    echo -e "\e[1;31mThe number of test IDs do not match the number of test cases!\e[m"
+    echo "number of test cases: $numberOfTestCasesTotal"
+    echo "number of false positives: $numberOfFalsePositivesTotal"
+    echo "number of test IDs: $numberOfTestIDsTotal"
+    echo ""
+    echo "trying to find files with missing IDs ..."
+
+    found=0
+    # find file with missing IDs
+    for file in $(find iceoryx_* -iname "*.cpp")
+    do
+        numberOfTestCasesInFile=$(numberOfTestCases $file)
+        numberOfFalsePositivesFile=$(numberOfFalsePositives $file)
+        numberOfTestCasesWithoutFalsePositivesFile=$numberOfTestCasesInFile-$numberOfFalsePositivesFile
+        numberOfTestIDsFile=$(numberOfTestIDs $file)
+        if [[ "$numberOfTestCasesWithoutFalsePositivesFile" -gt "$numberOfTestIDsFile" ]]; then
+            echo -e "\e[1;31mThe file \"$file\" is missing test IDs for some test cases!\e[m"
+            found=1
+        fi
+    done
+
+    if [[ $found -gt 0 ]]; then
+        echo "... done"
+    else
+        echo "... not found"
     fi
-done
+    exit 1
+fi
+
 
 ## unique test IDs
 notUniqueIds=$(grep -hr "::testing::Test::RecordProperty" | sort | uniq -d)
