5c3916fe26081fed92da3ee617a764f934417816
Mathias Kraus <mathias.kraus@apex.ai>
2023-02-08 00:48:29 +0100

    iox-#1613 Fix macOS build

--- iceoryx_posh/test/moduletests/test_popo_chunk_distributor.cpp
+++ iceoryx_posh/test/moduletests/test_popo_chunk_distributor.cpp
@@ -135,8 +135,7 @@ TYPED_TEST(ChunkDistributor_test, AddingNullptrQueueDoesNotWork)
     auto sutData = this->getChunkDistributorData();
     typename TestFixture::ChunkDistributor_t sut(sutData.get());
 
-
-    IOX_EXPECT_FATAL_FAILURE<iox::HoofsError>([&] { sut.tryAddQueue(nullptr); },
+    IOX_EXPECT_FATAL_FAILURE<iox::HoofsError>([&] { IOX_DISCARD_RESULT(sut.tryAddQueue(nullptr)); },
                                               iox::HoofsError::EXPECTS_ENSURES_FAILED);
 }
 
