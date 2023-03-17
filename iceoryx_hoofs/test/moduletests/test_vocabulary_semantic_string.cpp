9f78b68d9f81cbee4306cb1db7b390c31f8484de
Christian Eltzschig <me@elchris.org>
2023-03-17 18:02:47 +0100

    iox-#1942 Add tests to verify creation of the semantic string

--- iceoryx_hoofs/test/moduletests/test_vocabulary_semantic_string.cpp
+++ iceoryx_hoofs/test/moduletests/test_vocabulary_semantic_string.cpp
@@ -36,8 +36,16 @@ struct TestValues<UserName>
 {
     static constexpr uint64_t CAPACITY = platform::MAX_USER_NAME_LENGTH;
     static constexpr const char VALID_VALUES[][CAPACITY]{{"some-user"}, {"user2"}};
+    static constexpr const char INVALID_CHARACTER_VALUES[][CAPACITY]{
+        {"some-!user"}, {"*kasjd"}, {"_fuuuas"}, {"asd/asd"}, {";'1'fuuuu"}, {"argh/"}};
+    static constexpr const char INVALID_CONTENT_VALUES[][CAPACITY]{
+        {""}, {"-do-not-start-with-dash"}, {"5do-not-start-with-a-number"}};
+    static constexpr const char TOO_LONG_CONTENT_VALUES[][CAPACITY * 2]{{"i-am-waaaaay-toooooooo-loooooooong"}};
 };
 constexpr const char TestValues<UserName>::VALID_VALUES[][TestValues<UserName>::CAPACITY];
+constexpr const char TestValues<UserName>::INVALID_CHARACTER_VALUES[][TestValues<UserName>::CAPACITY];
+constexpr const char TestValues<UserName>::INVALID_CONTENT_VALUES[][TestValues<UserName>::CAPACITY];
+constexpr const char TestValues<UserName>::TOO_LONG_CONTENT_VALUES[][TestValues<UserName>::CAPACITY * 2];
 // NOLINTEND(hicpp-avoid-c-arrays, cppcoreguidelines-avoid-c-arrays, hicpp-explicit-conversions)
 
 template <typename T>
@@ -79,5 +87,46 @@ TYPED_TEST(SemanticString_test, InitializeWithValidStringValueWorks)
         EXPECT_THAT(sut->as_string(), Eq(string<SutType::capacity()>(TruncateToCapacity, value)));
     }
 }
-} // namespace
 
+TYPED_TEST(SemanticString_test, InitializeWithStringContainingIllegalCharactersFails)
+{
+    ::testing::Test::RecordProperty("TEST_ID", "14483f4e-d556-4770-89df-84d873428eee");
+    using SutType = typename TestFixture::SutType;
+
+    for (auto value : TestValues<SutType>::INVALID_CHARACTER_VALUES)
+    {
+        auto sut = SutType::create(string<SutType::capacity()>(TruncateToCapacity, value));
+
+        ASSERT_THAT(sut.has_error(), Eq(true));
+        ASSERT_THAT(sut.get_error(), Eq(SemanticStringError::ContainsInvalidCharacters));
+    }
+}
+
+TYPED_TEST(SemanticString_test, InitializeWithStringContainingIllegalContentFails)
+{
+    ::testing::Test::RecordProperty("TEST_ID", "9380f932-527f-4116-bd4f-dc8078b63330");
+    using SutType = typename TestFixture::SutType;
+
+    for (auto value : TestValues<SutType>::INVALID_CONTENT_VALUES)
+    {
+        auto sut = SutType::create(string<SutType::capacity()>(TruncateToCapacity, value));
+
+        ASSERT_THAT(sut.has_error(), Eq(true));
+        ASSERT_THAT(sut.get_error(), Eq(SemanticStringError::ContainsInvalidContent));
+    }
+}
+
+TYPED_TEST(SemanticString_test, InitializeWithTooLongContentFails)
+{
+    ::testing::Test::RecordProperty("TEST_ID", "b5597825-c559-48e7-96f3-5136fffc55d7");
+    using SutType = typename TestFixture::SutType;
+
+    for (auto value : TestValues<SutType>::TOO_LONG_CONTENT_VALUES)
+    {
+        auto sut = SutType::create(string<SutType::capacity() * 2>(TruncateToCapacity, value));
+
+        ASSERT_THAT(sut.has_error(), Eq(true));
+        ASSERT_THAT(sut.get_error(), Eq(SemanticStringError::ExceedsMaximumLength));
+    }
+}
+} // namespace
