4868ef714544556291e108bb3a95b837c9813af1
Christian Eltzschig <me@elchris.org>
2023-03-20 13:36:02 +0100

    iox-#1942 Add documentation to the semantic string

--- iceoryx_hoofs/vocabulary/include/iox/semantic_string.hpp
+++ iceoryx_hoofs/vocabulary/include/iox/semantic_string.hpp
@@ -24,6 +24,8 @@
 
 namespace iox
 {
+/// @brief Defines errors which can occur when modifying or creating a
+///         SemanticString
 enum class SemanticStringError
 {
     ContainsInvalidCharacters,
@@ -37,31 +39,84 @@ using DoesContainInvalidCharacter = bool (*)(const string<Capacity>& value);
 template <uint64_t Capacity>
 using DoesContainInvalidContent = bool (*)(const string<Capacity>& value);
 
-// TODO: doc add tests to the semantic string test suite
+/// @brief The SemanticString is a string which as an inner syntax and restrictions
+///         to valid content. Examples are for instance
+///         * UserName, only characters and numbers are allowed
+///         * FileNames, no slashes etc.
+///        The SemanticString shall be a basic building block to create string
+///        types with a semantic contract. It is used via inheritance. The user
+///        has to also define the maximum capacity, a callable which defines
+///        invalid characters as well as a callable which defines invalid content.
+/// @code
+/// bool user_name_does_contain_invalid_characters(const string<platform::MAX_USER_NAME_LENGTH>& value) noexcept;
+/// bool user_name_does_contain_invalid_content(const string<platform::MAX_USER_NAME_LENGTH>& value) noexcept;
+///
+/// // define custom semantic string UserName
+/// class UserName : public SemanticString<platform::MAX_USER_NAME_LENGTH,
+///                                        user_name_does_contain_invalid_content,
+///                                        user_name_does_contain_invalid_characters>
+/// {
+/// };
+/// @endcode
+/// @note Since the inner logic of the SemanticString is always the same additional
+///         implementations can be verified directly by the test suite defined in
+///         'test_vocabulary_semantic_string.cpp'.
+///         One has to only add the specific implementation to the 'Implementations'
+///         type list.
 template <uint64_t Capacity,
           DoesContainInvalidContent<Capacity> DoesContainInvalidContentCall,
           DoesContainInvalidCharacter<Capacity> DoesContainInvalidCharacterCall>
 class SemanticString
 {
   public:
+    /// @brief Creates a new SemanticString from the provided string literal.
+    ///         If the value contains invalid characters or invalid content
+    ///         the expected returns an error describing the cause.
+    /// @param[in] value the value of the SemanticString
+    /// @return expected either containing the new SemanticString or an error
     template <uint64_t N>
     // avoid-c-arrays: we would like to assign string_literals, safe since it is known
     //                 at compile time.
     // NOLINTNEXTLINE(hicpp-avoid-c-arrays, cppcoreguidelines-avoid-c-arrays, hicpp-explicit-conversions)
     static expected<SemanticString, SemanticStringError> create(const char (&value)[N]) noexcept;
 
+    /// @brief Creates a new SemanticString from the provided string.
+    ///         If the value contains invalid characters or invalid content
+    ///         the expected returns an error describing the cause.
+    /// @param[in] value the value of the SemanticString
+    /// @return expected either containing the new SemanticString or an error
     template <uint64_t N>
     static expected<SemanticString, SemanticStringError> create(const string<N>& value) noexcept;
 
+    /// @brief Returns the number of characters.
+    /// @return number of characters
     constexpr uint64_t size() const noexcept;
 
+    /// @brief Returns the capacity of the string.
+    /// @return the maximum amount of characters which can be stored.
     static constexpr uint64_t capacity() noexcept;
 
+    /// @brief Returns a const reference to the underlying string. It is const
+    ///         and shall not be modified to guarantee the contract that a
+    ///         SemanticString contains always a valid value.
+    /// @return string reference containing the actual value.
     constexpr const string<Capacity>& as_string() const noexcept;
 
+    /// @brief Appends another string to the SemanticString. If the value contains
+    ///        invalid characters or the result would end up in invalid content
+    ///        it fails.
+    /// @param[in] value the value which should be added
+    /// @return on failure the error inside the expected describes the failure
     template <typename T>
     expected<SemanticStringError> append(const T& value) noexcept;
 
+    /// @brief Inserts another string into the SemanticString. If the value contains
+    ///        invalid characters or the result would end up in invalid content
+    ///        it fails.
+    /// @param[in] pos the position where the string should be inserted
+    /// @param[in] str the value which should be added
+    /// @param[in] count how many characters of str shall be inserted
+    /// @return on failure the error inside the expected describes the failure
     template <typename T>
     expected<SemanticStringError> insert(const uint64_t pos, const T& str, const uint64_t count) noexcept;
 
