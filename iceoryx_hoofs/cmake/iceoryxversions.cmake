322c55d25500a24e67849b0097bb637be742496f
Mathias Kraus <elboberido@m-hias.de>
2023-10-25 00:06:08 +0200

    iox-#2057 Move 'iceoryx_versions.hpp' to 'hoofs'

--- iceoryx_hoofs/cmake/iceoryxversions.cmake
+++ iceoryx_hoofs/cmake/iceoryxversions.cmake
@@ -31,7 +31,7 @@ if(DEFINED ${PROJECT_VERSION_TWEAK})
 endif()
 
 configure_file("${CMAKE_CURRENT_SOURCE_DIR}/cmake/iceoryx_versions.hpp.in"
-  "${CMAKE_BINARY_DIR}/generated/iceoryx/include/iceoryx_versions.hpp" @ONLY)
-install(FILES ${CMAKE_BINARY_DIR}/generated/iceoryx/include/iceoryx_versions.hpp
+  "${CMAKE_BINARY_DIR}/generated/iceoryx_hoofs/include/iceoryx_versions.hpp" @ONLY)
+install(FILES ${CMAKE_BINARY_DIR}/generated/iceoryx_hoofs/include/iceoryx_versions.hpp
   DESTINATION include/${PREFIX}
   COMPONENT dev)
