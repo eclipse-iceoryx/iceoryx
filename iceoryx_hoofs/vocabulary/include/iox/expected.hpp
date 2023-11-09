7b5cd753ec4fe1c3dd86642cf238bcea53fc7ed5
Mathias Kraus <elboberido@m-hias.de>
2023-11-09 17:56:40 +0100

    iox-#1391 Use IOX_DEPRECATED_SINCE where appropriate

--- iceoryx_hoofs/vocabulary/include/iox/expected.hpp
+++ iceoryx_hoofs/vocabulary/include/iox/expected.hpp
@@ -18,6 +18,7 @@
 #define IOX_HOOFS_VOCABULARY_EXPECTED_HPP
 
 #include "iox/attributes.hpp"
+#include "iox/detail/deprecation_marker.hpp"
 #include "iox/detail/expected_helper.hpp"
 #include "iox/functional_interface.hpp"
 #include "iox/optional.hpp"
@@ -243,15 +244,15 @@ class IOX_NO_DISCARD expected final : public FunctionalInterface<expected<ValueT
 
     /// @copydoc expected::error()&
     /// @deprecated use 'error' instead of 'get_error'
-    [[deprecated("Use 'error' instead of 'get_error'")]] ErrorType& get_error() & noexcept;
+    IOX_DEPRECATED_SINCE(3, "Please use 'error' instead of 'get_error'") ErrorType& get_error() & noexcept;
 
     /// @copydoc expected::error()const&
     /// @deprecated use 'error' instead of 'get_error'
-    [[deprecated("Use 'error' instead of 'get_error'")]] const ErrorType& get_error() const& noexcept;
+    IOX_DEPRECATED_SINCE(3, "Please use 'error' instead of 'get_error'") const ErrorType& get_error() const& noexcept;
 
     /// @copydoc expected::error()&&
     /// @deprecated use 'error' instead of 'get_error'
-    [[deprecated("Use 'error' instead of 'get_error'")]] ErrorType&& get_error() && noexcept;
+    IOX_DEPRECATED_SINCE(3, "Please use 'error' instead of 'get_error'") ErrorType&& get_error() && noexcept;
 
     /// @brief  returns a lvalue reference to the contained success value, if the expected
     ///         does not contain a success value the error handler is called
