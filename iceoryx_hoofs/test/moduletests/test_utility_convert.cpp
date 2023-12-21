640c0105a452fcd68cac05291e5ef3b4ea84c8d9
Dennis Liu <dennis48161025@gmail.com>
2023-12-21 16:04:04 +0800

    iox-#2055 Use assertion for `has_value` check
    Signed-off-by: Dennis Liu <dennis48161025@gmail.com>

--- iceoryx_hoofs/test/moduletests/test_utility_convert.cpp
+++ iceoryx_hoofs/test/moduletests/test_utility_convert.cpp
@@ -111,7 +111,7 @@ TEST_F(convert_test, FromString_String)
     ::testing::Test::RecordProperty("TEST_ID", "22463da5-0fcb-4aa2-a7e5-68b863278a81");
     std::string source = "hello";
     auto result = iox::convert::from_string<std::string>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(true));
+    ASSERT_THAT(result.has_value(), Eq(true));
     EXPECT_THAT(result.value(), Eq(source));
 }
 
@@ -120,7 +120,7 @@ TEST_F(convert_test, fromString_Char_Success)
     ::testing::Test::RecordProperty("TEST_ID", "a15825c9-536a-4671-a502-6973490022e7");
     std::string source = "h";
     auto result = iox::convert::from_string<char>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(true));
+    ASSERT_THAT(result.has_value(), Eq(true));
     EXPECT_THAT(result.value(), Eq(source[0]));
 }
 
@@ -129,7 +129,7 @@ TEST_F(convert_test, fromString_Char_Fail)
     ::testing::Test::RecordProperty("TEST_ID", "656e87ad-6fdb-42d7-bf49-23f81a4f5a31");
     std::string source = "hasd";
     auto result = iox::convert::from_string<char>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(false));
+    ASSERT_THAT(result.has_value(), Eq(false));
 }
 
 TEST_F(convert_test, fromString_FLOAT_Success)
@@ -137,7 +137,7 @@ TEST_F(convert_test, fromString_FLOAT_Success)
     ::testing::Test::RecordProperty("TEST_ID", "d6255c3e-369e-43a0-a1ab-03f7b13d03c2");
     std::string source = "123.01";
     auto result = iox::convert::from_string<float>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(true));
+    ASSERT_THAT(result.has_value(), Eq(true));
     EXPECT_FLOAT_EQ(result.value(), 123.01F);
 }
 
@@ -146,7 +146,7 @@ TEST_F(convert_test, fromString_FLOAT_Fail)
     ::testing::Test::RecordProperty("TEST_ID", "e2b94d50-664c-4f9e-be4f-99212c6fa165");
     std::string source = "hasd";
     auto result = iox::convert::from_string<float>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(false));
+    ASSERT_THAT(result.has_value(), Eq(false));
 }
 
 TEST_F(convert_test, fromString_Double_Success)
@@ -154,7 +154,7 @@ TEST_F(convert_test, fromString_Double_Success)
     ::testing::Test::RecordProperty("TEST_ID", "95ba379e-120e-4b80-a829-33fe54f1bfed");
     std::string source = "123.04";
     auto result = iox::convert::from_string<double>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(true));
+    ASSERT_THAT(result.has_value(), Eq(true));
     EXPECT_THAT(result.value(), DoubleEq(123.04));
 }
 
@@ -163,7 +163,7 @@ TEST_F(convert_test, fromString_Double_Fail)
     ::testing::Test::RecordProperty("TEST_ID", "f4ace11b-a056-47b1-b6c5-6fb2c58e1a06");
     std::string source = "hasd";
     auto result = iox::convert::from_string<double>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(false));
+    ASSERT_THAT(result.has_value(), Eq(false));
 }
 
 TEST_F(convert_test, fromString_LongDouble_Success)
@@ -172,7 +172,7 @@ TEST_F(convert_test, fromString_LongDouble_Success)
     std::string source = "121.01";
     constexpr long double VERIFY = 121.01;
     auto result = iox::convert::from_string<long double>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(true));
+    ASSERT_THAT(result.has_value(), Eq(true));
     EXPECT_THAT(static_cast<double>(result.value()), DoubleEq(static_cast<double>(VERIFY)));
 }
 
@@ -181,7 +181,7 @@ TEST_F(convert_test, fromString_LongDouble_Fail)
     ::testing::Test::RecordProperty("TEST_ID", "519f2ac5-8836-419e-8034-377230a88a09");
     std::string source = "hasd";
     auto result = iox::convert::from_string<long double>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(false));
+    ASSERT_THAT(result.has_value(), Eq(false));
 }
 
 TEST_F(convert_test, fromString_UNSIGNED_Int_Success)
@@ -189,7 +189,7 @@ TEST_F(convert_test, fromString_UNSIGNED_Int_Success)
     ::testing::Test::RecordProperty("TEST_ID", "1edb8d5f-c42d-4d02-bc31-477f48898bbb");
     std::string source = "100";
     auto result = iox::convert::from_string<unsigned int>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(true));
+    ASSERT_THAT(result.has_value(), Eq(true));
     EXPECT_THAT(result.value(), Eq(100U));
 }
 
@@ -198,7 +198,7 @@ TEST_F(convert_test, fromString_UNSIGNED_Int_Fail)
     ::testing::Test::RecordProperty("TEST_ID", "6ce6de82-a6c0-4562-9c5c-663b93d768b3");
     std::string source = "-331";
     auto result = iox::convert::from_string<unsigned int>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(false));
+    ASSERT_THAT(result.has_value(), Eq(false));
 }
 
 TEST_F(convert_test, fromString_UNSIGNED_LongInt_Success)
@@ -206,7 +206,7 @@ TEST_F(convert_test, fromString_UNSIGNED_LongInt_Success)
     ::testing::Test::RecordProperty("TEST_ID", "054b08b2-54e1-4191-91b6-e6bec415612f");
     std::string source = "999";
     auto result = iox::convert::from_string<uint64_t>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(true));
+    ASSERT_THAT(result.has_value(), Eq(true));
     EXPECT_THAT(result.value(), Eq(999LU));
 }
 
@@ -215,7 +215,7 @@ TEST_F(convert_test, fromString_UNSIGNED_LongInt_Fail)
     ::testing::Test::RecordProperty("TEST_ID", "4b215747-90b2-4ca2-97ee-517c07597b1b");
     std::string source = "-a123";
     auto result = iox::convert::from_string<uint64_t>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(false));
+    ASSERT_THAT(result.has_value(), Eq(false));
 }
 
 TEST_F(convert_test, fromString_Int_Success)
@@ -223,7 +223,7 @@ TEST_F(convert_test, fromString_Int_Success)
     ::testing::Test::RecordProperty("TEST_ID", "9318ee60-f2e0-445a-b32d-c718cf918b18");
     std::string source = "3331";
     auto result = iox::convert::from_string<int>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(true));
+    ASSERT_THAT(result.has_value(), Eq(true));
     EXPECT_THAT(result.value(), Eq(3331));
 }
 
@@ -232,7 +232,7 @@ TEST_F(convert_test, fromString_Int_Fail)
     ::testing::Test::RecordProperty("TEST_ID", "f8e698a9-054d-4441-b196-bcd58a72b1d9");
     std::string source = "-+321";
     auto result = iox::convert::from_string<int>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(false));
+    ASSERT_THAT(result.has_value(), Eq(false));
 }
 
 TEST_F(convert_test, fromString_ShortInt_Success)
@@ -240,7 +240,7 @@ TEST_F(convert_test, fromString_ShortInt_Success)
     ::testing::Test::RecordProperty("TEST_ID", "e804f821-157d-4c52-81a7-75fce5a43805");
     std::string source = "12345";
     auto result = iox::convert::from_string<short>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(true));
+    ASSERT_THAT(result.has_value(), Eq(true));
     EXPECT_THAT(result.value(), Eq(12345));
 }
 
@@ -249,7 +249,7 @@ TEST_F(convert_test, fromString_ShortInt_Fail)
     ::testing::Test::RecordProperty("TEST_ID", "1150066b-cb42-4055-9927-2f20fb40bc87");
     std::string source = "-+123321";
     auto result = iox::convert::from_string<short>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(false));
+    ASSERT_THAT(result.has_value(), Eq(false));
 }
 
 TEST_F(convert_test, fromString_Bool_Success)
@@ -257,7 +257,7 @@ TEST_F(convert_test, fromString_Bool_Success)
     ::testing::Test::RecordProperty("TEST_ID", "893723fc-dfb8-46a4-b446-badaf8bad25a");
     std::string source = "1";
     auto result = iox::convert::from_string<bool>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(true));
+    ASSERT_THAT(result.has_value(), Eq(true));
     EXPECT_THAT(result.value(), Eq(true));
 }
 
@@ -266,7 +266,7 @@ TEST_F(convert_test, fromString_Bool_Fail)
     ::testing::Test::RecordProperty("TEST_ID", "1c937da6-29ea-49cf-a7d0-4c46f564c16e");
     std::string source = "-+222";
     auto result = iox::convert::from_string<bool>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(false));
+    ASSERT_THAT(result.has_value(), Eq(false));
 }
 
 TEST_F(convert_test, fromString_UShortInt_Success)
@@ -274,7 +274,7 @@ TEST_F(convert_test, fromString_UShortInt_Success)
     ::testing::Test::RecordProperty("TEST_ID", "99d22d80-3860-47fa-9f98-f11ff9629815");
     std::string source = "333";
     auto result = iox::convert::from_string<unsigned short>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(true));
+    ASSERT_THAT(result.has_value(), Eq(true));
     EXPECT_THAT(result.value(), Eq(333));
 }
 
@@ -283,7 +283,7 @@ TEST_F(convert_test, fromString_UShortInt_Fail)
     ::testing::Test::RecordProperty("TEST_ID", "6ab6ded6-dff3-401a-8a7f-98326da7cca6");
     std::string source = "-+111";
     auto result = iox::convert::from_string<unsigned short>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(false));
+    ASSERT_THAT(result.has_value(), Eq(false));
 }
 
 TEST_F(convert_test, fromString_LongInt_Success)
@@ -291,7 +291,7 @@ TEST_F(convert_test, fromString_LongInt_Success)
     ::testing::Test::RecordProperty("TEST_ID", "37133256-ae79-45c7-8c86-56bd33fa7bd8");
     std::string source = "-1123";
     auto result = iox::convert::from_string<int64_t>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(true));
+    ASSERT_THAT(result.has_value(), Eq(true));
     EXPECT_THAT(result.value(), Eq(-1123L));
 }
 
@@ -300,7 +300,7 @@ TEST_F(convert_test, fromString_LongInt_Fail)
     ::testing::Test::RecordProperty("TEST_ID", "0e368bf3-cb16-4829-a4cc-dc56e0bde958");
     std::string source = "-a121";
     auto result = iox::convert::from_string<int64_t>(source.c_str());
-    EXPECT_THAT(result.has_value(), Eq(false));
+    ASSERT_THAT(result.has_value(), Eq(false));
 }
 
 TEST_F(convert_test, fromString_MinMaxShort)
