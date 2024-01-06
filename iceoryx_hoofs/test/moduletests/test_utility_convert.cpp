26e351d38268469289a63a1df9c4f34c8f9061fa
Dennis Liu <dennis48161025@gmail.com>
2024-01-07 03:25:12 +0800

    iox-#2055 Rename to 'fromString_ioxString'
    Signed-off-by: Dennis Liu <dennis48161025@gmail.com>

--- iceoryx_hoofs/test/moduletests/test_utility_convert.cpp
+++ iceoryx_hoofs/test/moduletests/test_utility_convert.cpp
@@ -815,7 +815,7 @@ TEST_F(convert_test, fromString_LongDouble_EdgeCase_Inf_Success)
 
 /// SPECIAL FLOATING POINT TYPE EDGE CASES END
 
-TEST_F(convert_test, fromString_cxxString)
+TEST_F(convert_test, fromString_ioxString)
 {
     ::testing::Test::RecordProperty("TEST_ID", "dbf015bb-5f51-47e1-9d0e-0525f65e7803");
     std::string source = "hello";
