c19df2b52932559c3ce66368ee4ec227d2b439fb
Mathias Kraus <elboberido@m-hias.de>
2024-03-26 22:40:50 +0100

    iox-#2234 Assign copyright of elBoberido to ekxide IO GmbH

--- iceoryx_hoofs/posix/ipc/source/named_pipe.cpp
+++ iceoryx_hoofs/posix/ipc/source/named_pipe.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2021 - 2023 by Apex.AI Inc. All rights reserved.
-// Copyright (c) 2023 by Mathias Kraus <elboberido@m-hias.de>. All rights reserved.
+// Copyright (c) 2023 by ekxide IO GmbH. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
