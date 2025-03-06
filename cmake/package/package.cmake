0b6c557e2c43e43402b525717a269c5a164c4b27
Mathias Kraus <elboberido@m-hias.de>
2025-03-06 14:06:54 +0100

    iox-#1295 Bump version to 2.95.4

--- cmake/package/package.cmake
+++ cmake/package/package.cmake
@@ -14,7 +14,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 cmake_minimum_required(VERSION 3.16)
-set(IOX_VERSION_STRING "2.95.3")
+set(IOX_VERSION_STRING "2.95.4")
 
 project(iceoryx_package VERSION ${IOX_VERSION_STRING})
 
