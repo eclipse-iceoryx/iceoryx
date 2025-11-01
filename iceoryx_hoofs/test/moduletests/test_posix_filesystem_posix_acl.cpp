d14297c0fedfc35ef48c17c02c8f1ddd74889bae
Mathias Kraus <elboberido@m-hias.de>
2025-11-01 20:05:33 +0900

    iox-#2491 Relicense iceoryx_hoofs, iceoryx_platform, scripts, CI and git-hooks from Apache-2.0 to Apache-2.0 or MIT

--- iceoryx_hoofs/test/moduletests/test_posix_filesystem_posix_acl.cpp
+++ iceoryx_hoofs/test/moduletests/test_posix_filesystem_posix_acl.cpp
@@ -1,11 +1,10 @@
 // Copyright (c) 2019 by Robert Bosch GmbH. All rights reserved.
 // Copyright (c) 2021 - 2022 by Apex.AI Inc. All rights reserved.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
+// This program and the accompanying materials are made available under the
+// terms of the Apache Software License 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0, or the MIT license
+// which is available at https://opensource.org/licenses/MIT.
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0 OR MIT
 
 #include "iox/detail/posix_acl.hpp"
 
--- iceoryx_platform/win/source/socket.cpp
+++ iceoryx_platform/win/source/socket.cpp
@@ -1,10 +1,9 @@
 // Copyright (c) 2021 by Apex.AI Inc. All rights reserved.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
+// This program and the accompanying materials are made available under the
+// terms of the Apache Software License 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0, or the MIT license
+// which is available at https://opensource.org/licenses/MIT.
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0 OR MIT
 
 #include "iceoryx_platform/socket.hpp"
 #include "iceoryx_platform/logging.hpp"
